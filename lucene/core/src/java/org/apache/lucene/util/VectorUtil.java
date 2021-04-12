757da769196ff1a6e63f00794bf2758ea90fef41
Michael Sokolov <sokolovm@amazon.com>
2021-04-12 16:50:16 -0400

    Revert "LUCENE-9798 : Fix looping bug and made Full Knn calculation parallelizable (#55)"
    This reverts commit e7de06eb51e70de39625718354a8e1088f283d1d.

--- lucene/core/src/java/org/apache/lucene/util/VectorUtil.java
+++ lucene/core/src/java/org/apache/lucene/util/VectorUtil.java
@@ -17,8 +17,6 @@
 
 package org.apache.lucene.util;
 
-import java.util.Random;
-
 /** Utilities for computations with numeric arrays */
 public final class VectorUtil {
 
@@ -112,15 +110,6 @@ public final class VectorUtil {
       squareSum += diff * diff;
     }
     return squareSum;
-  }
-
-  public static float[] randomVector(Random random, int dim) {
-    float[] vec = new float[dim];
-    for (int i = 0; i < dim; i++) {
-      vec[i] = random.nextFloat();
-    }
-    VectorUtil.l2normalize(vec);
-    return vec;
   }
 
   /**
