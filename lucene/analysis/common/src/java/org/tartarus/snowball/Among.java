c575f493e0d826cd596d9ec570ea2aaa737b73f3
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-04-12 20:58:42 +0200

    This applies spotless to generated snowball classes (and recomputes checksums). I think it's much saner to look at.

--- lucene/analysis/common/src/java/org/tartarus/snowball/Among.java
+++ lucene/analysis/common/src/java/org/tartarus/snowball/Among.java
@@ -36,32 +36,36 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.Locale;
 
-/**
- * Internal class used by Snowball stemmers
- */
+/** Internal class used by Snowball stemmers */
 public class Among {
-  public Among (String s, int substring_i, int result) {
+  public Among(String s, int substring_i, int result) {
     this.s = s.toCharArray();
     this.substring_i = substring_i;
     this.result = result;
     this.method = null;
   }
 
-  public Among (String s, int substring_i, int result, String methodname,
-          MethodHandles.Lookup methodobject) {
+  public Among(
+      String s, int substring_i, int result, String methodname, MethodHandles.Lookup methodobject) {
     this.s = s.toCharArray();
     this.substring_i = substring_i;
     this.result = result;
-    final Class<? extends SnowballProgram> clazz = methodobject.lookupClass().asSubclass(SnowballProgram.class);
+    final Class<? extends SnowballProgram> clazz =
+        methodobject.lookupClass().asSubclass(SnowballProgram.class);
     if (methodname.length() > 0) {
       try {
-        this.method = methodobject.findVirtual(clazz, methodname, MethodType.methodType(boolean.class))
-          .asType(MethodType.methodType(boolean.class, SnowballProgram.class));
+        this.method =
+            methodobject
+                .findVirtual(clazz, methodname, MethodType.methodType(boolean.class))
+                .asType(MethodType.methodType(boolean.class, SnowballProgram.class));
       } catch (NoSuchMethodException | IllegalAccessException e) {
-        throw new RuntimeException(String.format(Locale.ENGLISH,
-          "Snowball program '%s' is broken, cannot access method: boolean %s()",
-          clazz.getSimpleName(), methodname
-        ), e);
+        throw new RuntimeException(
+            String.format(
+                Locale.ENGLISH,
+                "Snowball program '%s' is broken, cannot access method: boolean %s()",
+                clazz.getSimpleName(),
+                methodname),
+            e);
       }
     } else {
       this.method = null;
@@ -74,4 +78,5 @@ public class Among {
 
   // Make sure this is not accessible outside package for Java security reasons!
   final MethodHandle method; /* method to use if substring matches */
-};
+}
+;
--- lucene/analysis/common/src/java/org/tartarus/snowball/SnowballStemmer.java
+++ lucene/analysis/common/src/java/org/tartarus/snowball/SnowballStemmer.java
@@ -32,11 +32,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package org.tartarus.snowball;
 
-/**
- * Parent class of all snowball stemmers, which must implement <code>stem</code>
- */
+/** Parent class of all snowball stemmers, which must implement <code>stem</code> */
 public abstract class SnowballStemmer extends SnowballProgram {
   public abstract boolean stem();
 
   static final long serialVersionUID = 2016072500L;
-};
+}
+;
