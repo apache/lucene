9a346e3739647b4cecc16634ea92448c95a0cd23
Mayya Sharipova <mayya.sharipova@elastic.co>
2021-04-15 11:43:29 -0400

    Temporarily mute TestLucene50TermVectorsFormat:testMerge (#86)
    Relates to #11

--- lucene/backward-codecs/src/test/org/apache/lucene/backward_codecs/lucene50/TestLucene50TermVectorsFormat.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/backward_codecs/lucene50/TestLucene50TermVectorsFormat.java
@@ -16,6 +16,7 @@
  */
 package org.apache.lucene.backward_codecs.lucene50;
 
+import java.io.IOException;
 import org.apache.lucene.backward_codecs.lucene87.Lucene87RWCodec;
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.index.BaseTermVectorsFormatTestCase;
@@ -25,5 +26,11 @@ public class TestLucene50TermVectorsFormat extends BaseTermVectorsFormatTestCase
   @Override
   protected Codec getCodec() {
     return new Lucene87RWCodec();
+  }
+
+  @Override
+  @AwaitsFix(bugUrl = "https://issues.apache.org/jira/browse/LUCENE-9334")
+  public void testMerge() throws IOException {
+    super.testMerge();
   }
 }
