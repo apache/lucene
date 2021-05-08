2092e90363f69dbc0220a797bd82c2ff54c2265d
Zach Chen <zacharymorn@yahoo.com>
2021-05-08 14:53:44 -0700

    update clear

--- lucene/core/src/java/org/apache/lucene/search/DisiPriorityQueue.java
+++ lucene/core/src/java/org/apache/lucene/search/DisiPriorityQueue.java
@@ -125,10 +125,10 @@ public final class DisiPriorityQueue implements Iterable<DisiWrapper> {
 
   /** Clear the heap. */
   void clear() {
-    size = 0;
     for (int i = 0; i < size; ++i) {
       heap[i] = null;
     }
+    size = 0;
   }
 
   void upHeap(int i) {
