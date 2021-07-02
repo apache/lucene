f4de580dbf0631223e7f6cc2670f4f5a043e017e
Christine Poerschke <cpoerschke@apache.org>
2021-07-02 19:01:21 +0100

    spotless

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/miscellaneous/WordDelimiterGraphFilter.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/miscellaneous/WordDelimiterGraphFilter.java
@@ -36,8 +36,8 @@ import org.apache.lucene.util.RamUsageEstimator;
 /**
  * Splits words into subwords and performs optional transformations on subword groups, producing a
  * correct token graph so that e.g. {@link PhraseQuery} can work correctly when this filter is used
- * in the search-time analyzer. Unlike the now removed <code>WordDelimiterFilter</code>, this token filter
- * produces a correct token graph as output. However, it cannot consume an input token graph
+ * in the search-time analyzer. Unlike the now removed <code>WordDelimiterFilter</code>, this token
+ * filter produces a correct token graph as output. However, it cannot consume an input token graph
  * correctly. Processing is suppressed by {@link KeywordAttribute#isKeyword()}=true.
  *
  * <p>Words are split into subwords with the following rules:
