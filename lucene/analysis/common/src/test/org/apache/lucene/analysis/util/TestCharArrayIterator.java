982cb60fc535e522bf0cf3c59eaa3b0839995115
David Smiley <dsmiley@apache.org>
2021-07-06 13:54:04 -0400

    Hand edits

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/util/TestCharArrayIterator.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/util/TestCharArrayIterator.java
@@ -45,7 +45,7 @@ public class TestCharArrayIterator extends LuceneTestCase {
     BreakIterator bi = BreakIterator.getWordInstance(Locale.getDefault());
     Segment ci = new Segment();
     for (int i = 0; i < 10000; i++) {
-      char text[] = _TestUtil.randomUnicodeString(random).toCharArray();
+      char[] text = _TestUtil.randomUnicodeString(random).toCharArray();
       ci.array = text;
       ci.offset = 0;
       ci.count = text.length;
@@ -75,7 +75,7 @@ public class TestCharArrayIterator extends LuceneTestCase {
     BreakIterator bi = BreakIterator.getSentenceInstance(Locale.getDefault());
     Segment ci = new Segment();
     for (int i = 0; i < 10000; i++) {
-      char text[] = _TestUtil.randomUnicodeString(random).toCharArray();
+      char[] text = _TestUtil.randomUnicodeString(random).toCharArray();
       ci.array = text;
       ci.offset = 0;
       ci.count = text.length;
