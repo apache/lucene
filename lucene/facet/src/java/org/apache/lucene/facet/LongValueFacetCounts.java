ba2846cd7d9acace614c04c418f17d0f1bcf079d
Patrick Zhai <zhai7631@gmail.com>
2021-07-09 12:31:15 -0700

    LUCENE-10021: Upgrade HPPC to 0.9.0. Replace usage of ...ScatterMap to ...HashMap

--- lucene/facet/src/java/org/apache/lucene/facet/LongValueFacetCounts.java
+++ lucene/facet/src/java/org/apache/lucene/facet/LongValueFacetCounts.java
@@ -17,7 +17,7 @@
 
 package org.apache.lucene.facet;
 
-import com.carrotsearch.hppc.LongIntScatterMap;
+import com.carrotsearch.hppc.LongIntHashMap;
 import com.carrotsearch.hppc.cursors.LongIntCursor;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -52,7 +52,7 @@ public class LongValueFacetCounts extends Facets {
   private final int[] counts = new int[1024];
 
   /** Used for all values that are >= 1K. */
-  private final LongIntScatterMap hashCounts = new LongIntScatterMap();
+  private final LongIntHashMap hashCounts = new LongIntHashMap();
 
   /** Field being counted. */
   private final String field;
