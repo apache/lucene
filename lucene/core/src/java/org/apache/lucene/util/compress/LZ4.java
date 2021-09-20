2d0291ac57a39e935d89a6725a92a08837f7f378
Uwe Schindler <uschindler@apache.org>
2021-09-20 16:10:50 +0200

    Tidy code, add CHANGES.txt

--- lucene/core/src/java/org/apache/lucene/util/compress/LZ4.java
+++ lucene/core/src/java/org/apache/lucene/util/compress/LZ4.java
@@ -66,7 +66,7 @@ public final class LZ4 {
   private static int readInt(byte[] buf, int i) {
     // we hardcode LITTLE ENDIAN here as this is most performant on most platforms.
     // According to LZ4's alogrithm the endianness does not matter at all, but we
-    // want to prevent indexes to differ just because of platform endianness! 
+    // want to prevent indexes to differ just because of platform endianness!
     return (int) BitUtil.VH_LE_INT.get(buf, i);
   }
 
