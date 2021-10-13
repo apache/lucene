03cbc519e4e0fa8094b10f01c336fa016a375115
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-10-13 10:35:09 +0200

    LUCENE-10166: removed module-level README.txt and modified a few links, removed a few obsolete instructions from 20 years ago.

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/de/GermanNormalizationFilter.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/de/GermanNormalizationFilter.java
@@ -24,8 +24,8 @@ import org.apache.lucene.analysis.util.StemmerUtil;
 
 /**
  * Normalizes German characters according to the heuristics of the <a
- * href="http://snowball.tartarus.org/algorithms/german2/stemmer.html">German2 snowball
- * algorithm</a>. It allows for the fact that ä, ö and ü are sometimes written as ae, oe and ue.
+ * href="https://snowballstem.org/algorithms/german2/stemmer.html">German2 snowball algorithm</a>.
+ * It allows for the fact that ä, ö and ü are sometimes written as ae, oe and ue.
  *
  * <ul>
  *   <li>'ß' is replaced by 'ss'
