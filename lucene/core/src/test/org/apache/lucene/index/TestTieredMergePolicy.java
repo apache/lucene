0538c2c0dac3a0f4a0f9ea3f1c5ab2ac7ad8d008
Mike McCandless <mikemccand@apache.org>
2021-10-18 11:08:10 -0400

    LUCENE-10093: remove unused import

--- lucene/core/src/test/org/apache/lucene/index/TestTieredMergePolicy.java
+++ lucene/core/src/test/org/apache/lucene/index/TestTieredMergePolicy.java
@@ -17,7 +17,6 @@
 package org.apache.lucene.index;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
