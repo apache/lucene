593562b0a718e3927624e05454ad6d15f4ec897e
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-11-12 19:55:28 +0100

    experiments with luke.

---
+++ lucene/luke/src/java/module-info.java
@@ -0,0 +1,9 @@
+@SuppressWarnings("requires-automatic")
+module org.apache.lucene.luke {
+  requires java.desktop;
+  requires org.apache.logging.log4j;
+  requires lucene.core;
+// requires lucene.codecs;
+
+
+}
