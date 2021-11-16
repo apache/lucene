f90ffd2cba70bd99bedf59dc578afffc79594f35
Anand Kotriwal <anakot@amazon.com>
2021-11-16 16:24:18 +0530

    LUCENE-10237 : MergeOnCommitTieredMergePolicy to sandbox

---
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/index/MergeOnCommitTieredMergePolicy.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.lucene.sandbox.index;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.lucene.index.MergeTrigger;
+import org.apache.lucene.index.SegmentCommitInfo;
+import org.apache.lucene.index.SegmentInfos;
+import org.apache.lucene.index.TieredMergePolicy;
+
+/**
+ * A simple extension to {@link TieredMergePolicy} to merge all tiny segments (or at least segments
+ * smaller than specified in setSmallSegmentThresholdMB) into one segment on commit.
+ */
+public class MergeOnCommitTieredMergePolicy extends TieredMergePolicy {
+  private double maxSegmentSizeAsProportionOfIndex = 1.0;
+  private long smallSegmentThresholdBytes = Units.mbToBytes(100.0);
+  private long hardMaxSegmentSizeBytes = 5 * 1024 * 1024 * 1024L;
+
+  private static long indexSize(SegmentInfos infos) throws IOException {
+    long totalSize = 0;
+    for (SegmentCommitInfo sci : infos) {
+      totalSize += sci.sizeInBytes();
+    }
+    return totalSize;
+  }
+
+  public double getSmallSegmentThresholdMB() {
+    return Units.bytesToMB(smallSegmentThresholdBytes);
+  }
+
+  /**
+   * @param smallSegmentThresholdMB all segments smaller than this will be merged into a single
+   *     segment before commit completes.
+   */
+  public void setSmallSegmentThresholdMB(double smallSegmentThresholdMB) {
+    this.smallSegmentThresholdBytes = Units.mbToBytes(smallSegmentThresholdMB);
+  }
+
+  public double getMaxSegmentSizeAsProportionOfIndex() {
+    return maxSegmentSizeAsProportionOfIndex;
+  }
+
+  public void setMaxSegmentSizeAsProportionOfIndex(double maxSegmentSizeAsProportionOfIndex) {
+    if (maxSegmentSizeAsProportionOfIndex < 0.0 || maxSegmentSizeAsProportionOfIndex > 1.0) {
+      throw new IllegalArgumentException(
+          "maxSegmentSizeAsProportionOfIndex must be between 0 and 1; got: "
+              + maxSegmentSizeAsProportionOfIndex);
+    }
+    this.maxSegmentSizeAsProportionOfIndex = maxSegmentSizeAsProportionOfIndex;
+  }
+
+  @Override
+  public TieredMergePolicy setMaxMergedSegmentMB(double v) {
+    this.hardMaxSegmentSizeBytes = Units.mbToBytes(v);
+    return this;
+  }
+
+  @Override
+  public MergeSpecification findMerges(
+      MergeTrigger mergeTrigger, SegmentInfos infos, MergeContext mergeContext) throws IOException {
+    double maxSegmentSizeMB =
+        Units.bytesToMB(
+            Math.min(
+                hardMaxSegmentSizeBytes,
+                Math.max(
+                    Units.mbToBytes(getFloorSegmentMB()),
+                    (long) (getMaxSegmentSizeAsProportionOfIndex() * indexSize(infos)))));
+    super.setMaxMergedSegmentMB(maxSegmentSizeMB);
+    return super.findMerges(mergeTrigger, excludeSmallSegments(infos), mergeContext);
+  }
+
+  /**
+   * Returns a SegmentInfos with all segments smaller than smallSegmentThresholdBytes removed.
+   *
+   * @param realSegmentInfos the "real", unfiltered {@link SegmentInfos}
+   * @return a new {@link SegmentInfos} instance containing all {@link SegmentCommitInfo} from
+   *     realSegmentInfos that are at least as large as smallSegmentThresholdBytes.
+   * @throws IOException if unable to load the size of a {@link SegmentCommitInfo}
+   */
+  private SegmentInfos excludeSmallSegments(SegmentInfos realSegmentInfos) throws IOException {
+    SegmentInfos largeSegmentInfos =
+        new SegmentInfos(realSegmentInfos.getIndexCreatedVersionMajor());
+    for (SegmentCommitInfo sci : realSegmentInfos.asList()) {
+      if (sci.sizeInBytes() >= smallSegmentThresholdBytes) {
+        largeSegmentInfos.add(sci);
+      }
+    }
+    return largeSegmentInfos;
+  }
+
+  @Override
+  public MergeSpecification findFullFlushMerges(
+      MergeTrigger mergeTrigger, SegmentInfos segmentInfos, MergeContext mergeContext)
+      throws IOException {
+    List<SegmentCommitInfo> smallSegments = new ArrayList<>();
+    for (SegmentCommitInfo sci : segmentInfos) {
+      if (sci.sizeInBytes() < smallSegmentThresholdBytes) {
+        if (mergeContext.getMergingSegments().contains(sci) == false) {
+          smallSegments.add(sci);
+        }
+      }
+    }
+    if (smallSegments.size() > 1) {
+      MergeSpecification mergeSpecification = new MergeSpecification();
+      mergeSpecification.add(new OneMerge(smallSegments));
+      return mergeSpecification;
+    }
+    return null;
+  }
+  /** Utility class to handle conversion between megabytes and bytes */
+  public static class Units {
+    public static double bytesToMB(long bytes) {
+      return bytes / 1024. / 1024.;
+    }
+
+    public static long mbToBytes(double megabytes) {
+      return (long) (megabytes * 1024 * 1024);
+    }
+  }
+}
---
+++ lucene/sandbox/src/test/org/apache/lucene/sandbox/index/TestMergeOnCommitTieredMergePolicy.java
@@ -0,0 +1,304 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.lucene.sandbox.index;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import org.apache.lucene.index.BaseMergePolicyTestCase;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.MergePolicy;
+import org.apache.lucene.index.MergeTrigger;
+import org.apache.lucene.index.SegmentCommitInfo;
+import org.apache.lucene.index.SegmentInfos;
+import org.apache.lucene.index.TieredMergePolicy;
+import org.apache.lucene.util.InfoStream;
+import org.apache.lucene.util.NullInfoStream;
+import org.apache.lucene.util.TestUtil;
+import org.apache.lucene.util.Version;
+import org.junit.Ignore;
+
+/** Test for {@link org.apache.lucene.sandbox.index.MergeOnCommitTieredMergePolicy}. */
+public class TestMergeOnCommitTieredMergePolicy extends BaseMergePolicyTestCase {
+
+  private static long getIndexSize(SegmentInfos segmentInfos) throws IOException {
+    long totalBytes = 0;
+    for (SegmentCommitInfo sci : segmentInfos) {
+      totalBytes += sci.sizeInBytes();
+    }
+    return totalBytes;
+  }
+
+  private static long estimateMergeSize(
+      MergePolicy.OneMerge merge, MergePolicy.MergeContext mergeContext) throws IOException {
+    long estimatedMergeBytes = 0;
+    for (SegmentCommitInfo info : merge.segments) {
+      if (info.info.maxDoc() > 0) {
+        final int delCount = mergeContext.numDeletedDocs(info);
+        assert delCount <= info.info.maxDoc();
+        final double delRatio = ((double) delCount) / info.info.maxDoc();
+        estimatedMergeBytes += info.sizeInBytes() * (1.0 - delRatio);
+      }
+    }
+    return estimatedMergeBytes;
+  }
+
+  @Override
+  protected MergePolicy mergePolicy() {
+    Random r = random();
+    TieredMergePolicy tieredMergePolicy = newTieredMergePolicy(r);
+    MergeOnCommitTieredMergePolicy mergePolicy = new MergeOnCommitTieredMergePolicy();
+    mergePolicy.setDeletesPctAllowed(tieredMergePolicy.getDeletesPctAllowed());
+    mergePolicy.setFloorSegmentMB(tieredMergePolicy.getFloorSegmentMB());
+    mergePolicy.setForceMergeDeletesPctAllowed(tieredMergePolicy.getForceMergeDeletesPctAllowed());
+    mergePolicy.setMaxCFSSegmentSizeMB(tieredMergePolicy.getMaxCFSSegmentSizeMB());
+    mergePolicy.setMaxMergeAtOnce(tieredMergePolicy.getMaxMergeAtOnce());
+    mergePolicy.setMaxMergedSegmentMB(tieredMergePolicy.getMaxMergedSegmentMB());
+    mergePolicy.setSegmentsPerTier(tieredMergePolicy.getSegmentsPerTier());
+    mergePolicy.setNoCFSRatio(tieredMergePolicy.getNoCFSRatio());
+    mergePolicy.setSmallSegmentThresholdMB(TestUtil.nextInt(r, 1, 100));
+    return mergePolicy;
+  }
+
+  @Override
+  protected void assertSegmentInfos(MergePolicy policy, SegmentInfos infos) {}
+
+  // BaseMergePolicyTestCase#testForceMergeNotNeeded() calls #mergePolicy()
+  // which will eventually use MergeOnCommitTieredMergePolicy for running the test
+  // but MergeOnCommitTieredMergePolicy expects TreeMetrics to be set
+  @Override
+  public void testForceMergeNotNeeded() throws IOException {
+    super.testForceMergeNotNeeded();
+  }
+
+  @Override
+  protected void assertMerge(MergePolicy policy, MergePolicy.MergeSpecification merge)
+      throws IOException {
+    MergeOnCommitTieredMergePolicy mergePolicy = (MergeOnCommitTieredMergePolicy) policy;
+    for (MergePolicy.OneMerge oneMerge : merge.merges) {
+      for (SegmentCommitInfo sci : oneMerge.segments) {
+        assertTrue(
+            "Basic merge should not merge small segments",
+            sci.sizeInBytes()
+                >= MergeOnCommitTieredMergePolicy.Units.mbToBytes(
+                    mergePolicy.getSmallSegmentThresholdMB()));
+      }
+    }
+  }
+
+  public void testRegularMerge() throws IOException {
+    MergeOnCommitTieredMergePolicy mergePolicy = (MergeOnCommitTieredMergePolicy) mergePolicy();
+    double smallSegmentThresholdMB = mergePolicy.getSmallSegmentThresholdMB();
+    Random r = random();
+
+    for (int j = 0; j < 10_000; j++) {
+      SegmentInfos segmentInfos = new SegmentInfos(Version.LATEST.major);
+      int numSegs = random().nextInt(50);
+      Set<SegmentCommitInfo> mergingSegments = new HashSet<>();
+      Set<SegmentCommitInfo> smallSegments = new HashSet<>();
+      for (int i = 0; i < numSegs; i++) {
+        SegmentCommitInfo sci =
+            makeSegmentCommitInfo(
+                "_" + i,
+                TestUtil.nextInt(r, 10, 100),
+                r.nextInt(10),
+                r.nextDouble() * 2.0 * smallSegmentThresholdMB,
+                IndexWriter.SOURCE_FLUSH);
+        if (sci.sizeInBytes()
+            < MergeOnCommitTieredMergePolicy.Units.mbToBytes(smallSegmentThresholdMB)) {
+          smallSegments.add(sci);
+        }
+        if (r.nextBoolean()) {
+          mergingSegments.add(sci);
+        }
+        segmentInfos.add(sci);
+      }
+      MergePolicy.MergeContext context = new MergingMockMergeContext(mergingSegments);
+      MergePolicy.MergeSpecification mergeSpecification =
+          mergePolicy.findMerges(MergeTrigger.FULL_FLUSH, segmentInfos, context);
+      if (mergeSpecification != null) {
+        for (MergePolicy.OneMerge oneMerge : mergeSpecification.merges) {
+          for (SegmentCommitInfo sci : oneMerge.segments) {
+            assertFalse("Merges must not contain small segments", smallSegments.contains(sci));
+          }
+        }
+      }
+    }
+  }
+
+  public void testOnlySmallSegmentsInRegularMerge() throws IOException {
+    MergeOnCommitTieredMergePolicy mergePolicy = (MergeOnCommitTieredMergePolicy) mergePolicy();
+    double smallSegmentThresholdMB = mergePolicy.getSmallSegmentThresholdMB();
+    SegmentInfos segmentInfos = new SegmentInfos(Version.LATEST.major);
+    Random r = random();
+
+    int numSegs = random().nextInt(10);
+    for (int i = 0; i < numSegs; i++) {
+      segmentInfos.add(
+          makeSegmentCommitInfo(
+              "_" + i,
+              TestUtil.nextInt(r, 10, 100),
+              r.nextInt(10),
+              r.nextDouble() * smallSegmentThresholdMB,
+              IndexWriter.SOURCE_FLUSH));
+    }
+    MergePolicy.MergeContext context = new MockMergeContext(s -> 0);
+    // We shouldn't find any merges.
+    assertNull(mergePolicy.findMerges(MergeTrigger.EXPLICIT, segmentInfos, context));
+  }
+
+  public void testFindCommitMerges() throws IOException {
+    MergeOnCommitTieredMergePolicy mergePolicy = (MergeOnCommitTieredMergePolicy) mergePolicy();
+    double smallSegmentThresholdMB = mergePolicy.getSmallSegmentThresholdMB();
+    Random r = random();
+
+    for (int j = 0; j < 10_000; j++) {
+      SegmentInfos segmentInfos = new SegmentInfos(Version.LATEST.major);
+      int numSegs = random().nextInt(50);
+      Set<SegmentCommitInfo> mergingSegments = new HashSet<>();
+      Set<SegmentCommitInfo> smallSegments = new HashSet<>();
+      for (int i = 0; i < numSegs; i++) {
+        SegmentCommitInfo sci =
+            makeSegmentCommitInfo(
+                "_" + i,
+                TestUtil.nextInt(r, 10, 100),
+                r.nextInt(10),
+                r.nextDouble() * 2.0 * smallSegmentThresholdMB,
+                IndexWriter.SOURCE_FLUSH);
+        if (sci.sizeInBytes()
+            < MergeOnCommitTieredMergePolicy.Units.mbToBytes(smallSegmentThresholdMB)) {
+          smallSegments.add(sci);
+        }
+        if (r.nextBoolean()) {
+          mergingSegments.add(sci);
+        }
+        segmentInfos.add(sci);
+      }
+      MergePolicy.MergeContext context = new MergingMockMergeContext(mergingSegments);
+      MergePolicy.MergeSpecification mergeSpecification;
+
+      mergeSpecification =
+          mergePolicy.findFullFlushMerges(MergeTrigger.COMMIT, segmentInfos, context);
+
+      if (mergeSpecification == null) {
+        // If we didn't compute a merge, then we have at most one small segment not already
+        // participating in a merge.
+        boolean foundNonMergingSmallSegment = false;
+        for (SegmentCommitInfo smallSegment : smallSegments) {
+          if (!mergingSegments.contains(smallSegment)) {
+            assertFalse(
+                "If no merges, then at most one small segment is not already merging",
+                foundNonMergingSmallSegment);
+            foundNonMergingSmallSegment = true;
+          }
+        }
+      } else {
+        for (MergePolicy.OneMerge oneMerge : mergeSpecification.merges) {
+          for (SegmentCommitInfo sci : oneMerge.segments) {
+            assertTrue("Merges only contain small segments", smallSegments.contains(sci));
+            assertFalse(
+                "findFullFlushMerges must not return already merging segments",
+                mergingSegments.contains(sci));
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  @Ignore // This test takes > 2 minutes on a 2015 Macbook Pro
+  public void testSimulateUpdates() throws IOException {
+    super.testSimulateUpdates();
+  }
+
+  @Override
+  @Ignore // This test takes > 2 minutes on a 2015 Macbook Pro
+  public void testSimulateAppendOnly() throws IOException {
+    super.testSimulateAppendOnly();
+  }
+
+  public void testMaxSegmentSizeAsProportionOfIndexSize() throws IOException {
+    Random r = random();
+    MergeOnCommitTieredMergePolicy mergePolicy = (MergeOnCommitTieredMergePolicy) mergePolicy();
+    final double maxSegmentSizeAsProportionOfIndex = r.nextDouble();
+    mergePolicy.setMaxSegmentSizeAsProportionOfIndex(maxSegmentSizeAsProportionOfIndex);
+    mergePolicy.setMaxMergedSegmentMB(r.nextInt(20) + 1);
+    SegmentInfos segmentInfos = new SegmentInfos(Version.LATEST.major);
+
+    int numSegs = random().nextInt(20);
+    for (int i = 0; i < numSegs; i++) {
+      segmentInfos.add(
+          makeSegmentCommitInfo(
+              "_" + i,
+              TestUtil.nextInt(r, 10, 100),
+              r.nextInt(10),
+              r.nextInt(30),
+              IndexWriter.SOURCE_FLUSH));
+    }
+    long indexSize = getIndexSize(segmentInfos);
+    MergePolicy.MergeContext context = new MockMergeContext(s -> 0);
+
+    MergePolicy.MergeSpecification mergeSpecification =
+        mergePolicy.findMerges(MergeTrigger.EXPLICIT, segmentInfos, context);
+
+    if (mergeSpecification != null) {
+      for (MergePolicy.OneMerge oneMerge : mergeSpecification.merges) {
+        assertTrue(
+            estimateMergeSize(oneMerge, context) <= maxSegmentSizeAsProportionOfIndex * indexSize);
+      }
+    }
+  }
+
+  /** Mock merge context that can have merging segments. */
+  private static final class MergingMockMergeContext implements MergePolicy.MergeContext {
+    private final Set<SegmentCommitInfo> mergingSegments;
+    private final InfoStream infoStream =
+        new NullInfoStream() {
+          @Override
+          public boolean isEnabled(String component) {
+            // otherwise tests that simulate merging may bottleneck on generating messages
+            return false;
+          }
+        };
+
+    private MergingMockMergeContext(Set<SegmentCommitInfo> mergingSegments) {
+      this.mergingSegments = mergingSegments;
+    }
+
+    @Override
+    public int numDeletesToMerge(SegmentCommitInfo info) {
+      return 0;
+    }
+
+    @Override
+    public int numDeletedDocs(SegmentCommitInfo info) {
+      return 0;
+    }
+
+    @Override
+    public InfoStream getInfoStream() {
+      return infoStream;
+    }
+
+    @Override
+    public Set<SegmentCommitInfo> getMergingSegments() {
+      return mergingSegments;
+    }
+  }
+}
