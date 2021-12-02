8372dbd15aa1a6aa7f50e4d4bba857e9599a8e88
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-12-02 20:40:47 +0100

    LUCENE-10277: check consistency of modular vs. classpath service layer.

--- lucene/distribution-tests/src/test/org/apache/lucene/distribution/TestModularLayer.java
+++ lucene/distribution-tests/src/test/org/apache/lucene/distribution/TestModularLayer.java
@@ -16,31 +16,63 @@
  */
 package org.apache.lucene.distribution;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReader;
 import java.lang.module.ModuleReference;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Assumptions;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/** Sanity checks concerning the distribution's binary artifacts (modules). */
+/**
+ * Sanity checks concerning the distribution's binary artifacts (modules).
+ *
+ * <p>We do <em>not</em> want this module to depend on any Lucene classes (including the test
+ * framework) so that there is no risk of accidental classpath space pollution. This also means the
+ * default {@code LuceneTestCase} configuration setup is not used (you have to annotate test for
+ * JUnit, for example).
+ */
 public class TestModularLayer {
-  /**
-   * A path to a directory with Lucene distribution's modules. This property is passed by gradle
-   * build (but can be provided manually if you run from an IDE).
-   */
-  private static String MODULES_PROPERTY = "lucene.distribution.modules";
+  /** A path to a directory with Lucene distribution's modules. */
+  private static final String MODULES_PROPERTY = "lucene.distribution.modules";
 
   /** The expected distribution version of Lucene modules. */
-  private static String VERSION_PROPERTY = "lucene.distribution.version";
+  private static final String VERSION_PROPERTY = "lucene.distribution.version";
 
   /** Resolved modules from {@link #MODULES_PROPERTY}. */
   private static Set<ModuleReference> allModules;
 
+  /** Ensure Lucene classes are not visible. */
+  @BeforeClass
+  public static void checkLuceneNotInClasspath() {
+    Assertions.assertThatThrownBy(
+            () -> {
+              Class.forName("org.apache.lucene.index.IndexWriter");
+            })
+        .isInstanceOf(ClassNotFoundException.class);
+  }
+
+  /**
+   * We accept external properties that point to the assembled set of distribution modules and to
+   * their expected version. These properties are collected and passed by gradle but can be provided
+   * manually (for IDE launches).
+   */
   @BeforeClass
   public static void checkModulePathProvided() {
     String modulesPropertyValue = System.getProperty(MODULES_PROPERTY);
@@ -107,5 +139,77 @@ public class TestModularLayer {
           .as("Version of module: " + module.descriptor().name())
           .isEqualTo(luceneBuildVersion);
     }
+  }
+
+  /** Ensure SPIs are equal for the module and classpath layer. */
+  @Test
+  public void testModularAndClasspathProvidersAreConsistent() throws IOException {
+    for (var module : allModules) {
+      TreeMap<String, TreeSet<String>> modularProviders = getModularServiceProviders(module);
+      TreeMap<String, TreeSet<String>> classpathProviders = getClasspathServiceProviders(module);
+
+      // Compare services first so that the exception is shorter.
+      Assertions.assertThat(modularProviders.keySet())
+          .as("Modular services in module: " + module.descriptor().name())
+          .containsAll(classpathProviders.keySet());
+
+      // We're sure the services correspond to each other. Now, for each service, compare the
+      // providers.
+      for (var service : modularProviders.keySet()) {
+        Assertions.assertThat(modularProviders.get(service))
+            .as(
+                "Modular providers of service "
+                    + service
+                    + " in module: "
+                    + module.descriptor().name())
+            .containsAll(classpathProviders.get(service));
+      }
+    }
+  }
+
+  private TreeMap<String, TreeSet<String>> getClasspathServiceProviders(ModuleReference module)
+      throws IOException {
+    TreeMap<String, TreeSet<String>> services = new TreeMap<>();
+    Pattern serviceEntryPattern = Pattern.compile("META-INF/services/(?<serviceName>.+)");
+    try (ModuleReader reader = module.open();
+        Stream<String> entryStream = reader.list()) {
+      List<String> serviceProviderEntryList =
+          entryStream
+              .filter(entry -> serviceEntryPattern.matcher(entry).find())
+              .collect(Collectors.toList());
+
+      for (String entry : serviceProviderEntryList) {
+        List<String> implementations;
+        try (InputStream is = reader.open(entry).get()) {
+          implementations =
+              Arrays.stream(new String(is.readAllBytes(), StandardCharsets.UTF_8).split("\n"))
+                  .map(String::trim)
+                  .filter(line -> !line.isBlank() && !line.startsWith("#"))
+                  .collect(Collectors.toList());
+        }
+
+        Matcher matcher = serviceEntryPattern.matcher(entry);
+        if (!matcher.find()) {
+          throw new AssertionError("Impossible.");
+        }
+        String service = matcher.group("serviceName");
+        services.computeIfAbsent(service, k -> new TreeSet<>()).addAll(implementations);
+      }
+    }
+
+    return services;
+  }
+
+  private static TreeMap<String, TreeSet<String>> getModularServiceProviders(
+      ModuleReference module) {
+    return module.descriptor().provides().stream()
+        .collect(
+            Collectors.toMap(
+                ModuleDescriptor.Provides::service,
+                provides -> new TreeSet<>(provides.providers()),
+                (k, v) -> {
+                  throw new RuntimeException();
+                },
+                TreeMap::new));
   }
 }
