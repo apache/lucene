fd1315ccc9ff54a649cd339dc6af3e328934a872
guofeng.my <guofeng.my@bytedance.com>
2021-12-03 20:24:49 +0800

    fix

--- lucene/core/src/test/org/apache/lucene/util/TestFixedBitSet.java
+++ lucene/core/src/test/org/apache/lucene/util/TestFixedBitSet.java
@@ -455,7 +455,7 @@ public class TestFixedBitSet extends BaseBitSetTestCase<FixedBitSet> {
     int count1 = TestUtil.nextInt(random, 0, numBits1 - 1);
     int count2 = TestUtil.nextInt(random, 0, numBits2 - 1);
 
-    int min = TestUtil.nextInt(random, 0, count1);
+    int min = TestUtil.nextInt(random, 0, numBits1 - 1);
     int offSetWord1 = min >> 6;
     int offset1 = offSetWord1 << 6;
     int[] bits1 = makeIntArray(random, count1, min, numBits1 - 1);
