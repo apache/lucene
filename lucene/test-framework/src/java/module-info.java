66c108b31844d427e6546fc2adb3040eb541acd3
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-12-20 11:27:58 +0100

    Add module descriptor to the test framework.

---
+++ lucene/test-framework/src/java/module-info.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Lucene test framework. */
+@SuppressWarnings({"module", "requires-automatic"})
+module org.apache.lucene.test_framework {
+  requires org.apache.lucene.core;
+  requires org.apache.lucene.codecs;
+  requires java.logging;
+  requires junit;
+  requires randomizedtesting.runner;
+
+  exports org.apache.lucene.tests.analysis.standard;
+  exports org.apache.lucene.tests.analysis;
+  exports org.apache.lucene.tests.codecs.asserting;
+  exports org.apache.lucene.tests.codecs.blockterms;
+  exports org.apache.lucene.tests.codecs.bloom;
+  exports org.apache.lucene.tests.codecs.cheapbastard;
+  exports org.apache.lucene.tests.codecs.compressing.dummy;
+  exports org.apache.lucene.tests.codecs.compressing;
+  exports org.apache.lucene.tests.codecs.cranky;
+  exports org.apache.lucene.tests.codecs.mockrandom;
+  exports org.apache.lucene.tests.codecs.ramonly;
+  exports org.apache.lucene.tests.codecs.uniformsplit.sharedterms;
+  exports org.apache.lucene.tests.codecs.uniformsplit;
+  exports org.apache.lucene.tests.geo;
+  exports org.apache.lucene.tests.index;
+  exports org.apache.lucene.tests.mockfile;
+  exports org.apache.lucene.tests.search.similarities;
+  exports org.apache.lucene.tests.search;
+  exports org.apache.lucene.tests.store;
+  exports org.apache.lucene.tests.util.automaton;
+  exports org.apache.lucene.tests.util.fst;
+  exports org.apache.lucene.tests.util;
+}
