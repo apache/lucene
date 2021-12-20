838dacedb52efcf192fc5ece9041f049d127dcd7
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-12-20 10:14:10 +0100

    Add license.

--- lucene/core/src/java/org/apache/lucene/index/PackageSecrets.java
+++ lucene/core/src/java/org/apache/lucene/index/PackageSecrets.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.lucene.index;
 
 import org.apache.lucene.internal.tests.IndexPackageSecrets;
@@ -22,7 +38,7 @@ public final class PackageSecrets {
    * @return An instance of the secrets class; the return type is hidden from the public API.
    * @lucene.internal
    */
-  public static final Object getTestSecrets(Object accessToken) {
+  public static Object getTestSecrets(Object accessToken) {
     return new IndexPackageSecrets(accessToken) {
 
       @Override
