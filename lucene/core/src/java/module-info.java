864e2c3e8518f17dddcdf6bf35dad2bb5bb0cece
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2021-12-27 19:03:14 +0900

    update comment for module-info

--- lucene/core/src/java/module-info.java
+++ lucene/core/src/java/module-info.java
@@ -19,7 +19,7 @@
 @SuppressWarnings("module") // the test framework is compiled after the core...
 module org.apache.lucene.core {
   requires jdk.unsupported; // this is needed for MMapDirectory to unmap
-  requires static jdk.management; // this is needed to make RamUsageEstimator correctly works
+  requires static jdk.management; // this is optional but explicit declaration is recommended
 
   exports org.apache.lucene.analysis;
   exports org.apache.lucene.analysis.standard;
