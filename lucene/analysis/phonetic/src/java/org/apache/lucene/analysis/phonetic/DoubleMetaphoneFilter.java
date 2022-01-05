cf3e6c1ccc501e6165f125e167c11bb7c70642de
Uwe Schindler <uschindler@apache.org>
2022-01-05 15:26:45 +0100

    Final cleanup and changes entry

--- lucene/analysis/phonetic/src/java/org/apache/lucene/analysis/phonetic/DoubleMetaphoneFilter.java
+++ lucene/analysis/phonetic/src/java/org/apache/lucene/analysis/phonetic/DoubleMetaphoneFilter.java
@@ -39,8 +39,8 @@ public final class DoubleMetaphoneFilter extends TokenFilter {
    */
   public DoubleMetaphoneFilter(TokenStream input, int maxCodeLength, boolean inject) {
     super(input);
-    if (maxCodeLength <= 0) {
-      throw new IllegalArgumentException("maxCodeLength must be positive");
+    if (maxCodeLength < 1) {
+      throw new IllegalArgumentException("maxCodeLength must be >=1");
     }
     this.encoder.setMaxCodeLen(maxCodeLength);
     this.inject = inject;
