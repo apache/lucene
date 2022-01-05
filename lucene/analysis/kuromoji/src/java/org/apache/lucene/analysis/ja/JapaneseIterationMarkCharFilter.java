1f06f360133acd02cd531eaa95449472c252195d
Uwe Schindler <uschindler@apache.org>
2022-01-05 13:33:52 +0100

    fix formatting

--- lucene/analysis/kuromoji/src/java/org/apache/lucene/analysis/ja/JapaneseIterationMarkCharFilter.java
+++ lucene/analysis/kuromoji/src/java/org/apache/lucene/analysis/ja/JapaneseIterationMarkCharFilter.java
@@ -37,7 +37,8 @@ import org.apache.lucene.util.IgnoreRandomChains;
  * reached in order to not keep a copy of the character stream in memory. Vertical iteration marks,
  * which are even rarer than horizontal iteration marks in contemporary Japanese, are unsupported.
  */
-@IgnoreRandomChains(reason = "LUCENE-10358: fails with incorrect offsets or causes IndexOutOfBounds")
+@IgnoreRandomChains(
+    reason = "LUCENE-10358: fails with incorrect offsets or causes IndexOutOfBounds")
 public class JapaneseIterationMarkCharFilter extends CharFilter {
 
   /** Normalize kanji iteration marks by default */
