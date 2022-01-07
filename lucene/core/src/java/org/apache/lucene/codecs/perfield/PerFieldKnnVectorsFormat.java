d0ad9f5bfce54cd60b96eb2b7eec659497f9ec0b
zacharymorn <zacharymorn@yahoo.com>
2022-01-06 22:14:41 -0800

    LUCENE-10183: KnnVectorsWriter#writeField to take KnnVectorsReader instead of VectorValues (#534)

--- lucene/core/src/java/org/apache/lucene/codecs/perfield/PerFieldKnnVectorsFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/perfield/PerFieldKnnVectorsFormat.java
@@ -98,8 +98,9 @@ public abstract class PerFieldKnnVectorsFormat extends KnnVectorsFormat {
     }
 
     @Override
-    public void writeField(FieldInfo fieldInfo, VectorValues values) throws IOException {
-      getInstance(fieldInfo).writeField(fieldInfo, values);
+    public void writeField(FieldInfo fieldInfo, KnnVectorsReader knnVectorsReader)
+        throws IOException {
+      getInstance(fieldInfo).writeField(fieldInfo, knnVectorsReader);
     }
 
     @Override
