a82686c58e8d2ee707f69c44c4e35de88ba58c2d
Nikola Grcevski <nikola.grcevski@elastic.co>
2022-01-10 11:19:06 -0500

    Spotless

--- lucene/core/src/java/org/apache/lucene/index/LastingFilterCodecReader.java
+++ lucene/core/src/java/org/apache/lucene/index/LastingFilterCodecReader.java
@@ -17,20 +17,20 @@
 package org.apache.lucene.index;
 
 public abstract class LastingFilterCodecReader extends FilterCodecReader {
-    private final CacheHelper readerCacheHelper;
+  private final CacheHelper readerCacheHelper;
 
-    /**
-     * Creates a new FilterCodecReader.
-     *
-     * @param in the underlying CodecReader instance.
-     */
-    public LastingFilterCodecReader(CodecReader in) {
-        super(in);
-        this.readerCacheHelper = DelegatingCacheHelper.from(in.getReaderCacheHelper());
-    }
+  /**
+   * Creates a new FilterCodecReader.
+   *
+   * @param in the underlying CodecReader instance.
+   */
+  public LastingFilterCodecReader(CodecReader in) {
+    super(in);
+    this.readerCacheHelper = DelegatingCacheHelper.from(in.getReaderCacheHelper());
+  }
 
-    @Override
-    public CacheHelper getReaderCacheHelper() {
-        return readerCacheHelper;
-    }
+  @Override
+  public CacheHelper getReaderCacheHelper() {
+    return readerCacheHelper;
+  }
 }
--- lucene/core/src/java/org/apache/lucene/index/LastingFilterDirectoryReader.java
+++ lucene/core/src/java/org/apache/lucene/index/LastingFilterDirectoryReader.java
@@ -19,22 +19,23 @@ package org.apache.lucene.index;
 import java.io.IOException;
 
 public abstract class LastingFilterDirectoryReader extends FilterDirectoryReader {
-    protected final CacheHelper readerCacheHelper;
+  protected final CacheHelper readerCacheHelper;
 
-    /**
-     * Create a new FilterDirectoryReader that filters a passed in DirectoryReader, using the supplied
-     * SubReaderWrapper to wrap its subreader.
-     *
-     * @param in      the DirectoryReader to filter
-     * @param wrapper the SubReaderWrapper to use to wrap subreaders
-     */
-    public LastingFilterDirectoryReader(DirectoryReader in, SubReaderWrapper wrapper) throws IOException {
-        super(in, wrapper);
-        this.readerCacheHelper = DelegatingCacheHelper.from(in.getReaderCacheHelper());
-    }
+  /**
+   * Create a new FilterDirectoryReader that filters a passed in DirectoryReader, using the supplied
+   * SubReaderWrapper to wrap its subreader.
+   *
+   * @param in the DirectoryReader to filter
+   * @param wrapper the SubReaderWrapper to use to wrap subreaders
+   */
+  public LastingFilterDirectoryReader(DirectoryReader in, SubReaderWrapper wrapper)
+      throws IOException {
+    super(in, wrapper);
+    this.readerCacheHelper = DelegatingCacheHelper.from(in.getReaderCacheHelper());
+  }
 
-    @Override
-    public CacheHelper getReaderCacheHelper() {
-        return readerCacheHelper;
-    }
+  @Override
+  public CacheHelper getReaderCacheHelper() {
+    return readerCacheHelper;
+  }
 }
