5fae0b2f4ffc6deab48dd0f447f7d76587a328bf
guofeng.my <guofeng.my@bytedance.com>
2022-01-12 16:50:27 +0800

    use guard

--- lucene/core/src/java/org/apache/lucene/store/ByteBufferIndexInput.java
+++ lucene/core/src/java/org/apache/lucene/store/ByteBufferIndexInput.java
@@ -246,7 +246,23 @@ public abstract class ByteBufferIndexInput extends IndexInput implements RandomA
       // using #position instead of #mark here as #position is a final method
       int pos = curBuf.position();
       try {
-        return guard.getVInt(curBuf);
+        byte b = guard.getByte(curBuf);
+        if (b >= 0) return b;
+        int i = b & 0x7F;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7F) << 7;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7F) << 14;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7F) << 21;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        // Warning: the next ands use 0x0F / 0xF0 - beware copy/paste errors:
+        i |= (b & 0x0F) << 28;
+        if ((b & 0xF0) == 0) return i;
+        throw new IOException("Invalid vInt detected (too many bits)");
       } catch (
           @SuppressWarnings("unused")
           BufferUnderflowException e) {
@@ -266,7 +282,34 @@ public abstract class ByteBufferIndexInput extends IndexInput implements RandomA
       // using #position instead of #mark here as #position is a final method
       int pos = curBuf.position();
       try {
-        return guard.getVLong(curBuf);
+        byte b = guard.getByte(curBuf);
+        if (b >= 0) return b;
+        long i = b & 0x7FL;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 7;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 14;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 21;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 28;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 35;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 42;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 49;
+        if (b >= 0) return i;
+        b = guard.getByte(curBuf);
+        i |= (b & 0x7FL) << 56;
+        if (b >= 0) return i;
+        throw new IOException("Invalid vLong detected (negative values disallowed)");
       } catch (
           @SuppressWarnings("unused")
           BufferUnderflowException e) {
