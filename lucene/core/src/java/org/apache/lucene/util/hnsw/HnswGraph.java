188908e81919d2ea4e417d5cd19ab1dbf5c8ded9
Adrien Grand <jpountz@gmail.com>
2022-01-27 16:21:09 +0100

    iter

--- lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraph.java
+++ lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraph.java
@@ -168,8 +168,7 @@ public final class HnswGraph extends KnnGraphValues {
     }
 
     // A bound that holds the minimum similarity to the query vector that a candidate vector must
-    // have to be
-    // considered.
+    // have to be considered.
     BoundsChecker bound = BoundsChecker.create(similarityFunction.reversed);
     if (results.size() >= topK) {
       bound.set(results.topScore());
