27d727b388e955c173d6e403fe3f665cbadc08ff
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2022-02-09 21:42:03 +0900

    use existing o.a.l.util.IOSupplier

--- lucene/core/src/java/org/apache/lucene/util/IOUtils.java
+++ lucene/core/src/java/org/apache/lucene/util/IOUtils.java
@@ -526,17 +526,4 @@ public final class IOUtils {
   public interface IOFunction<T, R> {
     R apply(T t) throws IOException;
   }
-
-  /**
-   * A resource supplier function that may throw an IOException.
-   *
-   * <p>Note that this would open a resource such as a File. Consumers should make sure to close the
-   * resource (e.g., use try-with-resources)
-   *
-   * @see java.util.function.Supplier
-   */
-  @FunctionalInterface
-  public interface IOSupplier<T> {
-    T get() throws IOException;
-  }
 }
