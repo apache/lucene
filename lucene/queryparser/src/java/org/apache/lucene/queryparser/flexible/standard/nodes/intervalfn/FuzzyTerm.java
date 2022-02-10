9a293da5967ff272529a532106e64baecf28f24c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2022-02-10 12:18:13 +0100

    LUCENE-10414: Add fn:fuzzyTerm interval function to flexible query parser (#668)

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/flexible/standard/nodes/intervalfn/FuzzyTerm.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/flexible/standard/nodes/intervalfn/FuzzyTerm.java
@@ -16,41 +16,46 @@
  */
 package org.apache.lucene.queryparser.flexible.standard.nodes.intervalfn;
 
-import java.io.IOException;
-import java.util.regex.Pattern;
+import java.util.Locale;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.queries.intervals.Intervals;
 import org.apache.lucene.queries.intervals.IntervalsSource;
+import org.apache.lucene.search.FuzzyQuery;
 
-/** Node that represents {@link Intervals#analyzedText(String, Analyzer, String, int, boolean)}. */
-public class AnalyzedText extends IntervalFunction {
+/**
+ * An interval function equivalent to {@link FuzzyQuery}. A fuzzy term expands to a disjunction of
+ * intervals of terms that are within the specified {@code maxEdits} from the provided term. A limit
+ * of {@code maxExpansions} prevents the internal implementation from blowing up on too many
+ * potential candidate terms.
+ */
+public class FuzzyTerm extends IntervalFunction {
   private final String term;
+  private final int maxEdits;
+  private final Integer maxExpansions;
 
-  public AnalyzedText(String term) {
+  public FuzzyTerm(String term, Integer maxEdits, Integer maxExpansions) {
     this.term = term;
+    this.maxEdits = maxEdits == null ? FuzzyQuery.defaultMaxEdits : maxEdits;
+    this.maxExpansions = maxExpansions == null ? Intervals.DEFAULT_MAX_EXPANSIONS : maxExpansions;
   }
 
   @Override
   public IntervalsSource toIntervalSource(String field, Analyzer analyzer) {
-    int gaps = 0;
-    boolean ordered = true;
-    try {
-      return Intervals.analyzedText(term, analyzer, field, gaps, ordered);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return Intervals.fuzzyTerm(
+        term,
+        maxEdits,
+        FuzzyQuery.defaultPrefixLength,
+        FuzzyQuery.defaultTranspositions,
+        maxExpansions);
   }
 
   @Override
   public String toString() {
-    if (requiresQuotes(term)) {
-      return '"' + term + '"';
-    } else {
-      return term;
-    }
-  }
-
-  static boolean requiresQuotes(String term) {
-    return Pattern.compile("[\\s]").matcher(term).find();
+    return String.format(
+        Locale.ROOT,
+        "fn:fuzzyTerm(%s %d%s)",
+        AnalyzedText.requiresQuotes(term) ? '"' + term + '"' : term,
+        maxEdits,
+        maxExpansions);
   }
 }
