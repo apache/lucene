133103a956f5c564bbe40f3368e96bf632e2c482
xiaoping.wjp <xiaoping.wjp@alibaba-inc.com>
2022-03-30 11:05:06 +0800

    query support clone

--- lucene/queries/src/java/org/apache/lucene/queries/spans/SpanContainQuery.java
+++ lucene/queries/src/java/org/apache/lucene/queries/spans/SpanContainQuery.java
@@ -29,7 +29,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryVisitor;
 
-abstract class SpanContainQuery extends SpanQuery implements Cloneable {
+abstract class SpanContainQuery extends SpanQuery {
 
   SpanQuery big;
   SpanQuery little;
