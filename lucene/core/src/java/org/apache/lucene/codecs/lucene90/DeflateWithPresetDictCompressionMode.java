ebe2d7b4fd1199dfa27f734b39d555c5cc002642
xiaoping <wjp719@gmail.com>
2022-04-26 21:35:56 +0800

    LUCENE-10499: reduce unnecessary copy data overhead when growing array size (#786)
    Co-authored-by: xiaoping.wjp <xiaoping.wjp@alibaba-inc.com>

--- lucene/core/src/java/org/apache/lucene/codecs/lucene90/DeflateWithPresetDictCompressionMode.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene90/DeflateWithPresetDictCompressionMode.java
@@ -81,7 +81,7 @@ public final class DeflateWithPresetDictCompressionMode extends CompressionMode 
       // pad with extra "dummy byte": see javadocs for using Inflater(true)
       // we do it for compliance, but it's unnecessary for years in zlib.
       final int paddedLength = compressedLength + 1;
-      compressed = ArrayUtil.grow(compressed, paddedLength);
+      compressed = ArrayUtil.growNoCopy(compressed, paddedLength);
       in.readBytes(compressed, 0, compressedLength);
       compressed[compressedLength] = 0; // explicitly set dummy byte to 0
 
@@ -113,7 +113,7 @@ public final class DeflateWithPresetDictCompressionMode extends CompressionMode 
       }
       final int dictLength = in.readVInt();
       final int blockLength = in.readVInt();
-      bytes.bytes = ArrayUtil.grow(bytes.bytes, dictLength);
+      bytes.bytes = ArrayUtil.growNoCopy(bytes.bytes, dictLength);
       bytes.offset = bytes.length = 0;
 
       final Inflater decompressor = new Inflater(true);
