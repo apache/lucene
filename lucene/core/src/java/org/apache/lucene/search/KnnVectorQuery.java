1e16a0692e5d60109db01aab403105bcbb0d74a1
Lu Xugang <q133662517@Gmail.com>
2022-05-02 16:01:07 +0800

    LUCENE-10552: KnnVectorQuery has incorrect equals/ hashCode

--- lucene/core/src/java/org/apache/lucene/search/KnnVectorQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/KnnVectorQuery.java
@@ -288,15 +288,20 @@ public class KnnVectorQuery extends Query {
 
   @Override
   public boolean equals(Object obj) {
-    return sameClassAs(obj)
-        && ((KnnVectorQuery) obj).k == k
-        && ((KnnVectorQuery) obj).field.equals(field)
-        && Arrays.equals(((KnnVectorQuery) obj).target, target);
+    if (sameClassAs(obj) == false) {
+      return false;
+    }
+    return ((KnnVectorQuery) obj).k == k
+            && ((KnnVectorQuery) obj).field.equals(field)
+            && Arrays.equals(((KnnVectorQuery) obj).target, target)
+            && ((KnnVectorQuery) obj).filter == null
+        ? filter == null
+        : filter != null && ((KnnVectorQuery) obj).filter.equals(filter);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(classHash(), field, k, Arrays.hashCode(target));
+    return Objects.hash(classHash(), field, k, Arrays.hashCode(target), filter);
   }
 
   /** Caches the results of a KnnVector search: a list of docs and their scores */
