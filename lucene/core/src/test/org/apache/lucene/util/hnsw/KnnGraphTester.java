7563390d147386398b5af98d9adbae24dde0bfda
Michael Sokolov <sokolovm@amazon.com>
2022-05-04 21:17:50 +0000

    also use KnnVectorQuery for warming

--- lucene/core/src/test/org/apache/lucene/util/hnsw/KnnGraphTester.java
+++ lucene/core/src/test/org/apache/lucene/util/hnsw/KnnGraphTester.java
@@ -49,7 +49,6 @@ import org.apache.lucene.document.KnnVectorField;
 import org.apache.lucene.document.StoredField;
 import org.apache.lucene.index.CodecReader;
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.LeafReader;
@@ -63,7 +62,6 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.IntroSorter;
 import org.apache.lucene.util.PrintStreamInfoStream;
@@ -367,7 +365,7 @@ public class KnnGraphTester {
         for (int i = 0; i < numIters; i++) {
           // warm up
           targets.get(target);
-          doKnnSearch(reader, KNN_FIELD, target, topK, fanout);
+          doKnnVectorQuery(searcher, KNN_FIELD, target, topK, fanout);
         }
         targets.position(0);
         start = System.nanoTime();
@@ -434,21 +432,6 @@ public class KnnGraphTester {
   private static TopDocs doKnnVectorQuery(
       IndexSearcher searcher, String field, float[] vector, int k, int fanout) throws IOException {
     return searcher.search(new KnnVectorQuery(field, vector, k + fanout), k);
-  }
-
-  private static TopDocs doKnnSearch(
-      IndexReader reader, String field, float[] vector, int k, int fanout) throws IOException {
-    TopDocs[] results = new TopDocs[reader.leaves().size()];
-    for (LeafReaderContext ctx : reader.leaves()) {
-      Bits liveDocs = ctx.reader().getLiveDocs();
-      results[ctx.ord] =
-          ctx.reader().searchNearestVectors(field, vector, k + fanout, liveDocs, Integer.MAX_VALUE);
-      int docBase = ctx.docBase;
-      for (ScoreDoc scoreDoc : results[ctx.ord].scoreDocs) {
-        scoreDoc.doc += docBase;
-      }
-    }
-    return TopDocs.merge(k, results);
   }
 
   private float checkResults(TopDocs[] results, int[][] nn) {
