ad9f9685180c95977bfa9b91d8fcc01576651a99
Alan Woodward <romseygeek@apache.org>
2022-05-06 09:41:28 +0100

    LUCENE-10436: Reinstate public getdocValuesdocIdSetIterator method on DocValues (#869)
    The method moved from DocValuesFieldExistsQuery to DocValuesIterator, but the latter
    is a package-private utility class, making it invisible to client code.  This commit moves it
    back onto FieldExistsQuery, meaning that the upgrade path will be the same as for all other
    uses of DocValuesFieldExistsQuery.

--- lucene/core/src/java/org/apache/lucene/search/FieldExistsQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/FieldExistsQuery.java
@@ -41,6 +41,42 @@ public class FieldExistsQuery extends Query {
     this.field = Objects.requireNonNull(field);
   }
 
+  /**
+   * Returns a {@link DocIdSetIterator} from the given field or null if the field doesn't exist in
+   * the reader or if the reader has no doc values for the field.
+   */
+  public static DocIdSetIterator getDocValuesDocIdSetIterator(String field, LeafReader reader)
+      throws IOException {
+    FieldInfo fieldInfo = reader.getFieldInfos().fieldInfo(field);
+    final DocIdSetIterator iterator;
+    if (fieldInfo != null) {
+      switch (fieldInfo.getDocValuesType()) {
+        case NONE:
+          iterator = null;
+          break;
+        case NUMERIC:
+          iterator = reader.getNumericDocValues(field);
+          break;
+        case BINARY:
+          iterator = reader.getBinaryDocValues(field);
+          break;
+        case SORTED:
+          iterator = reader.getSortedDocValues(field);
+          break;
+        case SORTED_NUMERIC:
+          iterator = reader.getSortedNumericDocValues(field);
+          break;
+        case SORTED_SET:
+          iterator = reader.getSortedSetDocValues(field);
+          break;
+        default:
+          throw new AssertionError();
+      }
+      return iterator;
+    }
+    return null;
+  }
+
   public String getField() {
     return field;
   }
