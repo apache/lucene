520d771ebac1c2da822b74e2a44f3c096d5f96f6
Alan Woodward <romseygeek@apache.org>
2022-05-16 14:25:36 +0100

    LUCENE-10575: Fix some visibility issues (#894)

--- lucene/highlighter/src/java/org/apache/lucene/search/matchhighlight/MatchRegionRetriever.java
+++ lucene/highlighter/src/java/org/apache/lucene/search/matchhighlight/MatchRegionRetriever.java
@@ -70,10 +70,10 @@ public class MatchRegionRetriever {
   }
 
   /**
-   * An abstraction that provides document values for a given field. Default implementation in
-   * {@link DocumentFieldValueProvider} just reaches to a preloaded {@link Document}. It is possible
-   * to write a more efficient implementation on top of a reusable character buffer (that reuses the
-   * buffer while retrieving hit regions for documents).
+   * An abstraction that provides document values for a given field. The default implementation just
+   * reaches to a preloaded {@link Document}. It is possible to write a more efficient
+   * implementation on top of a reusable character buffer (that reuses the buffer while retrieving
+   * hit regions for documents).
    */
   @FunctionalInterface
   public interface FieldValueProvider {
