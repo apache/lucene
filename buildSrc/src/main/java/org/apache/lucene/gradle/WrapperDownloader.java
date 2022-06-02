5a9adf95a48f4b9442bb0349e4b1539b558342a8
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2022-06-02 23:27:02 +0900

    allow to use major version in range

--- buildSrc/src/main/java/org/apache/lucene/gradle/WrapperDownloader.java
+++ buildSrc/src/main/java/org/apache/lucene/gradle/WrapperDownloader.java
@@ -40,6 +40,7 @@ import static java.nio.file.StandardOpenOption.APPEND;
  */
 public class WrapperDownloader {
   private static final Runtime.Version REQUIRED_VERSION = Runtime.Version.parse("17.0.3");
+  private static final int MAXIMUM_FEATURE_VERSION = 17;
 
   public static void main(String[] args) {
     if (args.length != 1) {
@@ -58,8 +59,12 @@ public class WrapperDownloader {
 
   public static void checkVersion() {
     final Runtime.Version version = Runtime.version();
-    if (version.feature() != REQUIRED_VERSION.feature()) {
-      throw new IllegalStateException("Java version must be exactly " + REQUIRED_VERSION.feature() + " (>=" + REQUIRED_VERSION + "), your version: " + version);
+    if (version.feature() < REQUIRED_VERSION.feature() || version.feature() > MAXIMUM_FEATURE_VERSION) {
+      if (REQUIRED_VERSION.feature() == MAXIMUM_FEATURE_VERSION) {
+        throw new IllegalStateException("Java version must be exactly " + REQUIRED_VERSION.feature() + " (>=" + REQUIRED_VERSION + "), your version: " + version);
+      } else {
+        throw new IllegalStateException("Java version must be between " + REQUIRED_VERSION.feature() + " and " + MAXIMUM_FEATURE_VERSION + " (>=" + REQUIRED_VERSION + "), your version: " + version);
+      }
     }
     if (version.interim() < REQUIRED_VERSION.interim() || (version.interim() == REQUIRED_VERSION.interim() && version.update() < REQUIRED_VERSION.update())) {
       throw new IllegalStateException("You are using too old Java minor version. Use newer than " + REQUIRED_VERSION + ", your version: " + version);
