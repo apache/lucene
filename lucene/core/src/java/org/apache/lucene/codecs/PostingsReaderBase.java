a831b556bfececeba73b52ca50e18b5456826659
Vigya Sharma <vigyaspeaks@gmail.com>
2022-06-07 22:47:26 -0700

    remove extra PostingsEnum from docstring

--- lucene/core/src/java/org/apache/lucene/codecs/PostingsReaderBase.java
+++ lucene/core/src/java/org/apache/lucene/codecs/PostingsReaderBase.java
@@ -27,10 +27,9 @@ import org.apache.lucene.store.IndexInput;
 
 /**
  * The core terms dictionaries (BlockTermsReader, BlockTreeTermsReader) interact with a single
- * instance of this class to manage creation of {@link org.apache.lucene.index.PostingsEnum} and
- * {@link org.apache.lucene.index.PostingsEnum} instances. It provides an IndexInput (termsIn) where
- * this class may read any previously stored data that it had written in its corresponding {@link
- * PostingsWriterBase} at indexing time.
+ * instance of this class to manage creation of {@link org.apache.lucene.index.PostingsEnum}
+ * instances. It provides an IndexInput (termsIn) where this class may read any previously stored
+ * data that it had written in its corresponding {@link PostingsWriterBase} at indexing time.
  *
  * @lucene.experimental
  */
