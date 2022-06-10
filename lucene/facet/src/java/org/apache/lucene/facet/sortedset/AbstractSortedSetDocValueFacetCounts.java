b0c0b8fc0c4f00e982b04a988f46632519618600
Greg Miller <gsmiller@gmail.com>
2022-06-10 11:14:38 -0700

    pr feedback

--- lucene/facet/src/java/org/apache/lucene/facet/sortedset/AbstractSortedSetDocValueFacetCounts.java
+++ lucene/facet/src/java/org/apache/lucene/facet/sortedset/AbstractSortedSetDocValueFacetCounts.java
@@ -75,7 +75,8 @@ abstract class AbstractSortedSetDocValueFacetCounts extends Facets {
   @Override
   public Number getSpecificValue(String dim, String... path) throws IOException {
     if (stateConfig.getDimConfig(dim).hierarchical == false && path.length != 1) {
-      throw new IllegalArgumentException("path must be length=1");
+      throw new IllegalArgumentException(
+          dim + " is not configured as hierarchical, path must be length=1");
     }
     int ord = (int) dv.lookupTerm(new BytesRef(FacetsConfig.pathToString(dim, path)));
     if (ord < 0) {
