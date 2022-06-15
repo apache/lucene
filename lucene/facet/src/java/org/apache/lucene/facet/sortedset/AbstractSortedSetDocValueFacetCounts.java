8888ac5e08bdf74b1a1b7a45729ce52c48796b62
Greg Miller <gsmiller@gmail.com>
2022-06-15 11:31:23 -0700

    LUCENE-10584: Properly support #getSpecificValue for hierarchical dims in SSDV faceting

--- lucene/facet/src/java/org/apache/lucene/facet/sortedset/AbstractSortedSetDocValueFacetCounts.java
+++ lucene/facet/src/java/org/apache/lucene/facet/sortedset/AbstractSortedSetDocValueFacetCounts.java
@@ -75,8 +75,9 @@ abstract class AbstractSortedSetDocValueFacetCounts extends Facets {
 
   @Override
   public Number getSpecificValue(String dim, String... path) throws IOException {
-    if (path.length != 1) {
-      throw new IllegalArgumentException("path must be length=1");
+    if (stateConfig.getDimConfig(dim).hierarchical == false && path.length != 1) {
+      throw new IllegalArgumentException(
+          dim + " is not configured as hierarchical, path must be length=1");
     }
     int ord = (int) dv.lookupTerm(new BytesRef(FacetsConfig.pathToString(dim, path)));
     if (ord < 0) {
