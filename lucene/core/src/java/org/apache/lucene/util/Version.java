e3339bb3bca682471d03370f8670299b080ddfab
Mike Drob <mdrob@apple.com>
2022-06-21 22:53:53 -0700

    Add bugfix version 8.11.2

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -200,6 +200,13 @@ public final class Version {
   @Deprecated public static final Version LUCENE_8_11_1 = new Version(8, 11, 1);
 
   /**
+   * Match settings and bugs in Lucene's 8.11.2 release.
+   * @deprecated Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_8_11_2 = new Version(8, 11, 2);
+
+  /**
    * Match settings and bugs in Lucene's 8.12.0 release.
    *
    * @deprecated Use latest
