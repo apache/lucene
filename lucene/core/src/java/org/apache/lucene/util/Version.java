df5592f52beec39ce28be2b0ae553a892e25e75e
iverase <ivera@apache.org>
2022-07-29 10:04:56 +0200

    Add next bugfix version 9.3.1

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -223,11 +223,16 @@ public final class Version {
   @Deprecated public static final Version LUCENE_9_2_0 = new Version(9, 2, 0);
 
   /**
-   * Match settings and bugs in Lucene's 9.3.0 release.
-   *
+   * @deprecated (9.3.1) Use latest
+   */
+  @Deprecated
+  public static final Version LUCENE_9_3_0 = new Version(9, 3, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 9.3.1 release.
    * <p>Use this to get the latest &amp; greatest settings, bug fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_9_3_0 = new Version(9, 3, 0);
+  public static final Version LUCENE_9_3_1 = new Version(9, 3, 1);
 
   // To add a new version:
   //  * Only add above this comment
@@ -243,7 +248,7 @@ public final class Version {
    * <b>re-test your entire application</b> to ensure it behaves as expected, as some defaults may
    * have changed and may break functionality in your application.
    */
-  public static final Version LATEST = LUCENE_9_3_0;
+  public static final Version LATEST = LUCENE_9_3_1;
 
   /**
    * Constant for backwards compatibility.
