86779046e9c30147c78c21682d5e8aa3c23fbf47
Rushabh Shah <shahrs87@apache.org>
2022-07-31 21:40:01 -0700

    Revert "Adding Terms#empty(FieldInfo) method"
    This reverts commit 1d3976a9294ff897b5c8e3d2c39ab01506d41104.

--- lucene/core/src/java/org/apache/lucene/codecs/perfield/PerFieldPostingsFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/perfield/PerFieldPostingsFormat.java
@@ -296,9 +296,20 @@ public abstract class PerFieldPostingsFormat extends PostingsFormat {
 
       // Then rebuild fields:
       for (Map.Entry<String, FieldsProducer> ent : other.fields.entrySet()) {
-        FieldsProducer producer = oldToNew.get(ent.getValue());
-        assert producer != null;
-        fields.put(ent.getKey(), producer);
+        Terms terms = null;
+        try {
+          terms = other.terms(ent.getKey());
+        } catch (
+            @SuppressWarnings("unused")
+            IOException e) {
+          // Should not come here.
+        }
+        assert terms != null;
+        if (terms != Terms.EMPTY) {
+          FieldsProducer producer = oldToNew.get(ent.getValue());
+          assert producer != null;
+          fields.put(ent.getKey(), producer);
+        }
       }
 
       segment = other.segment;
