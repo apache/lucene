e9a9dfcaf4b11d0706f8386d3622ec74611096db
Luca Cavanna <javanna@users.noreply.github.com>
2022-09-20 11:05:39 +0200

    Guard FieldExistsQuery against null pointers
    FieldExistsQuery checks if there are points for a certain field, and then retrieves the
    corresponding point values. When all documents that had points for a certain field have
    been deleted from a certain segments, as well as merged away, field info may report
    that there are points yet the corresponding point values are null.
    
    With this change we add a null check in FieldExistsQuery. Long term, we will likely want
    to prevent this situation from happening.
    
    Relates #11393

--- lucene/core/src/java/org/apache/lucene/search/FieldExistsQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/FieldExistsQuery.java
@@ -230,7 +230,8 @@ public class FieldExistsQuery extends Query {
             != DocValuesType.NONE) { // the field indexes doc values
           if (reader.hasDeletions() == false) {
             if (fieldInfo.getPointDimensionCount() > 0) {
-              return reader.getPointValues(field).getDocCount();
+              PointValues pointValues = reader.getPointValues(field);
+              return pointValues == null ? 0 : pointValues.getDocCount();
             } else if (fieldInfo.getIndexOptions() != IndexOptions.NONE) {
               return reader.terms(field).getDocCount();
             }
--- lucene/core/src/test/org/apache/lucene/search/TestFieldExistsQuery.java
+++ lucene/core/src/test/org/apache/lucene/search/TestFieldExistsQuery.java
@@ -702,6 +702,28 @@ public class TestFieldExistsQuery extends LuceneTestCase {
     return v;
   }
 
+  public void testDeleteAllPointDocs() throws Exception {
+    try (Directory dir = newDirectory();
+        RandomIndexWriter iw = new RandomIndexWriter(random(), dir)) {
+
+      Document doc = new Document();
+      doc.add(new StringField("id", "0", Field.Store.NO));
+      doc.add(new LongPoint("long", 17));
+      doc.add(new NumericDocValuesField("long", 17));
+      iw.addDocument(doc);
+      iw.addDocument(new Document());
+      iw.commit();
+
+      iw.deleteDocuments(new Term("id", "0"));
+      iw.forceMerge(1);
+
+      try (IndexReader reader = iw.getReader()) {
+        IndexSearcher searcher = newSearcher(reader);
+        assertEquals(0, searcher.count(new FieldExistsQuery("long")));
+      }
+    }
+  }
+
   private void assertSameMatches(IndexSearcher searcher, Query q1, Query q2, boolean scores)
       throws IOException {
     final int maxDoc = searcher.getIndexReader().maxDoc();
