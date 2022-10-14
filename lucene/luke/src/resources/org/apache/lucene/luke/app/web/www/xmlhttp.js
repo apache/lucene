28bde80e9aa9344faa786ef1ccf5a8af531c3b8b
Michael Sokolov <sokolovm@amazon.com>
2022-10-14 17:40:36 -0400

    adding licenses

--- lucene/luke/src/resources/org/apache/lucene/luke/app/web/www/xmlhttp.js
+++ lucene/luke/src/resources/org/apache/lucene/luke/app/web/www/xmlhttp.js
@@ -1,11 +1,29 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Wrapper function library for XMLHttpRequest (ie AJAX).
  *
  * This library encapsulates the machinery required to set up callbacks to
  * user functions in a manner that is threadsafe (handles multiple
  * simultaneous requests), makes minimal use of global variables (one is
- * required to ensure memory cleanup in IE), and does not leak memory in IE
- * due to lost circular references that cannot be garbage-collected.
+ * required to ensure memory cleanup in old versions of IE), and does
+ * not leak memory in IE due to lost circular references that cannot be
+ * garbage-collected.
  *
  * There are two main entry points, get_request and post_form, which
  * implement HTTP GET and POST, respectively.  post_form is intended for
