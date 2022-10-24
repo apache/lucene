878a7c57a4763a306cc9e080da2315aa043dc29d
iverase <ivera@apache.org>
2022-10-24 14:53:42 +0200

    Add back-compat indices for 9.4.1

--- lucene/backward-codecs/src/test/org/apache/lucene/backward_index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/backward_index/TestBackwardsCompatibility.java
@@ -410,7 +410,9 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "9.3.0-cfs",
     "9.3.0-nocfs",
     "9.4.0-cfs",
-    "9.4.0-nocfs"
+    "9.4.0-nocfs",
+    "9.4.1-cfs",
+    "9.4.1-nocfs"
   };
 
   public static String[] getOldNames() {
@@ -447,7 +449,8 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
     "sorted.9.1.0",
     "sorted.9.2.0",
     "sorted.9.3.0",
-    "sorted.9.4.0"
+    "sorted.9.4.0",
+    "sorted.9.4.1"
   };
 
   public static String[] getOldSortedNames() {
