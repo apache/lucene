408aae5935e8993579d7af124fe211aa613d698b
Adrien Grand <jpountz@gmail.com>
2022-11-14 19:01:01 +0100

    Remove MMapDirectory changes.

--- lucene/core/src/test/org/apache/lucene/store/TestMmapDirectory.java
+++ lucene/core/src/test/org/apache/lucene/store/TestMmapDirectory.java
@@ -32,7 +32,7 @@ public class TestMmapDirectory extends BaseDirectoryTestCase {
   @Override
   protected Directory getDirectory(Path path) throws IOException {
     MMapDirectory m = new MMapDirectory(path);
-    m.setPreload((file, context) -> random().nextBoolean());
+    m.setPreload(random().nextBoolean());
     return m;
   }
 
