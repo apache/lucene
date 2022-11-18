49c28508e07da8ff88d5b3a7a2b2f9ff3dfa9ebb
Dawid Weiss <dawid.weiss@carrotsearch.com>
2022-11-18 15:33:18 +0100

    Add ASL headers.

--- dev-tools/scripts/StageArtifacts.java
+++ dev-tools/scripts/StageArtifacts.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -113,6 +130,7 @@ public class StageArtifacts {
           if (params.userPass == null) {
             Console console = System.console();
             if (console != null) {
+              System.out.println("Enter password for " + params.userName + ":");
               params.userPass = console.readPassword();
             } else {
               throw new RuntimeException("No console, can't prompt for password.");
