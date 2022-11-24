2d8eb66c798199d1263953186d3ba4cebf951ed2
Luca Cavanna <javanna@users.noreply.github.com>
2022-11-24 21:56:15 +0100

    Fix GeoDegeneratePath compile error

--- lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/GeoDegeneratePath.java
+++ lucene/spatial3d/src/java/org/apache/lucene/spatial3d/geom/GeoDegeneratePath.java
@@ -497,8 +497,10 @@ class GeoDegeneratePath extends GeoBasePath {
      */
     public double pathCenterDistance(
         final DistanceStyle distanceStyle, final double x, final double y, final double z) {
-      if (!isWithinSection(x, y, z)) {
-        return Double.POSITIVE_INFINITY;
+      for (final Membership m : cutoffPlanes) {
+        if (!m.isWithin(x, y, z)) {
+          return Double.POSITIVE_INFINITY;
+        }
       }
       return distanceStyle.toAggregationForm(distanceStyle.computeDistance(this.point, x, y, z));
     }
