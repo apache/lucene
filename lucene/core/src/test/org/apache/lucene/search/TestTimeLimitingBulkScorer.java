e5ed486d9067ad12f56bc24fe9310d124976a38a
Costin Leau <costin.leau@gmail.com>
2022-11-28 19:58:57 -0800

    Add exponential growth to TimeLimitingBulkScorer
    Fix #11676

--- lucene/core/src/test/org/apache/lucene/search/TestTimeLimitingBulkScorer.java
+++ lucene/core/src/test/org/apache/lucene/search/TestTimeLimitingBulkScorer.java
@@ -22,10 +22,15 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.QueryTimeout;
+import org.apache.lucene.index.QueryTimeoutImpl;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.tests.analysis.MockAnalyzer;
 import org.apache.lucene.tests.util.LuceneTestCase;
+import org.apache.lucene.tests.util.TestUtil;
+import org.apache.lucene.util.Bits;
+
+import java.io.IOException;
 
 /** Tests the {@link TimeLimitingBulkScorer}. */
 public class TestTimeLimitingBulkScorer extends LuceneTestCase {
@@ -61,6 +66,40 @@ public class TestTimeLimitingBulkScorer extends LuceneTestCase {
     directoryReader.close();
     directory.close();
   }
+  
+  public void testExponentialRate() throws Exception {
+    var bulkScorer = new BulkScorer() {
+      int expectedInterval = TimeLimitingBulkScorer.INTERVAL;
+      int lastInterval = 0;
+      int runs = TestUtil.nextInt(random(), 1, 100);
+
+      @Override
+      public int score(LeafCollector collector, Bits acceptDocs, int min, int max) throws IOException {
+        var difference = max - min;
+        // the rate shouldn't overflow - only increase or remain equal
+        assertTrue("Rate should only go up", difference >= lastInterval);
+        assertEquals("Incorrect rate encountered", expectedInterval, difference);
+
+        lastInterval = difference;
+        // use integer sum since the exponential growth formula yields different result due to rounding
+        expectedInterval = expectedInterval + expectedInterval / 2;
+        // overflow - stop at the previous one
+        if (expectedInterval < 0) {
+          expectedInterval = lastInterval;
+        }
+        // keep going or finish the test?
+        return --runs == 0 ? DocIdSetIterator.NO_MORE_DOCS : 0;
+      }
+
+      @Override
+      public long cost() {
+        return 1;
+      }
+    };
+
+    var scorer = new TimeLimitingBulkScorer(bulkScorer, new QueryTimeoutImpl(-1));
+    scorer.score(dummyCollector(), new Bits.MatchAllBits(Integer.MAX_VALUE), 0,  Integer.MAX_VALUE - 1);
+  }
 
   private static QueryTimeout countingQueryTimeout(int timeallowed) {
 
@@ -74,6 +113,19 @@ public class TestTimeLimitingBulkScorer extends LuceneTestCase {
           return true;
         }
         return false;
+      }
+    };
+  }
+
+  private static LeafCollector dummyCollector()  {
+    return new SimpleCollector() {
+      @Override
+      public ScoreMode scoreMode() {
+        return ScoreMode.COMPLETE_NO_SCORES;
+      }
+
+      @Override
+      public void collect(int doc) throws IOException {
       }
     };
   }
