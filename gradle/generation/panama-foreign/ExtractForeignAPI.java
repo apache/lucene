10d65f40e8b41066b6862a5e438bc63e8154c4b5
Uwe Schindler <uschindler@apache.org>
2022-12-26 21:58:46 +0100

    Use a API signatures JAR file committed to repository

---
+++ gradle/generation/panama-foreign/ExtractForeignAPI.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.SimpleRemapper;
+
+public final class ExtractForeignAPI {
+  
+  private static final FileTime FIXED_FILEDATE = FileTime.from(Instant.parse("2022-01-01T00:00:00Z"));
+  
+  public static void main(String... args) throws IOException {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Need one parameter with output file.");
+    }
+    var outputPath = Paths.get(args[0]);
+    var javaBaseModule = Paths.get(URI.create("jrt:/")).resolve("java.base").toRealPath();
+    var fileMatcher = javaBaseModule.getFileSystem().getPathMatcher("glob:java/{lang/foreign/*,nio/channels/FileChannel}.class");
+    try (var out = new ZipOutputStream(Files.newOutputStream(outputPath))) {
+      Files.walkFileTree(javaBaseModule, new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+          var relative = javaBaseModule.relativize(file);
+          if (fileMatcher.matches(relative)) {
+            System.out.println("Processing class file: " + relative);
+            try (var in = Files.newInputStream(file)) {
+              final var reader = new ClassReader(in);
+              final var cw = new ClassWriter(0);
+              reader.accept(new Handler(cw), ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG);
+              out.putNextEntry(new ZipEntry(reader.getClassName().concat(".class")).setLastModifiedTime(FIXED_FILEDATE));
+              out.write(cw.toByteArray());
+              out.closeEntry();
+            }
+          }
+          return FileVisitResult.CONTINUE;
+        }
+     });
+    }
+  }
+  
+  static class Handler extends ClassVisitor {
+    
+    Handler(ClassWriter out) {
+      super(Opcodes.ASM9, new ClassRemapper(out, new SimpleRemapper("jdk/internal/javac/PreviewFeature", "jdk/internal/javac/NoPreviewFeature")));
+    }
+    
+    private static boolean isHidden(int access) {
+      return (access & (Opcodes.ACC_PROTECTED | Opcodes.ACC_PUBLIC)) == 0;
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+      super.visit(Opcodes.V11, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+      if (isHidden(access)) {
+        return null;
+      }
+      return super.visitField(access, name, descriptor, signature, value);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+      if (isHidden(access)) {
+        return null;
+      }
+      return super.visitMethod(access, name, descriptor, signature, exceptions);
+    }
+
+    @Override
+    public void visitPermittedSubclass(String permittedSubclass) {
+      // not compatible with old compilers
+    }
+
+    @Override
+    public void visitSource(String source, String debug) {
+      // info not needed
+    }
+
+  }
+  
+}
