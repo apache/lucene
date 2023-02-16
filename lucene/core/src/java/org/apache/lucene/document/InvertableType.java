feba5031cb5dd2af7df6abec0f6335d00aab71f5
Adrien Grand <jpountz@gmail.com>
2023-02-16 14:46:19 +0100

    Remove type guessing.

---
+++ lucene/core/src/java/org/apache/lucene/document/InvertableType.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.lucene.document;
+
+import org.apache.lucene.index.IndexableField;
+
+/** Describes how an {@link IndexableField} should be inverted for indexing terms and postings. */
+public enum InvertableType {
+
+  /**
+   * The field should be inverted through its {@link IndexableField#binaryValue()}, with a frequency
+   * of 1.
+   */
+  TERM,
+
+  /**
+   * The field should be inverted through its {@link
+   * IndexableField#tokenStream(org.apache.lucene.analysis.Analyzer,
+   * org.apache.lucene.analysis.TokenStream)}.
+   */
+  TOKEN_STREAM;
+}
