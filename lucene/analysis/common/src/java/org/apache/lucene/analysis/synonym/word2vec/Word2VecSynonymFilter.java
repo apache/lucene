4deb0003c425bfaff3923661512c44a315172854
Alessandro Benedetti <a.benedetti@sease.io>
2023-04-24 17:28:12 +0200

    Word2VecSynonymFilter constructor null check (#12169)

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/synonym/word2vec/Word2VecSynonymFilter.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/synonym/word2vec/Word2VecSynonymFilter.java
@@ -65,6 +65,9 @@ public final class Word2VecSynonymFilter extends TokenFilter {
       int maxSynonymsPerTerm,
       float minAcceptedSimilarity) {
     super(input);
+    if (synonymProvider == null) {
+      throw new IllegalArgumentException("The SynonymProvider must be non-null");
+    }
     this.synonymProvider = synonymProvider;
     this.maxSynonymsPerTerm = maxSynonymsPerTerm;
     this.minAcceptedSimilarity = minAcceptedSimilarity;
