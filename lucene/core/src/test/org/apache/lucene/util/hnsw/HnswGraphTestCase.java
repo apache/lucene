ef57b746f5404c5a8603b7c8689a00c194cbe958
Jonathan Ellis <jbellis@datastax.com>
2023-04-28 14:53:04 -0500

    replace wildcard import with individual classes

--- lucene/core/src/test/org/apache/lucene/util/hnsw/HnswGraphTestCase.java
+++ lucene/core/src/test/org/apache/lucene/util/hnsw/HnswGraphTestCase.java
@@ -23,7 +23,15 @@ import static org.apache.lucene.tests.util.RamUsageTester.ramUsed;
 
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.lucene95.Lucene95Codec;
