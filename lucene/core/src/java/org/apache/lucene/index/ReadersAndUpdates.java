336433767c199910d817f9c0ca188832d00a7a31
luyuncheng <luyuncheng@bytedance.com>
2023-05-09 09:29:10 +0800

    Update ReadersAndUpdates.java
    precommit import

--- lucene/core/src/java/org/apache/lucene/index/ReadersAndUpdates.java
+++ lucene/core/src/java/org/apache/lucene/index/ReadersAndUpdates.java
@@ -18,8 +18,6 @@ package org.apache.lucene.index;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
