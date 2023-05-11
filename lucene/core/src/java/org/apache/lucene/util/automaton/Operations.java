88641e592459a2c85de549c7d743ecd4bd50927f
tang donghai <tangdhcs@gmail.com>
2023-05-12 01:34:19 +0800

    fix comment error

--- lucene/core/src/java/org/apache/lucene/util/automaton/Operations.java
+++ lucene/core/src/java/org/apache/lucene/util/automaton/Operations.java
@@ -1314,8 +1314,8 @@ public final class Operations {
    *
    * @param a The automaton whose states are to be topologically sorted.
    * @param visited A BitSet representing the visited states during the sorting process.
-   * @param states An int array which stores the reversed topological order of the states.
-   * @return the topologically sorted array of state ids
+   * @param states An int array which stores the states.
+   * @return the reversed topologically sorted array of state ids
    * @throws IllegalArgumentException if the input automaton has a cycle.
    */
   private static int topoSortStates(Automaton a, BitSet visited, int[] states) {
