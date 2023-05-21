afc3d5549cf13d09772b3e594c55570fb0e916d5
Robert Muir <rmuir@apache.org>
2023-05-21 17:40:10 -0400

    speed up vector computation: more efficient reduction, 4x unrolling

--- lucene/core/src/java20/org/apache/lucene/util/VectorUtilPanamaProvider.java
+++ lucene/core/src/java20/org/apache/lucene/util/VectorUtilPanamaProvider.java
@@ -31,22 +31,41 @@ final class VectorUtilPanamaProvider implements VectorUtilProvider {
   public float dotProduct(float[] a, float[] b) {
     int i = 0;
     float res = 0;
-    // if the array size is large (2x platform vector size), its worth the overhead to vectorize
-    // vector loop is unrolled a single time (2 accumulators in parallel)
-    if (a.length >= 2 * SPECIES.length()) {
+    // if the array size is large (> 2x platform vector size), its worth the overhead to vectorize
+    if (a.length > 2 * SPECIES.length()) {
+      // vector loop is unrolled 4x (4 accumulators in parallel)
       FloatVector acc1 = FloatVector.zero(SPECIES);
       FloatVector acc2 = FloatVector.zero(SPECIES);
-      int upperBound = SPECIES.loopBound(a.length - SPECIES.length());
-      for (; i < upperBound; i += 2 * SPECIES.length()) {
+      FloatVector acc3 = FloatVector.zero(SPECIES);
+      FloatVector acc4 = FloatVector.zero(SPECIES);
+      int upperBound = SPECIES.loopBound(a.length - 3*SPECIES.length());
+      for (; i < upperBound; i += 4 * SPECIES.length()) {
         FloatVector va = FloatVector.fromArray(SPECIES, a, i);
         FloatVector vb = FloatVector.fromArray(SPECIES, b, i);
         acc1 = acc1.add(va.mul(vb));
         FloatVector vc = FloatVector.fromArray(SPECIES, a, i + SPECIES.length());
         FloatVector vd = FloatVector.fromArray(SPECIES, b, i + SPECIES.length());
         acc2 = acc2.add(vc.mul(vd));
+        FloatVector ve = FloatVector.fromArray(SPECIES, a, i + 2*SPECIES.length());
+        FloatVector vf = FloatVector.fromArray(SPECIES, b, i + 2*SPECIES.length());
+        acc3 = acc3.add(ve.mul(vf));
+        FloatVector vg = FloatVector.fromArray(SPECIES, a, i + 3*SPECIES.length());
+        FloatVector vh = FloatVector.fromArray(SPECIES, b, i + 3*SPECIES.length());
+        acc4 = acc4.add(vg.mul(vh));
       }
-      res += acc1.reduceLanes(VectorOperators.ADD) + acc2.reduceLanes(VectorOperators.ADD);
+      // vector tail: less scalar computations for unaligned sizes, esp with big vector sizes
+      upperBound = SPECIES.loopBound(a.length);
+      for (; i < upperBound; i += SPECIES.length()) {
+        FloatVector va = FloatVector.fromArray(SPECIES, a, i);
+        FloatVector vb = FloatVector.fromArray(SPECIES, b, i);
+        acc1 = acc1.add(va.mul(vb));
+      }
+      // reduce
+      FloatVector res1 = acc1.add(acc2);
+      FloatVector res2 = acc3.add(acc4);
+      res += res1.add(res2).reduceLanes(VectorOperators.ADD);
     }
+
     for (; i < a.length; i++) {
       res += b[i] * a[i];
     }
