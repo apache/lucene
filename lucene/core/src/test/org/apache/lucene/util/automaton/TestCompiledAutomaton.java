a990c15286fe919dd24c99bcf113cd548be160b0
Greg Miller <gsmiller@gmail.com>
2023-05-26 08:55:24 -0700

    GH#12321: Marked DaciukMihovAutomatonBuilder as deprecated (#12332)
    Preparing to reduce visibility of this class in a future release

--- lucene/core/src/test/org/apache/lucene/util/automaton/TestCompiledAutomaton.java
+++ lucene/core/src/test/org/apache/lucene/util/automaton/TestCompiledAutomaton.java
@@ -35,7 +35,7 @@ public class TestCompiledAutomaton extends LuceneTestCase {
       terms.add(new BytesRef(s));
     }
     Collections.sort(terms);
-    final Automaton a = DaciukMihovAutomatonBuilder.build(terms);
+    final Automaton a = Automata.makeStringUnion(terms);
     return new CompiledAutomaton(a, true, false, determinizeWorkLimit, false);
   }
 
