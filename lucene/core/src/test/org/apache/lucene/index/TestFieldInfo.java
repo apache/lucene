d17c0e455afd81b7344be8886bd405ac205bb743
Tomas Fernandez Lobbe <tflobbe@apache.org>
2023-06-01 11:12:18 -0700

    Fix precommit

--- lucene/core/src/test/org/apache/lucene/index/TestFieldInfo.java
+++ lucene/core/src/test/org/apache/lucene/index/TestFieldInfo.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.lucene.index;
 
 import java.util.Arrays;
@@ -228,16 +244,6 @@ public class TestFieldInfo extends LuceneTestCase {
           softDeletesField);
     }
 
-    public FieldInfoTestBuilder setName(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public FieldInfoTestBuilder setNumber(int number) {
-      this.number = number;
-      return this;
-    }
-
     public FieldInfoTestBuilder setStoreTermVector(boolean storeTermVector) {
       this.storeTermVector = storeTermVector;
       return this;
@@ -268,11 +274,6 @@ public class TestFieldInfo extends LuceneTestCase {
       return this;
     }
 
-    public FieldInfoTestBuilder setAttributes(Map<String, String> attributes) {
-      this.attributes = attributes;
-      return this;
-    }
-
     public FieldInfoTestBuilder setPointDimensionCount(int pointDimensionCount) {
       this.pointDimensionCount = pointDimensionCount;
       return this;
@@ -285,27 +286,6 @@ public class TestFieldInfo extends LuceneTestCase {
 
     public FieldInfoTestBuilder setPointNumBytes(int pointNumBytes) {
       this.pointNumBytes = pointNumBytes;
-      return this;
-    }
-
-    public FieldInfoTestBuilder setVectorDimension(int vectorDimension) {
-      this.vectorDimension = vectorDimension;
-      return this;
-    }
-
-    public FieldInfoTestBuilder setVectorEncoding(VectorEncoding vectorEncoding) {
-      this.vectorEncoding = vectorEncoding;
-      return this;
-    }
-
-    public FieldInfoTestBuilder setVectorSimilarityFunction(
-        VectorSimilarityFunction vectorSimilarityFunction) {
-      this.vectorSimilarityFunction = vectorSimilarityFunction;
-      return this;
-    }
-
-    public FieldInfoTestBuilder setSoftDeletesField(boolean softDeletesField) {
-      this.softDeletesField = softDeletesField;
       return this;
     }
   }
