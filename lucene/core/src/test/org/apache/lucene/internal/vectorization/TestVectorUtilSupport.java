341d72e9a811b4227b4fa1d807e9f957d64a63de
Uwe Schindler <uschindler@apache.org>
2023-07-03 11:39:19 +0200

    Refactor vectorization support in Lucene (#12410)

--- lucene/core/src/test/org/apache/lucene/internal/vectorization/TestVectorUtilSupport.java
+++ lucene/core/src/test/org/apache/lucene/internal/vectorization/TestVectorUtilSupport.java
@@ -14,20 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.lucene.util;
+package org.apache.lucene.internal.vectorization;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import java.util.function.ToDoubleFunction;
 import java.util.function.ToIntFunction;
 import java.util.stream.IntStream;
-import org.apache.lucene.tests.util.LuceneTestCase;
-import org.junit.BeforeClass;
 
-public class TestVectorUtilProviders extends LuceneTestCase {
+public class TestVectorUtilSupport extends BaseVectorizationTestCase {
 
   private static final double DELTA = 1e-3;
-  private static final VectorUtilProvider LUCENE_PROVIDER = new VectorUtilDefaultProvider();
-  private static final VectorUtilProvider JDK_PROVIDER = VectorUtilProvider.lookup(true);
 
   private static final int[] VECTOR_SIZES = {
     1, 4, 6, 8, 13, 16, 25, 32, 64, 100, 128, 207, 256, 300, 512, 702, 1024
@@ -35,20 +31,13 @@ public class TestVectorUtilProviders extends LuceneTestCase {
 
   private final int size;
 
-  public TestVectorUtilProviders(int size) {
+  public TestVectorUtilSupport(int size) {
     this.size = size;
   }
 
   @ParametersFactory
   public static Iterable<Object[]> parametersFactory() {
     return () -> IntStream.of(VECTOR_SIZES).boxed().map(i -> new Object[] {i}).iterator();
-  }
-
-  @BeforeClass
-  public static void beforeClass() throws Exception {
-    assumeFalse(
-        "Test only works when JDK's vector incubator module is enabled.",
-        JDK_PROVIDER instanceof VectorUtilDefaultProvider);
   }
 
   public void testFloatVectors() {
@@ -73,11 +62,16 @@ public class TestVectorUtilProviders extends LuceneTestCase {
     assertFloatReturningProviders(p -> p.cosine(a, b));
   }
 
-  private void assertFloatReturningProviders(ToDoubleFunction<VectorUtilProvider> func) {
-    assertEquals(func.applyAsDouble(LUCENE_PROVIDER), func.applyAsDouble(JDK_PROVIDER), DELTA);
+  private void assertFloatReturningProviders(ToDoubleFunction<VectorUtilSupport> func) {
+    assertEquals(
+        func.applyAsDouble(LUCENE_PROVIDER.getVectorUtilSupport()),
+        func.applyAsDouble(PANAMA_PROVIDER.getVectorUtilSupport()),
+        DELTA);
   }
 
-  private void assertIntReturningProviders(ToIntFunction<VectorUtilProvider> func) {
-    assertEquals(func.applyAsInt(LUCENE_PROVIDER), func.applyAsInt(JDK_PROVIDER));
+  private void assertIntReturningProviders(ToIntFunction<VectorUtilSupport> func) {
+    assertEquals(
+        func.applyAsInt(LUCENE_PROVIDER.getVectorUtilSupport()),
+        func.applyAsInt(PANAMA_PROVIDER.getVectorUtilSupport()));
   }
 }
