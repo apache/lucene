e85088390a97c108d8dd2141c1ca94c4b175b04d
Lu Xugang <luxugang@apache.org>
2023-08-16 11:36:20 +0800

    Remove outdated comment in Scorer (#12494)
    we should delete this comment since this constructor parameters already removed from LUCENE-2876 , it's description of 'given Similarity' is a lit bit confuse to reader.
    
    Scorer always provide non-negative

--- lucene/core/src/java/org/apache/lucene/search/Scorer.java
+++ lucene/core/src/java/org/apache/lucene/search/Scorer.java
@@ -23,13 +23,7 @@ import java.util.Objects;
  * Expert: Common scoring functionality for different types of queries.
  *
  * <p>A <code>Scorer</code> exposes an {@link #iterator()} over documents matching a query in
- * increasing order of doc Id.
- *
- * <p>Document scores are computed using a given <code>Similarity</code> implementation.
- *
- * <p><b>NOTE</b>: The values Float.Nan, Float.NEGATIVE_INFINITY and Float.POSITIVE_INFINITY are not
- * valid scores. Certain collectors (eg {@link TopScoreDocCollector}) will not properly collect hits
- * with these scores.
+ * increasing order of doc id.
  */
 public abstract class Scorer extends Scorable {
 
