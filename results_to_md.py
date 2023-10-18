4564f46f2e96339aca6336903dceb19380f5d9a6
Mike McCandless <mikemccand@apache.org>
2023-10-18 11:02:41 -0400

    delete temp utility tools

--- results_to_md.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-results = pickle.load(open('results.pk', 'rb'))
-
-print('|NodeHash size|FST (mb)|RAM (mb)|Build time (sec)|')
-print('|-------------|--------|--------|----------------|')
-for size, fst_mb, fst_build_sec, ram_mb in results:
-  print(f'|{size}|{fst_mb:.1f}|{ram_mb:.1f}|{fst_build_sec:.1f}|')
