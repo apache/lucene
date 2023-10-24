3d4082ca3dcfea41b702a741f8db99858abee8f1
Adrien Grand <jpountz@gmail.com>
2023-10-24 16:57:05 +0200

    Run merge-on-full-flush even though no changes got flushed. (#12549)
    Currently, merge-on-full-flush only checks if merges need to run if changes
    have been flushed to disk. This prevents from having different merging logic
    for refreshes and commits, since the merge policy would not be checked upon
    commit if no new documents got indexed since the previous refresh.

--- lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
@@ -3680,7 +3680,7 @@ public class IndexWriter
               // merge completes which would otherwise have
               // removed the files we are now syncing.
               deleter.incRef(toCommit.files(false));
-              if (anyChanges && maxCommitMergeWaitMillis > 0) {
+              if (maxCommitMergeWaitMillis > 0) {
                 // we can safely call preparePointInTimeMerge since writeReaderPool(true) above
                 // wrote all
                 // necessary files to disk and checkpointed them.
@@ -4232,7 +4232,6 @@ public class IndexWriter
           flushSuccess = true;
         } finally {
           assert Thread.holdsLock(fullFlushLock);
-          ;
           docWriter.finishFullFlush(flushSuccess);
           processEvents(false);
         }
