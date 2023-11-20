d5b1e80ba3f2a37bc5df1de3a9d8f7da6cb1b5f3
guofeng.my <guofeng.my@bytedance.com>
2023-11-20 19:12:32 +0800

    push msb benchmark code

--- lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/DocSorterBenchmark.java
+++ lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/DocSorterBenchmark.java
@@ -24,8 +24,11 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.BaseLSBRadixSorter;
+import org.apache.lucene.util.IntroSorter;
 import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.LongsRef;
+import org.apache.lucene.util.MSBRadixSorter;
+import org.apache.lucene.util.Sorter;
 import org.apache.lucene.util.TimSorter;
 import org.apache.lucene.util.packed.PackedInts;
 import org.openjdk.jmh.annotations.*;
@@ -44,16 +47,47 @@ import org.openjdk.jmh.annotations.*;
 public class DocSorterBenchmark {
 
   private DocOffsetTimSorter timSorter;
-  private DocOffsetRadixSorter radixSorter;
+  private DocOffsetLSBRadixSorter lsbRadixSorter;
+  private DocOffsetMSBRadixSorter msbRadixSorter;
 
   @Param({"100000"})
   int size;
 
-  @Param({"natural", "reverse", "random", "partial"})
+  @Param({"natural", "reverse", "random", "partial", "natural_exception", "reverse_exception"})
   String order;
 
   @Param({"24", "31"})
   int bit;
+
+  static {
+    DocSorterBenchmark docSorterBenchmark = new DocSorterBenchmark();
+    docSorterBenchmark.size = 100000;
+    docSorterBenchmark.order = "random";
+    docSorterBenchmark.bit = 31;
+    docSorterBenchmark.init();
+    docSorterBenchmark.msbSorter();
+    docSorterBenchmark.lsbSorter();
+    docSorterBenchmark.timSorter();
+    int size = docSorterBenchmark.size;
+    if (!Arrays.equals(
+        docSorterBenchmark.timSorter.docs,
+        0,
+        size,
+        docSorterBenchmark.lsbRadixSorter.docs,
+        0,
+        size)) {
+      throw new RuntimeException("lsb wrong");
+    }
+    if (!Arrays.equals(
+        docSorterBenchmark.timSorter.docs,
+        0,
+        size,
+        docSorterBenchmark.msbRadixSorter.docs,
+        0,
+        size)) {
+      throw new RuntimeException("msb wrong");
+    }
+  }
 
   @Setup(Level.Invocation)
   public void init() {
@@ -84,11 +118,26 @@ public class DocSorterBenchmark {
         }
         Arrays.sort(doc, p * pSize, size);
       }
+      case "natural_exception" -> {
+        Arrays.sort(doc);
+        for (int i = 0; i < 32; i++) {
+          doc[random.nextInt(doc.length)] = random.nextInt(maxDoc);
+        }
+      }
+      case "reverse_exception" -> {
+        List<Integer> docs = Arrays.stream(doc).sorted().boxed().collect(Collectors.toList());
+        Collections.reverse(docs);
+        doc = docs.stream().mapToInt(i -> i).toArray();
+        for (int i = 0; i < 32; i++) {
+          doc[random.nextInt(doc.length)] = random.nextInt(maxDoc);
+        }
+      }
     }
 
-    timSorter = new DocOffsetTimSorter(size / 64);
+    timSorter = new DocOffsetTimSorter(size / 8);
     timSorter.reset(doc.clone(), off.clone());
-    radixSorter = new DocOffsetRadixSorter(bit, doc.clone(), off.clone());
+    lsbRadixSorter = new DocOffsetLSBRadixSorter().reset(bit, doc.clone(), off.clone());
+    msbRadixSorter = new DocOffsetMSBRadixSorter(bit, doc.clone(), off.clone());
   }
 
   @Benchmark
@@ -97,8 +146,13 @@ public class DocSorterBenchmark {
   }
 
   @Benchmark
-  public void radixSorter() {
-    radixSorter.sort(0, size);
+  public void lsbSorter() {
+    lsbRadixSorter.sort(0, size);
+  }
+
+  @Benchmark
+  public void msbSorter() {
+    msbRadixSorter.sort(0, size);
   }
 
   private static final class DocOffsetTimSorter extends TimSorter {
@@ -163,7 +217,7 @@ public class DocSorterBenchmark {
     }
   }
 
-  private static final class DocOffsetRadixSorter extends BaseLSBRadixSorter {
+  private static final class DocOffsetLSBRadixSorter extends BaseLSBRadixSorter {
 
     private int[] docs;
     private int[] srcDocs;
@@ -173,14 +227,13 @@ public class DocSorterBenchmark {
     private long[] srcOffsets;
     private long[] destOffsets;
 
-    public DocOffsetRadixSorter(int bits, int[] docs, long[] offsets) {
-      super(bits);
+    public DocOffsetLSBRadixSorter() {
+      super(-1);
       destDocs = IntsRef.EMPTY_INTS;
       destOffsets = LongsRef.EMPTY_LONGS;
-      reset(bits, docs, offsets);
     }
 
-    public DocOffsetRadixSorter reset(int bits, int[] docs, long[] offsets) {
+    public DocOffsetLSBRadixSorter reset(int bits, int[] docs, long[] offsets) {
       this.bits = bits;
       this.docs = docs;
       this.srcDocs = docs;
@@ -238,6 +291,100 @@ public class DocSorterBenchmark {
       long tmpOffset = srcOffsets[i];
       srcOffsets[i] = srcOffsets[j];
       srcOffsets[j] = tmpOffset;
+    }
+  }
+
+  private static final class DocOffsetMSBRadixSorter extends MSBRadixSorter {
+
+    private final int[] docs;
+    private final long[] offsets;
+    private int k;
+
+    public DocOffsetMSBRadixSorter(int bits, int[] docs, long[] offsets) {
+      super((bits + 7) / 8);
+      this.docs = docs;
+      this.offsets = offsets;
+    }
+
+    @Override
+    protected int byteAt(int i, int k) {
+      return byteShift(i, shift(k));
+    }
+
+    private int shift(int k) {
+      return (maxLength - k - 1) << 3;
+    }
+
+    private int byteShift(int i, int shift) {
+      return ((docs[i] >>> shift) & 0xFF);
+    }
+
+    private int bucketShift(int i, int shift) {
+      return byteShift(i, shift) + 1;
+    }
+
+    @Override
+    protected void buildHistogram(
+        int prefixCommonBucket, int prefixCommonLen, int from, int to, int k, int[] histogram) {
+      this.k = k;
+      final int shift = shift(k);
+      histogram[prefixCommonBucket] = prefixCommonLen;
+      for (int i = from; i < to; ++i) {
+        int b = bucketShift(i, shift);
+        histogram[b]++;
+      }
+    }
+
+    @Override
+    protected Sorter getFallbackSorter(int k) {
+      return new IntroSorter() {
+        int pivot;
+
+        @Override
+        protected void setPivot(int i) {
+          this.pivot = docs[i];
+        }
+
+        @Override
+        protected int comparePivot(int j) {
+          return pivot - docs[j];
+        }
+
+        @Override
+        protected int compare(int i, int j) {
+          return docs[i] - docs[j];
+        }
+
+        @Override
+        protected void swap(int i, int j) {
+          DocOffsetMSBRadixSorter.this.swap(i, j);
+        }
+      };
+    }
+
+    @Override
+    protected void reorder(int from, int to, int[] startOffsets, int[] endOffsets, int k) {
+      assert this.k == k;
+      final int shift = shift(k);
+      for (int i = 0; i < HISTOGRAM_SIZE; ++i) {
+        final int limit = endOffsets[i];
+        for (int h1 = startOffsets[i]; h1 < limit; h1 = startOffsets[i]) {
+          final int b = bucketShift(from + h1, shift);
+          final int h2 = startOffsets[b]++;
+          swap(from + h1, from + h2);
+        }
+      }
+    }
+
+    @Override
+    protected void swap(int i, int j) {
+      int tmp = docs[i];
+      docs[i] = docs[j];
+      docs[j] = tmp;
+
+      long tmpOffset = offsets[i];
+      offsets[i] = offsets[j];
+      offsets[j] = tmpOffset;
     }
   }
 }
