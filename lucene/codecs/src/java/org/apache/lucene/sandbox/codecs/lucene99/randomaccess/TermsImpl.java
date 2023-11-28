cd60a4f4338a314f86a327d243f4b634a0c65d67
Tony Xu <tonyx@amazon.com>
2023-11-28 00:09:49 -0800

    Implement interesect

--- lucene/codecs/src/java/org/apache/lucene/sandbox/codecs/lucene99/randomaccess/TermsImpl.java
+++ lucene/codecs/src/java/org/apache/lucene/sandbox/codecs/lucene99/randomaccess/TermsImpl.java
@@ -27,9 +27,19 @@ import org.apache.lucene.index.PostingsEnum;
 import org.apache.lucene.index.TermState;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.AttributeSource;
 import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.BytesRefBuilder;
+import org.apache.lucene.util.RamUsageEstimator;
+import org.apache.lucene.util.automaton.ByteRunnable;
+import org.apache.lucene.util.automaton.CompiledAutomaton;
 import org.apache.lucene.util.fst.BytesRefPrimitiveLongFSTEnum;
+import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.PrimitiveLongFST;
+import org.apache.lucene.util.fst.PrimitiveLongFST.PrimitiveLongArc;
+import org.apache.lucene.util.fst.PrimitiveLongFST.PrimitiveLongFSTOutputs;
+import org.apache.lucene.util.fst.Util;
 
 final class TermsImpl extends Terms {
   private final FieldInfo fieldInfo;
@@ -104,12 +114,13 @@ final class TermsImpl extends Terms {
     return new RandomAccessTermsEnum();
   }
 
-  // TODO: implement a more efficient version via FST
-  //  @Override
-  //  public TermsEnum intersect(CompiledAutomaton compiled, BytesRef startTerm) throws IOException
-  // {
-  //    return null;
-  //  }
+  @Override
+  public TermsEnum intersect(CompiledAutomaton compiled, BytesRef startTerm) throws IOException {
+    if (compiled.type != CompiledAutomaton.AUTOMATON_TYPE.NORMAL) {
+      throw new IllegalArgumentException("please use CompiledAutomaton.getTermsEnum instead");
+    }
+    return new RandomAccessIntersectTermsEnum(compiled, startTerm);
+  }
 
   final class RandomAccessTermsEnum extends TermsEnum {
     private AttributeSource attrs;
@@ -243,6 +254,371 @@ final class TermsImpl extends Terms {
     @Override
     public void seekExact(long ord) throws IOException {
       throw new UnsupportedOperationException("By ord lookup not supported.");
+    }
+  }
+
+  final class RandomAccessIntersectTermsEnum extends TermsEnum {
+    private AttributeSource attrs;
+
+    private BytesRefBuilder term;
+
+    private boolean isTermStateCurrent;
+
+    private IntBlockTermState termState;
+
+    private final PrimitiveLongFST fst;
+
+    private final FST.BytesReader fstReader;
+
+    private final ByteRunnable fsa;
+
+    private PrimitiveLongFSTOutputs fstOutputs = PrimitiveLongFSTOutputs.getSingleton();
+
+    private final TermDataReaderProvider.TermDataReader termDataReader;
+
+    private Frame[] stack;
+
+    private int level;
+
+    private boolean pending;
+
+    private final class Frame {
+      /* fst stats */
+      PrimitiveLongArc fstArc;
+      long output;
+      /* automaton stats */
+      int fsaState;
+
+      Frame() {
+        this.fstArc = new PrimitiveLongArc();
+        this.fsaState = -1;
+      }
+
+      @Override
+      public String toString() {
+        return "arc=" + fstArc + " state=" + fsaState;
+      }
+    }
+
+    /**
+     * Inspired by {@link org.apache.lucene.codecs.memory.FSTTermsReader}'s IntersectTermsEnum
+     */
+    RandomAccessIntersectTermsEnum(CompiledAutomaton compiled, BytesRef startTerm)
+        throws IOException {
+      termState = (IntBlockTermState) lucene99PostingsReader.newTermState();
+      fst = termsDict.termsIndex().primitiveLongFST();
+      fstReader = fst.getBytesReader();
+      fsa = compiled.getByteRunnable();
+      termDataReader = termsDict.termDataReaderProvider().newReader();
+
+      stack = new Frame[16];
+      for (int i = 0; i < stack.length; i++) {
+        this.stack[i] = new Frame();
+      }
+      loadVirtualFrame(newFrame());
+      level = 0;
+
+      pushFrame(loadFirstFrame(newFrame()));
+      if (startTerm == null) {
+        pending = isAccept(topFrame());
+      } else {
+        doSeekCeil(startTerm);
+        pending =
+            (term == null || !startTerm.equals(term.get()))
+                && isValid(topFrame())
+                && isAccept(topFrame());
+      }
+    }
+
+    @Override
+    public BytesRef next() throws IOException {
+      if (pending) {
+        pending = false;
+        updateTermStateIfNeeded();
+        return term();
+      }
+      isTermStateCurrent = false;
+      DFS:
+      while (level > 0) {
+        Frame frame = newFrame();
+        if (loadExpandFrame(topFrame(), frame) != null) { // has valid target
+          pushFrame(frame);
+          if (isAccept(frame)) { // gotcha
+            break;
+          }
+          continue; // check next target
+        }
+        frame = popFrame();
+        while (level > 0) {
+          if (loadNextFrame(topFrame(), frame) != null) { // has valid sibling
+            pushFrame(frame);
+            if (isAccept(frame)) { // gotcha
+              break DFS;
+            }
+            continue DFS; // check next target
+          }
+          frame = popFrame();
+        }
+        return null;
+      }
+      if (term != null) {
+        updateTermStateIfNeeded();
+      }
+      return term();
+    }
+
+    private long accumulateOutput() {
+      long output = 0;
+      int upto = 0;
+      Frame last, next;
+      last = stack[1];
+      while (upto != level) {
+        upto++;
+        next = stack[upto];
+        output = fstOutputs.add(next.output, output);
+        last = next;
+      }
+      if (last.fstArc.isFinal()) {
+        output = fstOutputs.add(output, last.fstArc.nextFinalOutput());
+      }
+      return output;
+    }
+
+    private BytesRef doSeekCeil(BytesRef target) throws IOException {
+      Frame frame = null;
+      int label, upto = 0, limit = target.length;
+      while (upto < limit) { // to target prefix, or ceil label (rewind prefix)
+        frame = newFrame();
+        label = target.bytes[target.offset + upto] & 0xff;
+        frame = loadCeilFrame(label, topFrame(), frame);
+        if (frame == null || frame.fstArc.label() != label) {
+          break;
+        }
+        assert isValid(frame); // target must be fetched from automaton
+        pushFrame(frame);
+        upto++;
+      }
+      if (upto == limit) { // got target
+        return term();
+      }
+      if (frame != null) { // got larger term('s prefix)
+        pushFrame(frame);
+        return isAccept(frame) ? term() : next();
+      }
+      while (level > 0) { // got target's prefix, advance to larger term
+        frame = popFrame();
+        while (level > 0 && !canRewind(frame)) {
+          frame = popFrame();
+        }
+        if (loadNextFrame(topFrame(), frame) != null) {
+          pushFrame(frame);
+          return isAccept(frame) ? term() : next();
+        }
+      }
+      return null;
+    }
+
+    /** Load frame for target arc(node) on fst */
+    Frame loadExpandFrame(Frame top, Frame frame) throws IOException {
+      if (!canGrow(top)) {
+        return null;
+      }
+      frame.fstArc = fst.readFirstRealTargetArc(top.fstArc.target(), frame.fstArc, fstReader);
+      frame.fsaState = fsa.step(top.fsaState, frame.fstArc.label());
+      // if (TEST) System.out.println(" loadExpand frame="+frame);
+      if (frame.fsaState == -1) {
+        return loadNextFrame(top, frame);
+      }
+      frame.output = frame.fstArc.output();
+      return frame;
+    }
+
+    Frame loadCeilFrame(int label, Frame top, Frame frame) throws IOException {
+      PrimitiveLongArc arc = frame.fstArc;
+      arc = Util.readCeilArc(label, fst, top.fstArc, arc, fstReader);
+      if (arc == null) {
+        return null;
+      }
+      frame.fsaState = fsa.step(top.fsaState, arc.label());
+      if (frame.fsaState == -1) {
+        return loadNextFrame(top, frame);
+      }
+      frame.output = frame.fstArc.output();
+      return frame;
+    }
+
+    /** Load frame for sibling arc(node) on fst */
+    Frame loadNextFrame(Frame top, Frame frame) throws IOException {
+      if (!canRewind(frame)) {
+        return null;
+      }
+      while (!frame.fstArc.isLast()) {
+        frame.fstArc = fst.readNextRealArc(frame.fstArc, fstReader);
+        frame.fsaState = fsa.step(top.fsaState, frame.fstArc.label());
+        if (frame.fsaState != -1) {
+          break;
+        }
+      }
+      if (frame.fsaState == -1) {
+        return null;
+      }
+      frame.output = frame.fstArc.output();
+      return frame;
+    }
+
+    void updateTermStateIfNeeded() throws IOException {
+      if (!isTermStateCurrent) {
+        long fstOutput = accumulateOutput();
+        TermsIndex.TypeAndOrd typeAndOrd = TermsIndex.decodeLong(fstOutput);
+        termState =
+            termDataReader.getTermState(
+                typeAndOrd.termType(), typeAndOrd.ord(), fieldInfo.getIndexOptions());
+        isTermStateCurrent = true;
+      }
+    }
+
+    @Override
+    public AttributeSource attributes() {
+      if (attrs == null) {
+        attrs = new AttributeSource();
+      }
+      return attrs;
+    }
+
+    @Override
+    public boolean seekExact(BytesRef text) throws IOException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BytesRef term() throws IOException {
+      return term == null ? null : term.get();
+    }
+
+    @Override
+    public int docFreq() throws IOException {
+      updateTermStateIfNeeded();
+      return termState.docFreq;
+    }
+
+    @Override
+    public long totalTermFreq() throws IOException {
+      updateTermStateIfNeeded();
+      return termState.totalTermFreq;
+    }
+
+    @Override
+    public PostingsEnum postings(PostingsEnum reuse, int flags) throws IOException {
+      updateTermStateIfNeeded();
+      return lucene99PostingsReader.postings(fieldInfo, termState, reuse, flags);
+    }
+
+    @Override
+    public ImpactsEnum impacts(int flags) throws IOException {
+      updateTermStateIfNeeded();
+      return lucene99PostingsReader.impacts(fieldInfo, termState, flags);
+    }
+
+    @Override
+    public TermState termState() throws IOException {
+      updateTermStateIfNeeded();
+      return termState.clone();
+    }
+
+    /** Virtual frame, never pop */
+    Frame loadVirtualFrame(Frame frame) {
+      frame.output = fstOutputs.getNoOutput();
+      frame.fsaState = -1;
+      return frame;
+    }
+
+    Frame newFrame() {
+      if (level + 1 == stack.length) {
+        final Frame[] temp =
+            new Frame[ArrayUtil.oversize(level + 2, RamUsageEstimator.NUM_BYTES_OBJECT_REF)];
+        System.arraycopy(stack, 0, temp, 0, stack.length);
+        for (int i = stack.length; i < temp.length; i++) {
+          temp[i] = new Frame();
+        }
+        stack = temp;
+      }
+      return stack[level + 1];
+    }
+
+    Frame topFrame() {
+      return stack[level];
+    }
+
+    boolean isAccept(Frame frame) { // reach a term both fst&fsa accepts
+      return fsa.isAccept(frame.fsaState) && frame.fstArc.isFinal();
+    }
+
+    boolean isValid(Frame frame) { // reach a prefix both fst&fsa won't reject
+      return /*frame != null &&*/ frame.fsaState != -1;
+    }
+
+    boolean canGrow(Frame frame) { // can walk forward on both fst&fsa
+      return frame.fsaState != -1 && PrimitiveLongFST.targetHasArcs(frame.fstArc);
+    }
+
+    boolean canRewind(Frame frame) { // can jump to sibling
+      return !frame.fstArc.isLast();
+    }
+
+    void pushFrame(Frame frame) {
+      term = grow(frame.fstArc.label());
+      level++;
+    }
+
+    Frame popFrame() {
+      term = shrink();
+      level--;
+      return stack[level + 1];
+    }
+
+    Frame loadFirstFrame(Frame frame) {
+      frame.fstArc = fst.getFirstArc(frame.fstArc);
+      frame.output = frame.fstArc.output();
+      frame.fsaState = 0;
+      return frame;
+    }
+
+    BytesRefBuilder grow(int label) {
+      if (term == null) {
+        term = new BytesRefBuilder();
+      } else {
+        term.append((byte) label);
+      }
+      return term;
+    }
+
+    BytesRefBuilder shrink() {
+      if (term.length() == 0) {
+        term = null;
+      } else {
+        term.setLength(term.length() - 1);
+      }
+      return term;
+    }
+
+    @Override
+    public long ord() throws IOException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void seekExact(long ord) throws IOException {
+      throw new UnsupportedOperationException("By ord lookup not supported.");
+    }
+
+    @Override
+    public SeekStatus seekCeil(BytesRef text) throws IOException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void seekExact(BytesRef target, TermState state) throws IOException {
+      throw new UnsupportedOperationException();
     }
   }
 }
--- lucene/core/src/java/org/apache/lucene/util/fst/Util.java
+++ lucene/core/src/java/org/apache/lucene/util/fst/Util.java
@@ -32,6 +32,7 @@ import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.IntsRefBuilder;
 import org.apache.lucene.util.fst.FST.Arc;
 import org.apache.lucene.util.fst.FST.BytesReader;
+import org.apache.lucene.util.fst.PrimitiveLongFST.PrimitiveLongArc;
 
 /**
  * Static helper methods.
@@ -897,6 +898,88 @@ public final class Util {
   }
 
   /**
+   * TODO: can we work around this???
+   *
+   * <p>Same as {@link Util#readCeilArc(int, FST, Arc, Arc, BytesReader)} but for {@link
+   * PrimitiveLongFST}
+   */
+  public static PrimitiveLongArc readCeilArc(
+      int label,
+      PrimitiveLongFST fst,
+      PrimitiveLongArc follow,
+      PrimitiveLongArc arc,
+      BytesReader in)
+      throws IOException {
+    if (label == PrimitiveLongFST.END_LABEL) {
+      return PrimitiveLongFST.readEndArc(follow, arc);
+    }
+    if (!PrimitiveLongFST.targetHasArcs(follow)) {
+      return null;
+    }
+    fst.readFirstTargetArc(follow, arc, in);
+    if (arc.bytesPerArc() != 0 && arc.label() != PrimitiveLongFST.END_LABEL) {
+      if (arc.nodeFlags() == PrimitiveLongFST.ARCS_FOR_DIRECT_ADDRESSING) {
+        // Fixed length arcs in a direct addressing node.
+        int targetIndex = label - arc.label();
+        if (targetIndex >= arc.numArcs()) {
+          return null;
+        } else if (targetIndex < 0) {
+          return arc;
+        } else {
+          if (PrimitiveLongArc.BitTable.isBitSet(targetIndex, arc, in)) {
+            fst.readArcByDirectAddressing(arc, in, targetIndex);
+            assert arc.label() == label;
+          } else {
+            int ceilIndex = PrimitiveLongArc.BitTable.nextBitSet(targetIndex, arc, in);
+            assert ceilIndex != -1;
+            fst.readArcByDirectAddressing(arc, in, ceilIndex);
+            assert arc.label() > label;
+          }
+          return arc;
+        }
+      } else if (arc.nodeFlags() == PrimitiveLongFST.ARCS_FOR_CONTINUOUS) {
+        int targetIndex = label - arc.label();
+        if (targetIndex >= arc.numArcs()) {
+          return null;
+        } else if (targetIndex < 0) {
+          return arc;
+        } else {
+          fst.readArcByContinuous(arc, in, targetIndex);
+          assert arc.label() == label;
+          return arc;
+        }
+      }
+      // Fixed length arcs in a binary search node.
+      int idx = binarySearch(fst, arc, label);
+      if (idx >= 0) {
+        return fst.readArcByIndex(arc, in, idx);
+      }
+      idx = -1 - idx;
+      if (idx == arc.numArcs()) {
+        // DEAD END!
+        return null;
+      }
+      return fst.readArcByIndex(arc, in, idx);
+    }
+
+    // Variable length arcs in a linear scan list,
+    // or special arc with label == FST.END_LABEL.
+    fst.readFirstRealTargetArc(follow.target(), arc, in);
+
+    while (true) {
+      // System.out.println("  non-bs cycle");
+      if (arc.label() >= label) {
+        // System.out.println("    found!");
+        return arc;
+      } else if (arc.isLast()) {
+        return null;
+      } else {
+        fst.readNextRealArc(arc, in);
+      }
+    }
+  }
+
+  /**
    * Perform a binary search of Arcs encoded as a packed array
    *
    * @param fst the FST from which to read
@@ -936,8 +1019,7 @@ public final class Util {
   }
 
   /** Same as {@link Util#binarySearch(FST, Arc, int)} but for {@link PrimitiveLongFST} */
-  static int binarySearch(
-      PrimitiveLongFST fst, PrimitiveLongFST.PrimitiveLongArc arc, int targetLabel)
+  static int binarySearch(PrimitiveLongFST fst, PrimitiveLongArc arc, int targetLabel)
       throws IOException {
     assert arc.nodeFlags() == FST.ARCS_FOR_BINARY_SEARCH
         : "Arc is not encoded as packed array for binary search (nodeFlags="
