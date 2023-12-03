fdcc5aee56dab2e47d1e3b4f92a9f14ae7a9fddd
Deepika Sharma <dpshrma@amazon.com>
2023-12-03 19:41:38 +0530

    Added timeout support for query rewrite operation in IndexSearcher

--- lucene/core/src/java/org/apache/lucene/index/ExitableIndexReader.java
+++ lucene/core/src/java/org/apache/lucene/index/ExitableIndexReader.java
@@ -18,6 +18,11 @@
 package org.apache.lucene.index;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.lucene.util.Bits;
 
 /**
  * The {@link ExitableIndexReader} is used to timeout I/O operation which is done during query
@@ -29,6 +34,7 @@ import java.io.IOException;
 public final class ExitableIndexReader extends IndexReader {
   private final IndexReader indexReader;
   private final QueryTimeout queryTimeout;
+
   /**
    * Create a ExitableIndexReader wrapper over another {@link IndexReader} with a specified timeout.
    *
@@ -39,12 +45,18 @@ public final class ExitableIndexReader extends IndexReader {
   public ExitableIndexReader(IndexReader indexReader, QueryTimeout queryTimeout) {
     this.indexReader = indexReader;
     this.queryTimeout = queryTimeout;
+    doWrapDirectoryReader(indexReader, queryTimeout);
   }
+
   /** Thrown when elapsed search time exceeds allowed search time. */
   @SuppressWarnings("serial")
   static class TimeExceededException extends RuntimeException {
     private TimeExceededException() {
       super("TimeLimit Exceeded");
+    }
+
+    private TimeExceededException(Exception e) {
+      super(e);
     }
   }
 
@@ -142,5 +154,294 @@ public final class ExitableIndexReader extends IndexReader {
       throw new ExitableIndexReader.TimeExceededException();
     }
     return indexReader.getSumTotalTermFreq(field);
+  }
+
+  /** Thrown when elapsed search time exceeds allowed search time. */
+  protected static void doWrapDirectoryReader(IndexReader in, QueryTimeout queryTimeout) {
+    try {
+      Map<CacheKey, LeafReader> readerCache = new HashMap<>();
+      List<LeafReaderContext> leaves = in.leaves();
+      List<LeafReader> readers = new ArrayList<>();
+      for (LeafReaderContext leafCtx : leaves) {
+        LeafReader reader = leafCtx.reader();
+        readers.add(reader);
+        // we try to reuse the life docs instances here if the reader cache key didn't change
+        if (reader instanceof ExitableIndexReader.TimeoutLeafReader
+            && reader.getReaderCacheHelper() != null) {
+          readerCache.put((reader).getReaderCacheHelper().getKey(), reader);
+        }
+      }
+      ExitableSubReaderWrapper exitableSubReaderWrapper =
+          new ExitableSubReaderWrapper(readerCache, queryTimeout);
+      exitableSubReaderWrapper.wrap(readers);
+    } catch (TimeExceededException e) {
+      throw new TimeExceededException(e);
+    }
+  }
+
+  /** Thrown when elapsed search time exceeds allowed search time. */
+  private static class ExitableSubReaderWrapper extends FilterDirectoryReader.SubReaderWrapper {
+    private final Map<CacheKey, LeafReader> mapping;
+
+    private final QueryTimeout queryTimeout;
+
+    public ExitableSubReaderWrapper(
+        Map<CacheKey, LeafReader> oldReadersCache, QueryTimeout queryTimeout) {
+      assert oldReadersCache != null;
+      this.mapping = oldReadersCache;
+      this.queryTimeout = queryTimeout;
+    }
+
+    @Override
+    protected LeafReader[] wrap(List<? extends LeafReader> readers) {
+      List<LeafReader> wrapped = new ArrayList<>(readers.size());
+      for (LeafReader reader : readers) {
+        LeafReader wrap = wrap(reader);
+        assert wrap != null;
+        if (wrap.numDocs() != 0) {
+          wrapped.add(wrap);
+        }
+      }
+      return wrapped.toArray(new LeafReader[0]);
+    }
+
+    @Override
+    public LeafReader wrap(LeafReader reader) {
+      CacheHelper readerCacheHelper = reader.getReaderCacheHelper();
+      if (readerCacheHelper != null && mapping.containsKey(readerCacheHelper.getKey())) {
+        // if the reader cache helper didn't change and we have it in the cache don't bother
+        // creating a new one
+        return mapping.get(readerCacheHelper.getKey());
+      }
+      return new TimeoutLeafReader(reader, queryTimeout);
+    }
+  }
+
+  /**
+   * This reader filters out documents that have a doc values value in the given field and treat
+   * these documents as soft deleted. Hard deleted documents will also be filtered out in the life
+   * docs of this reader.
+   */
+  public static class TimeoutLeafReader extends FilterLeafReader {
+    /**
+     * Construct a FilterLeafReader based on the specified base reader.
+     *
+     * <p>Note that base reader is closed if this FilterLeafReader is closed.
+     *
+     * @param in specified base reader.
+     */
+
+    /** The underlying LeafReader. */
+    protected final LeafReader in;
+
+    private final QueryTimeout queryTimeout;
+
+    @Override
+    public CacheHelper getReaderCacheHelper() {
+      return null;
+    }
+
+    @Override
+    public CacheHelper getCoreCacheHelper() {
+      return null;
+    }
+
+    /**
+     * Create a TimeoutLeafReader wrapper over another {@link FilterLeafReader} with a specified
+     * timeout.
+     *
+     * @param in the wrapped {@link LeafReader}
+     * @param queryTimeout max time allowed for collecting hits after which {@link
+     *     ExitableIndexReader.TimeExceededException} is thrown
+     */
+    protected TimeoutLeafReader(LeafReader in, QueryTimeout queryTimeout) {
+      super(in);
+      if (in == null) {
+        throw new NullPointerException("incoming LeafReader must not be null");
+      }
+      this.in = in;
+      this.queryTimeout = queryTimeout;
+      in.registerParentReader(this);
+    }
+
+    @Override
+    public Bits getLiveDocs() {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getLiveDocs();
+    }
+
+    @Override
+    public FieldInfos getFieldInfos() {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getFieldInfos();
+    }
+
+    @Override
+    public PointValues getPointValues(String field) throws IOException {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getPointValues(field);
+    }
+
+    @Override
+    public FloatVectorValues getFloatVectorValues(String field) throws IOException {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getFloatVectorValues(field);
+    }
+
+    @Override
+    public ByteVectorValues getByteVectorValues(String field) throws IOException {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getByteVectorValues(field);
+    }
+
+    @Override
+    public TermVectors termVectors() throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.termVectors();
+    }
+
+    @Override
+    public int numDocs() {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      // Don't call ensureOpen() here (it could affect performance)
+      return in.numDocs();
+    }
+
+    @Override
+    public int maxDoc() {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      // Don't call ensureOpen() here (it could affect performance)
+      return in.maxDoc();
+    }
+
+    @Override
+    public StoredFields storedFields() throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.storedFields();
+    }
+
+    @Override
+    protected void doClose() throws IOException {
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      in.close();
+    }
+
+    @Override
+    public Terms terms(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.terms(field);
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder buffer = new StringBuilder("FilterLeafReader(");
+      buffer.append(in);
+      buffer.append(')');
+      return buffer.toString();
+    }
+
+    @Override
+    public NumericDocValues getNumericDocValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getNumericDocValues(field);
+    }
+
+    @Override
+    public BinaryDocValues getBinaryDocValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getBinaryDocValues(field);
+    }
+
+    @Override
+    public SortedDocValues getSortedDocValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getSortedDocValues(field);
+    }
+
+    @Override
+    public SortedNumericDocValues getSortedNumericDocValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getSortedNumericDocValues(field);
+    }
+
+    @Override
+    public SortedSetDocValues getSortedSetDocValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getSortedSetDocValues(field);
+    }
+
+    @Override
+    public NumericDocValues getNormValues(String field) throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getNormValues(field);
+    }
+
+    @Override
+    public LeafMetaData getMetaData() {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      return in.getMetaData();
+    }
+
+    @Override
+    public void checkIntegrity() throws IOException {
+      ensureOpen();
+      if (queryTimeout.shouldExit()) {
+        throw new ExitableIndexReader.TimeExceededException();
+      }
+      in.checkIntegrity();
+    }
+
+    @Override
+    /** Returns the wrapped {@link LeafReader}. */
+    public LeafReader getDelegate() {
+      return in;
+    }
   }
 }
