eba6fe6d206e680fc0b1c164fb755474dfdf237b
guofeng.my <guofeng.my@bytedance.com>
2023-12-11 22:14:01 +0800

    remove ignore

--- lucene/backward-codecs/src/test/org/apache/lucene/backward_index/TestBackwardsCompatibility.java
+++ lucene/backward-codecs/src/test/org/apache/lucene/backward_index/TestBackwardsCompatibility.java
@@ -2245,7 +2245,6 @@ public class TestBackwardsCompatibility extends LuceneTestCase {
   // #12895: test on a carefully crafted 9.8.0 index (from a small contiguous subset
   // of wikibigall unique terms) that shows the read-time exception of
   // IntersectTermsEnum (used by WildcardQuery)
-  @Ignore("re-enable once we merge #12900")
   public void testWildcardQueryExceptions990() throws IOException {
     Path path = createTempDir("12895");
 
