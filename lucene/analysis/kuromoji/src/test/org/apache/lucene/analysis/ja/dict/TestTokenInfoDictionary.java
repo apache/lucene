e1beed173aa9556bdc4ac0e0cff5387974243f48
Michael McCandless <mikemccand@apache.org>
2023-12-14 07:38:34 -0500

    Ensure Nori/Kuromoji shipped binary FST is the latest version (#12933)
    * ensure Nori/Kuromoji shipped binary FST is the latest version (closes #12911)
    
    * fold feedback from @uschindler: sharpen test failure methods to give the specific gradlew command to regenerate the precise FST (not everything)
    
    * add javadoc for FSTMetadata.getVersion

--- lucene/analysis/kuromoji/src/test/org/apache/lucene/analysis/ja/dict/TestTokenInfoDictionary.java
+++ lucene/analysis/kuromoji/src/test/org/apache/lucene/analysis/ja/dict/TestTokenInfoDictionary.java
@@ -21,6 +21,8 @@ import static org.apache.lucene.analysis.ja.dict.BinaryDictionary.POSDICT_FILENA
 import static org.apache.lucene.analysis.ja.dict.BinaryDictionary.TARGETMAP_FILENAME_SUFFIX;
 import static org.apache.lucene.analysis.ja.dict.TokenInfoDictionary.FST_FILENAME_SUFFIX;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -30,12 +32,14 @@ import java.nio.file.Path;
 import org.apache.lucene.analysis.ja.util.DictionaryBuilder;
 import org.apache.lucene.analysis.ja.util.DictionaryBuilder.DictionaryFormat;
 import org.apache.lucene.analysis.ja.util.ToStringUtil;
+import org.apache.lucene.store.InputStreamDataInput;
 import org.apache.lucene.tests.util.LuceneTestCase;
 import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.IntsRefBuilder;
 import org.apache.lucene.util.UnicodeUtil;
 import org.apache.lucene.util.fst.FST;
 import org.apache.lucene.util.fst.IntsRefFSTEnum;
+import org.apache.lucene.util.fst.PositiveIntOutputs;
 
 /** Tests of TokenInfoDictionary build tools; run using ant test-tools */
 public class TestTokenInfoDictionary extends LuceneTestCase {
@@ -178,6 +182,27 @@ public class TestTokenInfoDictionary extends LuceneTestCase {
     }
     if (VERBOSE) {
       System.out.println("checked " + numTerms + " terms, " + numWords + " words.");
+    }
+  }
+
+  // #12911: make sure our shipped binary FST for TokenInfoDictionary is the latest & greatest
+  // format
+  public void testBinaryFSTIsLatestFormat() throws Exception {
+    try (InputStream is =
+        new BufferedInputStream(
+            TokenInfoDictionary.getClassResource(TokenInfoDictionary.FST_FILENAME_SUFFIX))) {
+      // we only need to load the FSTMetadata to check version:
+      int actualVersion =
+          FST.readMetadata(new InputStreamDataInput(is), PositiveIntOutputs.getSingleton())
+              .getVersion();
+      assertEquals(
+          "TokenInfoDictionary's FST is not the latest version: expected "
+              + FST.VERSION_CURRENT
+              + " but got: "
+              + actualVersion
+              + "; run \"./gradlew :lucene:analysis:kuromoji:regenerate\" to regenerate this FST",
+          FST.VERSION_CURRENT,
+          actualVersion);
     }
   }
 }
