5d86135e56eb7b90879d047d28d53cbf98afe8e0
SylvanBrocard <sylvan.brocard@gmail.com>
2023-12-19 17:21:41 +0100

    [wip] use pqueue over sset

--- lucene/sandbox/dpu/src/native_lib/src/topdoc_sync.c
+++ lucene/sandbox/dpu/src/native_lib/src/topdoc_sync.c
@@ -9,9 +9,10 @@
 #include "dpu_error.h"
 #include "dpu_types.h"
 
-#define i_type SSet
-#define i_key int
-#include "stc/csset.h"
+#define i_val int
+#define i_cmp -c_default_cmp
+#define i_type PQue
+#include <stc/cpque.h>
 
 #define DPU_PROPAGATE(s)                                                                                                         \
     do {                                                                                                                         \
@@ -32,7 +33,7 @@ struct update_bounds_atomic_context {
     uint32_t *nr_topdocs;
     int *bound_by_dpu;
     pthread_mutex_t *mutex_array;
-    SSet *score_sets;
+    PQue *score_pques;
 };
 
 // TODO(sbrocard) Implement this
@@ -59,15 +60,13 @@ update_bounds_atomic(struct dpu_set_t rank, uint32_t rank_id, void *args)
     }
 
     for (int i_qry = 0; i_qry < nr_queries; i_qry++) {
-        SSet *score_set = &ctx->score_sets[i_qry];
+        PQue *score_pque = &ctx->score_pques[i_qry];
         pthread_mutex_lock(&ctx->mutex_array[i_qry]);
         for (int i_dpu = 0; i_dpu < nr_dpus; i_dpu++) {
             int best_score = (*my_bounds)[i_dpu][i_qry];
-            if (best_score > *SSet_front(score_set)) {
-                SSet_result res = SSet_insert(score_set, best_score);
-                if (res.inserted && SSet_size(score_set) > nr_topdocs[i_qry]) {
-                    SSet_erase_at(score_set, SSet_begin(score_set));
-                }
+            if (PQue_size(score_pque) < nr_topdocs[i_qry] || best_score > *PQue_top(score_pque)) {
+                PQue_pop(score_pque);
+                PQue_push(score_pque, best_score);
             }
         }
         pthread_mutex_unlock(&ctx->mutex_array[i_qry]);
@@ -83,9 +82,9 @@ broadcast_new_bounds(struct dpu_set_t set);
 dpu_error_t
 topdocs_lower_bound_sync(struct dpu_set_t set, uint32_t nr_dpus, uint32_t nr_ranks, uint32_t *nr_topdocs, int nr_queries)
 {
-    SSet *score_sets = malloc(nr_queries * sizeof(SSet));
+    PQue *score_pques = malloc(nr_queries * sizeof(PQue));
     for (int i = 0; i < nr_queries; i++) {
-        score_sets[i] = SSet_with_capacity(nr_topdocs[i] + 1);
+        score_pques[i] = PQue_with_capacity(nr_topdocs[i]);
     }
 
     pthread_mutex_t *mutex_array = malloc(nr_queries * sizeof(pthread_mutex_t));
@@ -100,7 +99,7 @@ topdocs_lower_bound_sync(struct dpu_set_t set, uint32_t nr_dpus, uint32_t nr_ran
         nr_topdocs,
         bound_by_dpu,
         mutex_array,
-        score_sets,
+        score_pques,
     };
 
     while (!all_dpus_have_finished()) {
@@ -120,9 +119,9 @@ topdocs_lower_bound_sync(struct dpu_set_t set, uint32_t nr_dpus, uint32_t nr_ran
     free(mutex_array);
     free(bound_by_dpu);
     for (int i = 0; i < nr_queries; i++) {
-        SSet_drop(&score_sets[i]);
+        PQue_drop(&score_pques[i]);
     }
-    free(score_sets);
+    free(score_pques);
 
     return DPU_OK;
 }
