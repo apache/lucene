fd950c0fad4ff6c5f60ccc2e1568815c6e291470
easyice <80152403@qq.com>
2024-01-07 19:22:59 +0800

    use growExact

--- lucene/core/src/java/org/apache/lucene/codecs/lucene90/LZ4WithPresetDictCompressionMode.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene90/LZ4WithPresetDictCompressionMode.java
@@ -128,8 +128,9 @@ public final class LZ4WithPresetDictCompressionMode extends CompressionMode {
       }
 
       // Read blocks that intersect with the interval we need
-      if (offsetInBlock < offset + length) {
-        bytes.bytes = ArrayUtil.grow(bytes.bytes, bytes.length + offset + length - offsetInBlock);
+      int bytesNeeded = bytes.length + offset + length - offsetInBlock;
+      if (offsetInBlock < offset + length && bytes.bytes.length < bytesNeeded) {
+        bytes.bytes = ArrayUtil.growExact(bytes.bytes, bytesNeeded);
       }
       while (offsetInBlock < offset + length) {
         final int bytesToDecompress = Math.min(blockLength, offset + length - offsetInBlock);
