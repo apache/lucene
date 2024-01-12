6e1ce4cc6b7a5a06e35a01a2347de9ba765dee61
Michael Froh <msfroh@gmail.com>
2024-01-12 15:09:18 +0000

    Output binary doc values as hex array in SimpleTextCodec (#12987)
    Binary doc values were being written directly in SimpleTextCodec, though
    they may not be valid UTF-8 (i.e. they may not be "text"). This change
    encodes them as a string representing an array of hexadecimal bytes.

--- lucene/codecs/src/test/org/apache/lucene/codecs/simpletext/TestSimpleTextDocValuesFormat.java
+++ lucene/codecs/src/test/org/apache/lucene/codecs/simpletext/TestSimpleTextDocValuesFormat.java
@@ -16,8 +16,23 @@
  */
 package org.apache.lucene.codecs.simpletext;
 
+import java.io.IOException;
+import java.util.List;
 import org.apache.lucene.codecs.Codec;
+import org.apache.lucene.document.BinaryDocValuesField;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.document.SortedSetDocValuesField;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.tests.analysis.MockAnalyzer;
 import org.apache.lucene.tests.index.BaseDocValuesFormatTestCase;
+import org.apache.lucene.tests.index.RandomIndexWriter;
+import org.apache.lucene.tests.util.TestUtil;
+import org.apache.lucene.util.BytesRef;
 
 /** Tests SimpleTextDocValuesFormat */
 public class TestSimpleTextDocValuesFormat extends BaseDocValuesFormatTestCase {
@@ -26,5 +41,38 @@ public class TestSimpleTextDocValuesFormat extends BaseDocValuesFormatTestCase {
   @Override
   protected Codec getCodec() {
     return codec;
+  }
+
+  public void testFileIsUTF8() throws IOException {
+    try (Directory dir = newDirectory()) {
+      IndexWriterConfig conf = newIndexWriterConfig(new MockAnalyzer(random()));
+      try (RandomIndexWriter writer = new RandomIndexWriter(random(), dir, conf)) {
+        for (int i = 0; i < 100; i++) {
+          writer.addDocument(
+              List.of(
+                  new SortedDocValuesField(
+                      "sortedVal", newBytesRef(TestUtil.randomSimpleString(random()))),
+                  new SortedSetDocValuesField(
+                      "sortedSetVal", newBytesRef(TestUtil.randomSimpleString(random()))),
+                  new NumericDocValuesField("numberVal", random().nextLong()),
+                  new BinaryDocValuesField("binaryVal", TestUtil.randomBinaryTerm(random()))));
+        }
+      }
+      for (String file : dir.listAll()) {
+        if (file.endsWith("dat")) {
+          try (IndexInput input = dir.openChecksumInput(file, IOContext.READONCE)) {
+            long length = input.length();
+            if (length > 20_000) {
+              // Avoid allocating a huge array if the length is wrong
+              fail("Doc values should not be this large");
+            }
+            byte[] bytes = new byte[(int) length];
+            input.readBytes(bytes, 0, bytes.length);
+            BytesRef bytesRef = new BytesRef(bytes);
+            assertNotEquals(bytesRef.toString(), Term.toString(bytesRef));
+          }
+        }
+      }
+    }
   }
 }
