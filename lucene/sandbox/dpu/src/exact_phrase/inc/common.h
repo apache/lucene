eb85dc9d4bd42cf36aff33ea7ec785706d732bf3
SylvanBrocard <sylvan.brocard@gmail.com>
2024-02-02 16:24:46 +0100

    turn off hicpp linting

--- lucene/sandbox/dpu/src/exact_phrase/inc/common.h
+++ lucene/sandbox/dpu/src/exact_phrase/inc/common.h
@@ -58,10 +58,6 @@
  */
 #define NORM_INV_TYPE uint8_t
 
-
-#undef __MRAM_UNALIGNED_ACCESS_LOG_NB_VLOCK
-#define __MRAM_UNALIGNED_ACCESS_LOG_NB_VLOCK 11U
-
 #undef mram_write_int_atomic
 /**
  * @def mram_write_int_atomic
@@ -102,7 +98,7 @@
         vmutex_lock(&__mram_unaligned_access_virtual_locks, __mram_update_int_atomic_hash);                                      \
         mram_read(((__mram_ptr void *)(__mram_update_int_atomic_dest_low)), &__mram_unaligned_access_buffer[me() << 3U], 8U);    \
         update_func((int *)__builtin_assume_aligned(&__mram_unaligned_access_buffer[me() << 3U]                                  \
-                            + ((uintptr_t)(__mram_update_int_atomic_dest_low != (uintptr_t)(dest)) << 2U),                                  \
+                            + ((uintptr_t)(__mram_update_int_atomic_dest_low != (uintptr_t)(dest)) << 2U),                       \
                         4U),                                                                                                     \
             args);                                                                                                               \
         mram_write(&__mram_unaligned_access_buffer[me() << 3U], ((__mram_ptr void *)(__mram_update_int_atomic_dest_low)), 8U);   \
