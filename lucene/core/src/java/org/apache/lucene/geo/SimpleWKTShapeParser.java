dd2fc3ff5f527ed141eb056135e9aebe22535d68
Dmitry Cherniachenko <2sabio@gmail.com>
2024-02-14 13:59:44 +0100

    Backport: Make `static final Map` constants immutable (#13100)

--- lucene/core/src/java/org/apache/lucene/geo/SimpleWKTShapeParser.java
+++ lucene/core/src/java/org/apache/lucene/geo/SimpleWKTShapeParser.java
@@ -21,6 +21,7 @@ import java.io.StreamTokenizer;
 import java.io.StringReader;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -404,21 +405,23 @@ public class SimpleWKTShapeParser {
     ENVELOPE("envelope"); // not part of the actual WKB spec
 
     private final String shapeName;
-    private static Map<String, ShapeType> shapeTypeMap = new HashMap<>();
+    private static final Map<String, ShapeType> shapeTypeMap;
     private static final String BBOX = "BBOX";
 
     static {
+      Map<String, ShapeType> shapeTypes = new HashMap<>();
       for (ShapeType type : values()) {
-        shapeTypeMap.put(type.shapeName, type);
+        shapeTypes.put(type.shapeName, type);
       }
-      shapeTypeMap.put(ENVELOPE.wktName().toLowerCase(Locale.ROOT), ENVELOPE);
+      shapeTypes.put(ENVELOPE.wktName().toLowerCase(Locale.ROOT), ENVELOPE);
+      shapeTypeMap = Collections.unmodifiableMap(shapeTypes);
     }
 
     ShapeType(String shapeName) {
       this.shapeName = shapeName;
     }
 
-    protected String typename() {
+    String typename() {
       return shapeName;
     }
 
@@ -429,10 +432,9 @@ public class SimpleWKTShapeParser {
 
     public static ShapeType forName(String shapename) {
       String typename = shapename.toLowerCase(Locale.ROOT);
-      for (ShapeType type : values()) {
-        if (type.shapeName.equals(typename)) {
-          return type;
-        }
+      ShapeType type = shapeTypeMap.get(typename);
+      if (type != null) {
+        return type;
       }
       throw new IllegalArgumentException("unknown geo_shape [" + shapename + "]");
     }
