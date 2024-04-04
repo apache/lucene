02bc51a7537ac4c24818e28fdbd2e48b81e42d22
Robert Muir <rmuir@apache.org>
2024-04-04 16:46:37 -0400

    move TestBugInSomething to analysis.tests
    This test is setup to reproduce complex failures from TestRandomChains,
    e.g. it has SopFilter and other tools for debugging. But it still
    resides in the analysis/common module and currently can't be used to
    debug any TestRandomChains failures that use other modules (e.g. icu).
    
    relates to #13271

--- lucene/analysis.tests/src/test/org/apache/lucene/analysis/tests/TestBugInSomething.java
+++ lucene/analysis.tests/src/test/org/apache/lucene/analysis/tests/TestBugInSomething.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.lucene.analysis.core;
+package org.apache.lucene.analysis.tests;
 
 import java.io.IOException;
 import java.io.Reader;
