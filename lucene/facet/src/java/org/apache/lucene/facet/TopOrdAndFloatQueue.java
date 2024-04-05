9ba4af7b883c1c5cd3453dc60d8024851e658367
Stefan Vodita <41467371+stefanvodita@users.noreply.github.com>
2024-04-05 12:28:47 +0100

    Reduce duplication in taxonomy facets; always do counts (#12966)
    This is a large change, refactoring most of the taxonomy facets code and changing internal behaviour, without changing the API. There are specific API changes this sets us up to do later, e.g. retrieving counts from aggregation facets.
    
    1. Move most of the responsibility from TaxonomyFacets implementations to TaxonomyFacets itself. This reduces code duplication and enables future development. Addresses genericity issue mentioned in #12553.
    2. As a consequence, introduce sparse values to FloatTaxonomyFacets, which previously used dense values always. This issue is part of #12576.
    3. Compute counts for all taxonomy facets always, which enables us to add an API to retrieve counts for association facets in the future. Addresses #11282.
    4. As a consequence of having counts, we can check whether we encountered a label while faceting (count > 0), while previously we relied on the aggregation value to be positive. Closes #12585.
    5. Introduce the idea of doing multiple aggregations in one go, with association facets doing the aggregation they were already doing, plus a count. We can extend to an arbitrary number of aggregations, as suggested in #12546.
    6. Don't change the API. The only change in behaviour users should notice is the fix for non-positive aggregation values, which were previously discarded.
    7. Add tests which were missing for sparse/dense values and non-positive aggregations.

--- lucene/facet/src/java/org/apache/lucene/facet/TopOrdAndFloatQueue.java
+++ lucene/facet/src/java/org/apache/lucene/facet/TopOrdAndFloatQueue.java
@@ -16,37 +16,42 @@
  */
 package org.apache.lucene.facet;
 
-import org.apache.lucene.util.PriorityQueue;
-
-/** Keeps highest results, first by largest float value, then tie break by smallest ord. */
-public class TopOrdAndFloatQueue extends PriorityQueue<TopOrdAndFloatQueue.OrdAndValue> {
-
-  /** Holds a single entry. */
-  public static final class OrdAndValue {
-
-    /** Ordinal of the entry. */
-    public int ord;
-
-    /** Value associated with the ordinal. */
-    public float value;
-
-    /** Default constructor. */
-    public OrdAndValue() {}
-  }
+/** Keeps highest results, first by largest float value, then tie-break by smallest ord. */
+public class TopOrdAndFloatQueue extends TopOrdAndNumberQueue {
 
   /** Sole constructor. */
   public TopOrdAndFloatQueue(int topN) {
     super(topN);
   }
 
-  @Override
-  protected boolean lessThan(OrdAndValue a, OrdAndValue b) {
-    if (a.value < b.value) {
-      return true;
-    } else if (a.value > b.value) {
-      return false;
-    } else {
-      return a.ord > b.ord;
+  /** Holds an ordinal and a float value. */
+  public static final class OrdAndFloat extends OrdAndValue {
+    /** The value corresponding to the ordinal is a float. */
+    public float value;
+
+    /** Default constructor. */
+    public OrdAndFloat() {}
+
+    @Override
+    public boolean lessThan(OrdAndValue other) {
+      OrdAndFloat otherOrdAndFloat = (OrdAndFloat) other;
+      if (value < otherOrdAndFloat.value) {
+        return true;
+      }
+      if (value > otherOrdAndFloat.value) {
+        return false;
+      }
+      return ord > otherOrdAndFloat.ord;
     }
+
+    @Override
+    public Number getValue() {
+      return value;
+    }
+  }
+
+  @Override
+  public OrdAndValue newOrdAndValue() {
+    return new OrdAndFloat();
   }
 }
