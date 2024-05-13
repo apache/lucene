f10748cee730cb7eb9517fdd428a8e269a355dbe
Benjamin Trent <ben.w.trent@gmail.com>
2024-05-13 11:00:04 -0400

    Ensure negative scores aren not returned from scalar quantization scorer (#13356)
    Depending on how we quantize and then scale, we can edge down below 0 for dotproduct scores.
    
    This is exceptionally rare, I have only seen it in extreme circumstances in tests (with random data and low dimensionality).

--- lucene/core/src/java/org/apache/lucene/util/quantization/ScalarQuantizedVectorSimilarity.java
+++ lucene/core/src/java/org/apache/lucene/util/quantization/ScalarQuantizedVectorSimilarity.java
@@ -80,8 +80,10 @@ public interface ScalarQuantizedVectorSimilarity {
     public float score(
         byte[] queryVector, float queryOffset, byte[] storedVector, float vectorOffset) {
       int dotProduct = comparator.compare(storedVector, queryVector);
+      // For the current implementation of scalar quantization, all dotproducts should be >= 0;
+      assert dotProduct >= 0;
       float adjustedDistance = dotProduct * constMultiplier + queryOffset + vectorOffset;
-      return (1 + adjustedDistance) / 2;
+      return Math.max((1 + adjustedDistance) / 2, 0);
     }
   }
 
@@ -99,6 +101,8 @@ public interface ScalarQuantizedVectorSimilarity {
     public float score(
         byte[] queryVector, float queryOffset, byte[] storedVector, float vectorOffset) {
       int dotProduct = comparator.compare(storedVector, queryVector);
+      // For the current implementation of scalar quantization, all dotproducts should be >= 0;
+      assert dotProduct >= 0;
       float adjustedDistance = dotProduct * constMultiplier + queryOffset + vectorOffset;
       return scaleMaxInnerProductScore(adjustedDistance);
     }
