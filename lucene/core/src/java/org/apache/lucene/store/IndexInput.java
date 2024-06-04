8375caf96284853877be544ec8366854e226a447
Adrien Grand <jpountz@gmail.com>
2024-06-04 18:01:48 +0200

    iter

--- lucene/core/src/java/org/apache/lucene/store/IndexInput.java
+++ lucene/core/src/java/org/apache/lucene/store/IndexInput.java
@@ -216,7 +216,7 @@ public abstract class IndexInput extends DataInput implements Closeable {
    * <p>The default implementation is a no-op.
    *
    * @param offset start offset
-   * @param length the number of bytes to prefetch
+   * @param length the number of bytes
    */
   public void readAhead(long offset, long length) throws IOException {}
 }
