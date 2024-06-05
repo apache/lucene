6235ceef6f37d3b63ff3d95ed55fadfff765f7c5
Ignacio Vera <ivera@apache.org>
2024-06-05 10:09:16 +0200

    iter javadocs

--- lucene/core/src/java/org/apache/lucene/index/DocValuesSkipper.java
+++ lucene/core/src/java/org/apache/lucene/index/DocValuesSkipper.java
@@ -19,7 +19,13 @@ package org.apache.lucene.index;
 import java.io.IOException;
 import org.apache.lucene.search.DocIdSetIterator;
 
-/** Skipper for {@link DocValues}. */
+/**
+ * Skipper for {@link DocValues}.
+ *
+ * <p>A skipper has a position that can only be advanced via {@link #advance(int)}. The next advance
+ * position must be greater than {@link #maxDocID(int)} at level 0. A skipper's position, along with
+ * a {@code level}, determines the interval at which the skipper is currently situated.
+ */
 public abstract class DocValuesSkipper {
 
   /**
@@ -37,23 +43,23 @@ public abstract class DocValuesSkipper {
   public abstract int numLevels();
 
   /**
-   * Return the minimum doc ID on the given level, inclusive. This returns {@code -1} if {@link
-   * #advance(int)} has not been called yet and {@link DocIdSetIterator#NO_MORE_DOCS} if the
-   * iterator is exhausted. This method is non-increasing when {@code level} increases. Said
+   * Return the minimum doc ID of the interval on the given level, inclusive. This returns {@code
+   * -1} if {@link #advance(int)} has not been called yet and {@link DocIdSetIterator#NO_MORE_DOCS}
+   * if the iterator is exhausted. This method is non-increasing when {@code level} increases. Said
    * otherwise {@code minDocID(level+1) <= minDocId(level)}.
    */
   public abstract int minDocID(int level);
 
   /**
-   * Return the maximum doc ID on the given level, inclusive. This returns {@code -1} if {@link
-   * #advance(int)} has not been called yet and {@link DocIdSetIterator#NO_MORE_DOCS} if the
-   * iterator is exhausted. This method is non-decreasing when {@code level} decreases. Said
+   * Return the maximum doc ID of the interval on the given level, inclusive. This returns {@code
+   * -1} if {@link #advance(int)} has not been called yet and {@link DocIdSetIterator#NO_MORE_DOCS}
+   * if the iterator is exhausted. This method is non-decreasing when {@code level} decreases. Said
    * otherwise {@code maxDocID(level+1) >= maxDocId(level)}.
    */
   public abstract int maxDocID(int level);
 
   /**
-   * Return the minimum value at the given level, inclusive.
+   * Return the minimum value of the interval at the given level, inclusive.
    *
    * <p><b>NOTE</b>: It is only guaranteed that values in this interval are greater than or equal
    * the returned value. There is no guarantee that one document actually has this value.
@@ -61,7 +67,7 @@ public abstract class DocValuesSkipper {
   public abstract long minValue(int level);
 
   /**
-   * Return the maximum value at the given level, inclusive.
+   * Return the maximum value of the interval at the given level, inclusive.
    *
    * <p><b>NOTE</b>: It is only guaranteed that values in this interval are less than or equal the
    * returned value. There is no guarantee that one document actually has this value.
