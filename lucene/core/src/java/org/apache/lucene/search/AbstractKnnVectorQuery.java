1666bf21fa83f3ced88a97a5d80a3e4400350556
Christine Poerschke <cpoerschke@apache.org>
2024-06-28 11:42:47 +0100

    reduce IndexReader.leaves() calls in AbstractKnnVectorQuery.findSegmentStarts

--- lucene/core/src/java/org/apache/lucene/search/AbstractKnnVectorQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/AbstractKnnVectorQuery.java
@@ -268,19 +268,19 @@ abstract class AbstractKnnVectorQuery extends Query {
       docs[i] = topK.scoreDocs[i].doc;
       scores[i] = topK.scoreDocs[i].score;
     }
-    int[] segmentStarts = findSegmentStarts(reader, docs);
+    int[] segmentStarts = findSegmentStarts(reader.leaves(), docs);
     return new DocAndScoreQuery(docs, scores, maxScore, segmentStarts, reader.getContext().id());
   }
 
-  static int[] findSegmentStarts(IndexReader reader, int[] docs) {
-    int[] starts = new int[reader.leaves().size() + 1];
+  static int[] findSegmentStarts(List<LeafReaderContext> leaves, int[] docs) {
+    int[] starts = new int[leaves.size() + 1];
     starts[starts.length - 1] = docs.length;
     if (starts.length == 2) {
       return starts;
     }
     int resultIndex = 0;
     for (int i = 1; i < starts.length - 1; i++) {
-      int upper = reader.leaves().get(i).docBase;
+      int upper = leaves.get(i).docBase;
       resultIndex = Arrays.binarySearch(docs, resultIndex, docs.length, upper);
       if (resultIndex < 0) {
         resultIndex = -1 - resultIndex;
