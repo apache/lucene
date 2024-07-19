17d05fc71fddd54387aa00c0208c147f82523445
Egor Potemkin <eg13reg@gmail.com>
2024-07-19 11:37:11 +0100

    Reducer#MAX and #SUM: implement all interface methods

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/abstracts/Reducer.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/abstracts/Reducer.java
@@ -16,48 +16,67 @@
  */
 package org.apache.lucene.sandbox.facet.abstracts;
 
-/** Reducer for numeric values. * */
+/** Reducer for numeric values. */
 public interface Reducer {
 
-  /** Int values reducer. * */
-  default int reduce(int a, int b) {
-    throw new UnsupportedOperationException("Incompatible types");
-  }
+  /** Int values reducer. */
+  int reduce(int a, int b);
+
+  /** Long values reducer. */
+  long reduce(long a, long b);
+
+  /** Float values reducer. */
+  float reduce(float a, float b);
+
+  /** Double values reducer. */
+  double reduce(double a, double b);
   ;
 
-  /** Long values reducer. * */
-  default long reduce(long a, long b) {
-    throw new UnsupportedOperationException("Incompatible types");
-  }
-  ;
-
-  /** Float values reducer. * */
-  default float reduce(float a, float b) {
-    throw new UnsupportedOperationException("Incompatible types");
-  }
-  ;
-
-  /** Double values reducer. * */
-  default double reduce(double a, double b) {
-    throw new UnsupportedOperationException("Incompatible types");
-  }
-  ;
-
-  /** Reducer that returns MAX of two values. * */
-  public static final Reducer MAX =
+  /** Reducer that returns MAX of two values. */
+  Reducer MAX =
       new Reducer() {
+        @Override
+        public int reduce(int a, int b) {
+          return Math.max(a, b);
+        }
+
         @Override
         public long reduce(long a, long b) {
           return Math.max(a, b);
         }
+
+        @Override
+        public float reduce(float a, float b) {
+          return Math.max(a, b);
+        }
+
+        @Override
+        public double reduce(double a, double b) {
+          return Math.max(a, b);
+        }
       };
 
-  /** Reducer that returns SUM of two values. * */
-  public static final Reducer SUM =
+  /** Reducer that returns SUM of two values. */
+  Reducer SUM =
       new Reducer() {
+        @Override
+        public int reduce(int a, int b) {
+          return Math.addExact(a, b);
+        }
+
         @Override
         public long reduce(long a, long b) {
           return Math.addExact(a, b);
+        }
+
+        @Override
+        public float reduce(float a, float b) {
+          return a + b;
+        }
+
+        @Override
+        public double reduce(double a, double b) {
+          return a + b;
         }
       };
 }
