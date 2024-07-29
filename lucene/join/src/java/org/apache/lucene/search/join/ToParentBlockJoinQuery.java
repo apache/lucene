6b3b13212f88e35af88a5b2fd3362ea37c3a3546
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-07-29 14:15:54 -0400

    PR feedback

--- lucene/join/src/java/org/apache/lucene/search/join/ToParentBlockJoinQuery.java
+++ lucene/join/src/java/org/apache/lucene/search/join/ToParentBlockJoinQuery.java
@@ -104,6 +104,8 @@ public class ToParentBlockJoinQuery extends Query {
       // if the score is needed and the score mode is not max, we force the collection mode to
       // COMPLETE because the
       // child query cannot skip non-competitive documents.
+      // weightScoreMode.needsScores() will always be true here, but keep the check to make the
+      // logic clearer.
       childWeight =
           childQuery.createWeight(
               searcher,
@@ -162,7 +164,9 @@ public class ToParentBlockJoinQuery extends Query {
 
         @Override
         public void setTopLevelScoringClause() throws IOException {
-          childScorerSupplier.setTopLevelScoringClause();
+          if (scoreMode == ScoreMode.Max) {
+            childScorerSupplier.setTopLevelScoringClause();
+          }
         }
       };
     }
