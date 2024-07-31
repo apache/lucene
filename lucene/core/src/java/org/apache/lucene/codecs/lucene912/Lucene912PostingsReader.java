e58509b226874acf07ba6e5900bd38e7eec12281
Armin Braun <me@obrown.io>
2024-07-31 19:34:35 +0200

    Wrap Executor in TaskExecutor to never reject (#13622)
    Make it so rejected tasks are execute right away on the caller thread.
    Users of the API shouldn't have to worry about rejections when we don't
    expose any upper limit to the task count that we put on the executor that
    would help in sizing a queue for the executor.

--- lucene/core/src/java/org/apache/lucene/codecs/lucene912/Lucene912PostingsReader.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene912/Lucene912PostingsReader.java
@@ -32,7 +32,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.RandomAccess;
-
 import org.apache.lucene.codecs.BlockTermState;
 import org.apache.lucene.codecs.CodecUtil;
 import org.apache.lucene.codecs.PostingsReaderBase;
