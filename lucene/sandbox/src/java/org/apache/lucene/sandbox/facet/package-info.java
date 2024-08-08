9391e2caec60933631ae3a85abc29fcf9fe7605b
Egor Potemkin <eg13reg@gmail.com>
2024-08-08 23:23:02 +0100

    Use "facet ordinals" instead of "facet IDs" in the last two places

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/package-info.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/package-info.java
@@ -18,7 +18,7 @@
  * Sandbox faceting - Collectors that compute facets. Facet Ordinals/Ids: Each doc may have
  * different facets and therefore, different facet ordinals. For example a book can have Author,
  * Publish Date, Page Count etc. as facets. The specific value for each of these Facets for a book
- * can be mapped to an ordinal. Facet Ids may be common across different book documents.
+ * can be mapped to an ordinal. Facet ordinals may be common across different book documents.
  * FacetCutter: Can interpret Facets of a specific type for a doc type and output all the Facet
  * Ordinals for the type for the doc. Facet Recorders: record data per ordinal. Some recorders may
  * compute aggregations and record per ordinal data aggregated across an index.
