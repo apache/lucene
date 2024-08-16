fcc6b35524911551a8733b2047f4af3d2e487ce8
huangbojian <1825636423@qq.com>
2024-08-16 16:59:42 +0800

    Simplify code and reduce the number of lines of code

--- lucene/core/src/java/org/apache/lucene/util/fst/FSTCompiler.java
+++ lucene/core/src/java/org/apache/lucene/util/fst/FSTCompiler.java
@@ -871,12 +871,9 @@ public class FSTCompiler<T> {
     int pos1 = 0;
     int pos2 = input.offset;
     final int pos1Stop = Math.min(lastInput.length(), input.length);
-    while (true) {
-      if (pos1 >= pos1Stop || lastInput.intAt(pos1) != input.ints[pos2]) {
-        break;
-      }
-      pos1++;
-      pos2++;
+    while (pos1 < pos1Stop && lastInput.intAt(pos1) == input.ints[pos2]) {
+        pos1++;
+        pos2++;
     }
     final int prefixLenPlus1 = pos1 + 1;
 
