d42e9c49b63d0448d5d2db9d6ece1fa62a7fdf73
Luca Cavanna <javanna@apache.org>
2024-09-09 13:05:31 +0200

    adjust facet test

--- lucene/sandbox/src/test/org/apache/lucene/sandbox/facet/TestRangeFacet.java
+++ lucene/sandbox/src/test/org/apache/lucene/sandbox/facet/TestRangeFacet.java
@@ -1555,7 +1555,7 @@ public class TestRangeFacet extends SandboxFacetTestCase {
 
     IndexReader r = writer.getReader();
 
-    IndexSearcher s = newSearcher(r, false, false);
+    IndexSearcher s = newSearcher(r, false, false, Concurrency.INTER_SEGMENT);
     // DrillSideways requires the entire range of docs to be scored at once, so it doesn't support
     // timeouts whose implementation scores one window of doc IDs at a time.
     s.setTimeout(null);
