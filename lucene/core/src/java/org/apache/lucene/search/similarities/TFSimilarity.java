b8feaa2664f36d79384f0a5ef35dc6f16bccfc63
Christine Poerschke <cpoerschke@apache.org>
2024-09-11 08:27:16 +0100

    action code review feedback w.r.t. TFSimilarity.computeNorm

--- lucene/core/src/java/org/apache/lucene/search/similarities/TFSimilarity.java
+++ lucene/core/src/java/org/apache/lucene/search/similarities/TFSimilarity.java
@@ -17,15 +17,50 @@
 package org.apache.lucene.search.similarities;
 
 import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.search.CollectionStatistics;
 import org.apache.lucene.search.TermStatistics;
+import org.apache.lucene.util.SmallFloat;
 
 /** Similarity that returns the TF as score */
 public class TFSimilarity extends Similarity {
+  private final boolean discountOverlaps;
+
+  /**
+   * TFSimilarity with these default values:
+   *
+   * <ul>
+   *   <li>{@code discountOverlaps = true}
+   * </ul>
+   */
+  public TFSimilarity() {
+    this(true);
+  }
+
+  /**
+   * @param discountOverlaps True if overlap tokens (tokens with a position of increment of zero)
+   *     are discounted from the document's length.
+   */
+  public TFSimilarity(boolean discountOverlaps) {
+    this.discountOverlaps = discountOverlaps;
+  }
+
+  /** Returns true if overlap tokens are discounted from the document's length. */
+  public boolean getDiscountOverlaps() {
+    return discountOverlaps;
+  }
 
   @Override
   public long computeNorm(FieldInvertState state) {
-    return 1; // we dont care
+    final int numTerms;
+    if (state.getIndexOptions() == IndexOptions.DOCS && state.getIndexCreatedVersionMajor() >= 8) {
+      numTerms = state.getUniqueTermCount();
+    } else if (discountOverlaps) {
+      numTerms = state.getLength() - state.getNumOverlap();
+    } else {
+      numTerms = state.getLength();
+    }
+    return SmallFloat.intToByte4(numTerms);
   }
 
   @Override
