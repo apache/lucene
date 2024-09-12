1c1058ce2e406773742778ca4adcf31b4b82c3d7
Christine Poerschke <cpoerschke@apache.org>
2024-09-12 11:23:22 +0100

    PayloadScoreQuery javadoc update w.r.t. SpanQuery use (#13731)
    (cherry picked from commit ef577ea43a991b99dd14f5668aadbea8e66795bb)

--- lucene/queries/src/java/org/apache/lucene/queries/payloads/PayloadScoreQuery.java
+++ lucene/queries/src/java/org/apache/lucene/queries/payloads/PayloadScoreQuery.java
@@ -38,7 +38,11 @@ import org.apache.lucene.search.QueryVisitor;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.util.BytesRef;
 
-/** A Query class that uses a {@link PayloadFunction} to modify the score of a wrapped SpanQuery */
+/**
+ * A Query class that uses a {@link PayloadFunction} to modify the score of a wrapped {@link
+ * SpanQuery}. A wrapped span query is used due to the way that payload values are indexed, see
+ * {@link PostingsEnum#PAYLOADS}.
+ */
 public class PayloadScoreQuery extends SpanQuery {
 
   private final SpanQuery wrappedQuery;
