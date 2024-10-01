9354aa104bf824f0b8d5ff9be67dec48c9ae9d79
Adrien Grand <jpountz@gmail.com>
2024-10-01 09:56:21 +0200

    Bump the codec version to 10.0. (#13815)
    Bump the codec version to 10.0.
    
    Lucene100Codec is the exact same file format as Lucene912Codec. This codec
    dance just makes things slightly easier to reason about since our backward
    compatibility guarantees are aligned with major version: once we drop support
    for 9.x indices, we can remove all `Lucene9XXCodec`s.

--- lucene/core/src/java/org/apache/lucene/codecs/Codec.java
+++ lucene/core/src/java/org/apache/lucene/codecs/Codec.java
@@ -55,7 +55,7 @@ public abstract class Codec implements NamedSPILoader.NamedSPI {
       return LOADER;
     }
 
-    static Codec defaultCodec = LOADER.lookup("Lucene912");
+    static Codec defaultCodec = LOADER.lookup("Lucene100");
   }
 
   private final String name;
