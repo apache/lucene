7f50732cc3e7281359c6952016cfc7927f997c63
Ignacio Vera <ivera@apache.org>
2024-10-09 12:33:34 +0200

    Avoid performance regression by constructing lazily the PointTree in NumericComparator (#13498) (#13877)

--- lucene/core/src/java/org/apache/lucene/search/comparators/NumericComparator.java
+++ lucene/core/src/java/org/apache/lucene/search/comparators/NumericComparator.java
@@ -95,7 +95,8 @@ public abstract class NumericComparator<T extends Number> extends FieldComparato
     private final LeafReaderContext context;
     protected final NumericDocValues docValues;
     private final PointValues pointValues;
-    private final PointValues.PointTree pointTree;
+    // lazily constructed to avoid performance overhead when this is not used
+    private PointValues.PointTree pointTree;
     // if skipping functionality should be enabled on this segment
     private final boolean enableSkipping;
     private final int maxDoc;
@@ -139,7 +140,6 @@ public abstract class NumericComparator<T extends Number> extends FieldComparato
                   + " expected "
                   + bytesCount);
         }
-        this.pointTree = pointValues.getPointTree();
         this.enableSkipping = true; // skipping is enabled when points are available
         this.maxDoc = context.reader().maxDoc();
         this.competitiveIterator = DocIdSetIterator.all(maxDoc);
@@ -147,7 +147,6 @@ public abstract class NumericComparator<T extends Number> extends FieldComparato
           encodeTop();
         }
       } else {
-        this.pointTree = null;
         this.enableSkipping = false;
         this.maxDoc = 0;
       }
@@ -273,7 +272,8 @@ public abstract class NumericComparator<T extends Number> extends FieldComparato
 
       final long threshold = iteratorCost >>> 3;
 
-      if (PointValues.isEstimatedPointCountGreaterThanOrEqualTo(visitor, pointTree, threshold)) {
+      if (PointValues.isEstimatedPointCountGreaterThanOrEqualTo(
+          visitor, getPointTree(), threshold)) {
         // the new range is not selective enough to be worth materializing, it doesn't reduce number
         // of docs at least 8x
         updateSkipInterval(false);
@@ -288,6 +288,13 @@ public abstract class NumericComparator<T extends Number> extends FieldComparato
       competitiveIterator = result.build().iterator();
       iteratorCost = competitiveIterator.cost();
       updateSkipInterval(true);
+    }
+
+    private PointValues.PointTree getPointTree() throws IOException {
+      if (pointTree == null) {
+        pointTree = pointValues.getPointTree();
+      }
+      return pointTree;
     }
 
     private void updateSkipInterval(boolean success) {
