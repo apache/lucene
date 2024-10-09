7239ab150d04748544b4a8740e2e15c5dc3dad8f
Mike McCandless <mikemccand@apache.org>
2024-10-09 15:12:18 -0400

    bump 9.12.x version to 9.12.1 and add bwc indices for 9.12.0

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -327,9 +327,16 @@ public final class Version {
   /**
    * Match settings and bugs in Lucene's 9.12.0 release.
    *
+   * @deprecated Use latest
+   */
+  @Deprecated public static final Version LUCENE_9_12_0 = new Version(9, 12, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 9.12.1 release.
+   *
    * <p>Use this to get the latest &amp; greatest settings, bug fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_9_12_0 = new Version(9, 12, 0);
+  public static final Version LUCENE_9_12_1 = new Version(9, 12, 1);
 
   // To add a new version:
   //  * Only add above this comment
@@ -345,7 +352,7 @@ public final class Version {
    * <b>re-test your entire application</b> to ensure it behaves as expected, as some defaults may
    * have changed and may break functionality in your application.
    */
-  public static final Version LATEST = LUCENE_9_12_0;
+  public static final Version LATEST = LUCENE_9_12_1;
 
   /**
    * Constant for backwards compatibility.
