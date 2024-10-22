f95b59fca9a2505596383fba39c760b3dede2a4e
Laurent <jakubinal@lexum.com>
2024-10-22 17:45:30 -0400

    tidied2

--- lucene/core/src/java/org/apache/lucene/search/DisjunctionMaxQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/DisjunctionMaxQuery.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import org.apache.lucene.index.LeafReaderContext;
 
 /**
--- lucene/core/src/test/org/apache/lucene/search/TestDisjunctionMaxQuery.java
+++ lucene/core/src/test/org/apache/lucene/search/TestDisjunctionMaxQuery.java
@@ -25,6 +25,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
