apache/lucene

commit bf69c3cf4370c1afc932e3d94703b3cea75e5415

Make some BooleanQuery methods public and a new `#add(Collection)` method for BQ builder (#13950)

lucene/core/src/java/org/apache/lucene/search/BooleanQuery.java
@@ -88,6 +88,22 @@ public class BooleanQuery extends Query implements Iterable<BooleanClause> {
     }
 
     /**
+     * Add a collection of BooleanClauses to this {@link Builder}. Note that the order in which
+     * clauses are added does not have any impact on matching documents or query performance.
+     *
+     * @throws IndexSearcher.TooManyClauses if the new number of clauses exceeds the maximum clause
+     *     number
+     */
+    public Builder add(Collection<BooleanClause> collection) {
+      // see #addClause(BooleanClause)
+      if ((clauses.size() + collection.size()) > IndexSearcher.maxClauseCount) {
+        throw new IndexSearcher.TooManyClauses();
+      }
+      clauses.addAll(collection);
+      return this;
+    }
+
+    /**
      * Add a new clause to this {@link Builder}. Note that the order in which clauses are added does
      * not have any impact on matching documents or query performance.
      *
@@ -136,7 +152,7 @@ public class BooleanQuery extends Query implements Iterable<BooleanClause> {
   }
 
   /** Return the collection of queries for the given {@link Occur}. */
-  Collection<Query> getClauses(Occur occur) {
+  public Collection<Query> getClauses(Occur occur) {
     return clauseSets.get(occur);
   }
 

commit 15c0e3a26181668610ba9f212e387f6ccd1f5c17

This fixes immutability of clauseSets (broken by #13950)

lucene/core/src/java/org/apache/lucene/search/BooleanQuery.java
@@ -124,7 +124,8 @@ public class BooleanQuery extends Query implements Iterable<BooleanClause> {
 
   private final int minimumNumberShouldMatch;
   private final List<BooleanClause> clauses; // used for toString() and getClauses()
-  private final Map<Occur, Collection<Query>> clauseSets; // used for equals/hashcode
+  // WARNING: Do not let clauseSets escape from this class as it breaks immutability:
+  private final Map<Occur, Collection<Query>> clauseSets; // used for equals/hashCode
 
   private BooleanQuery(int minimumNumberShouldMatch, BooleanClause[] clauses) {
     this.minimumNumberShouldMatch = minimumNumberShouldMatch;
@@ -153,7 +154,9 @@ public class BooleanQuery extends Query implements Iterable<BooleanClause> {
 
   /** Return the collection of queries for the given {@link Occur}. */
   public Collection<Query> getClauses(Occur occur) {
-    return clauseSets.get(occur);
+    // turn this immutable here, because we need to preserve the correct collection types for
+    // equals/hashCode!
+    return Collections.unmodifiableCollection(clauseSets.get(occur));
   }
 
   /**

lucene/core/src/test/org/apache/lucene/search/TestBooleanQuery.java
@@ -1383,4 +1383,36 @@ public class TestBooleanQuery extends LuceneTestCase {
           }
         });
   }
+
+  public void testClauseSetsImmutability() throws Exception {
+    Term a = new Term("f", "a");
+    Term b = new Term("f", "b");
+    Term c = new Term("f", "c");
+    Term d = new Term("f", "d");
+    BooleanQuery.Builder bqBuilder = new BooleanQuery.Builder();
+    bqBuilder.add(new TermQuery(a), Occur.SHOULD);
+    bqBuilder.add(new TermQuery(a), Occur.SHOULD);
+    bqBuilder.add(new TermQuery(b), Occur.MUST);
+    bqBuilder.add(new TermQuery(b), Occur.MUST);
+    bqBuilder.add(new TermQuery(c), Occur.FILTER);
+    bqBuilder.add(new TermQuery(c), Occur.FILTER);
+    bqBuilder.add(new TermQuery(d), Occur.MUST_NOT);
+    bqBuilder.add(new TermQuery(d), Occur.MUST_NOT);
+    BooleanQuery bq = bqBuilder.build();
+    // should and must are not dedupliacated
+    assertEquals(2, bq.getClauses(Occur.SHOULD).size());
+    assertEquals(2, bq.getClauses(Occur.MUST).size());
+    // filter and must not are deduplicated
+    assertEquals(1, bq.getClauses(Occur.FILTER).size());
+    assertEquals(1, bq.getClauses(Occur.MUST_NOT).size());
+    // check immutability
+    for (var occur : Occur.values()) {
+      assertThrows(
+          UnsupportedOperationException.class,
+          () -> bq.getClauses(occur).add(new MatchNoDocsQuery()));
+    }
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> bq.clauses().add(new BooleanClause(new MatchNoDocsQuery(), Occur.SHOULD)));
+  }
 }