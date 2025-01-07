0e9f6d4bc9a98eb33d594409ce8e4b3a6b4b1a06
Ishan Chattopadhyaya <ishan@apache.org>
2025-01-07 21:28:17 +0530

    Test fixes

--- lucene/sandbox/src/test/org/apache/lucene/sandbox/vectorsearch/TestCuVS.java
+++ lucene/sandbox/src/test/org/apache/lucene/sandbox/vectorsearch/TestCuVS.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SuppressSysoutChecks(bugUrl = "prints info from within cuvs")
-public class IntegrationTest extends LuceneTestCase {
+public class TestCuVS extends LuceneTestCase {
 
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
