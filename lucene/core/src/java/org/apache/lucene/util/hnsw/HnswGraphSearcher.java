a71e93602e4c67b9317b5c416b72f8d93c679b6e
Tommaso Teofili <tommaso.teofili@elastic.co>
2025-01-15 15:57:48 +0100

    tidy

--- lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraphSearcher.java
+++ lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraphSearcher.java
@@ -20,8 +20,8 @@ package org.apache.lucene.util.hnsw;
 import static org.apache.lucene.search.DocIdSetIterator.NO_MORE_DOCS;
 
 import java.io.IOException;
-import org.apache.lucene.search.HnswKnnCollector;
 import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.HnswKnnCollector;
 import org.apache.lucene.search.KnnCollector;
 import org.apache.lucene.search.TopKnnCollector;
 import org.apache.lucene.search.knn.EntryPointProvider;
