5ec63662ecbc754f74742447e93954192e3849d3
Egor Potemkin <eg13reg@gmail.com>
2025-01-27 17:35:23 +0300

    Initial commit for utility classes for the sandbox facets module
    Simplifies requesting taxonomy and long range facets
    Makes sure we don't count docs two times for the same index field when there are multiple requests
    Returns FacetResult - to make it easier to migrate for classic facets module users
    
    TODOs:
    - better names - maybe use names suggested in TK
    - implement for all existing facet cutters
    - add comprehensive tests
    - fix total value where possible in tests

---
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/utils/FacetBuilder.java
@@ -0,0 +1,34 @@
+package org.apache.lucene.sandbox.facet.utils;
+
+import org.apache.lucene.facet.FacetResult;
+import org.apache.lucene.sandbox.facet.FacetFieldCollectorManager;
+
+/**
+ * End-to-end (request, collection and results) management of a single facet dimension.
+ * Requires implementation for each facet type (taxonomy, ranges, etc)*/
+public interface FacetBuilder {
+
+    /** Init attrs required for collection phase, or reuse ones from
+     * the argument that uses the same attrs for collection.
+     *
+     * @return similar if it is not null, otherwise this. */
+    FacetBuilder initOrReuseCollector(FacetBuilder similar);
+
+
+    /** Create {@link org.apache.lucene.search.CollectorManager} for this facet (dimension). */
+    FacetFieldCollectorManager<?> getCollectorManager();
+
+    /** Uniaue key for collection time.
+     * Multiple facet requests can use the same collector, e.g. when they require
+     * counting matches for the same field.
+     * <p>
+     * Default: this, to use unique collector for this request.
+     * */
+    default Object collectionKey() {
+        return this;
+    }
+
+    /** Get results for this request. */
+    FacetResult getResult();
+
+}
