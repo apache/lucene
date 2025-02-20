ccdad268e2daea6dece81481df9b3c32e7c996be
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2025-02-20 11:37:01 -0500

    Reduce knn recall test flakiness

--- lucene/test-framework/src/java/org/apache/lucene/tests/codecs/asserting/AssertingKnnVectorsFormat.java
+++ lucene/test-framework/src/java/org/apache/lucene/tests/codecs/asserting/AssertingKnnVectorsFormat.java
@@ -62,6 +62,11 @@ public class AssertingKnnVectorsFormat extends KnnVectorsFormat {
     return KnnVectorsFormat.DEFAULT_MAX_DIMENSIONS;
   }
 
+  @Override
+  public String toString() {
+    return "AssertingKnnVectorsFormat{" + "delegate=" + delegate + '}';
+  }
+
   static class AssertingKnnVectorsWriter extends KnnVectorsWriter {
     final KnnVectorsWriter delegate;
 
