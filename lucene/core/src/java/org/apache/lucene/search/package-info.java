e568579b9a09b7b4416ada64b2f6f6e0fff646cb
Adrien Grand <jpountz@gmail.com>
2025-02-27 22:19:56 +0100

    Update lucene/core/src/java/org/apache/lucene/search/package-info.java
    Co-authored-by: Robert Muir <rcmuir@gmail.com>

--- lucene/core/src/java/org/apache/lucene/search/package-info.java
+++ lucene/core/src/java/org/apache/lucene/search/package-info.java
@@ -382,7 +382,7 @@
  * produce scores that not only have different ranges, but also different distributions within this
  * range. A robust way of combining multiple retrieval pipelines consists of combining the top hits
  * that they produce through their ranks rather than through their scores using reciprocal rank
- * fusion. This is exposed via {@link org.apache.lucene.search.TopDocs#rrf(int, int, TopDocs[])}.
+ * fusion. This is exposed via {@link org.apache.lucene.search.TopDocs#rrf(int topN, int k, TopDocs[] hits)}.
  *
  * <h2>Custom Queries &mdash; Expert Level</h2>
  *
