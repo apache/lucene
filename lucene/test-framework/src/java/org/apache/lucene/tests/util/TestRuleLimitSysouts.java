50b57322ea200cf2c92bfc5a03f31427251099fd
Dawid Weiss <dawid.weiss@carrotsearch.com>
2025-02-27 20:44:32 +0100

    Fix TestSysoutLimits by making nested test classes not extend LuceneTestCase, which prints occasional warnings during randomized setup. #14307

--- lucene/test-framework/src/java/org/apache/lucene/tests/util/TestRuleLimitSysouts.java
+++ lucene/test-framework/src/java/org/apache/lucene/tests/util/TestRuleLimitSysouts.java
@@ -207,6 +207,7 @@ public class TestRuleLimitSysouts extends TestRuleAdapter {
       checkCaptureStreams();
     }
     resetCaptureState();
+    var bef = bytesWritten.get();
     applyClassAnnotations();
   }
 
