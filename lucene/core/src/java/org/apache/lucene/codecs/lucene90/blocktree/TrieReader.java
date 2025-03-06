63b0cdd9641e0d042d2dabb019696529b26b7d5a
guofeng.my <guofeng.my@bytedance.com>
2025-03-06 17:13:00 +0800

    license

--- lucene/core/src/java/org/apache/lucene/codecs/lucene90/blocktree/TrieReader.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene90/blocktree/TrieReader.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.lucene.codecs.lucene90.blocktree;
 
 import static org.apache.lucene.codecs.lucene90.blocktree.Trie.PositionStrategy.ARRAY;
@@ -11,7 +27,6 @@ import org.apache.lucene.store.RandomAccessInput;
 
 class TrieReader {
 
-  private static final long META_BYTES = 3L;
   private static final long NO_OUTPUT = -1;
   static final int HAS_OUTPUT = 1 << 6;
   static final int SINGLE_CHILD = 1 << 7;
