52f3c3e6e5ef5a552b74485729f98ef26b2ef44a
Ignacio Vera <ivera@apache.org>
2025-03-13 16:45:02 +0100

    deduplicate standard BKDConfig records (#14338)
    This should save some heap usage on the BKDReader.

--- lucene/core/src/java/org/apache/lucene/util/bkd/BKDReader.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/BKDReader.java
@@ -68,7 +68,7 @@ public class BKDReader extends PointValues {
     }
     final int maxPointsInLeafNode = metaIn.readVInt();
     final int bytesPerDim = metaIn.readVInt();
-    config = new BKDConfig(numDims, numIndexDims, bytesPerDim, maxPointsInLeafNode);
+    config = BKDConfig.of(numDims, numIndexDims, bytesPerDim, maxPointsInLeafNode);
 
     // Read index:
     numLeaves = metaIn.readVInt();
