ea292f68bb4087ada8cf5ae76e4b4a0d82eae98f
Ignacio Vera <ivera@apache.org>
2025-03-15 19:53:55 +0100

    Specialize Single block DirectMonotonicReader

--- lucene/core/src/java/org/apache/lucene/util/packed/DirectMonotonicReader.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/DirectMonotonicReader.java
@@ -27,130 +27,23 @@ import org.apache.lucene.util.LongValues;
  *
  * @see DirectMonotonicWriter
  */
-public final class DirectMonotonicReader extends LongValues {
+public abstract sealed class DirectMonotonicReader extends LongValues
+    permits DirectMonotonicReader.MultiBlockDirectMonotonicReader,
+        DirectMonotonicReader.SingleBlockDirectMonotonicReader {
+
+  private static final Meta SINGLE_ZERO_BLOCK = new SingleBlockMeta(0L, 0.0f, (byte) 0, 0L);
 
   /**
    * In-memory metadata that needs to be kept around for {@link DirectMonotonicReader} to read data
    * from disk.
    */
-  public static class Meta {
+  public abstract static sealed class Meta permits SingleBlockMeta, MultiBlockMeta {
 
-    // Use a shift of 63 so that there would be a single block regardless of the number of values.
-    private static final Meta SINGLE_ZERO_BLOCK = new Meta(1L, 63);
-
-    private final int blockShift;
-    private final int numBlocks;
-    private final long[] mins;
-    private final float[] avgs;
-    private final byte[] bpvs;
-    private final long[] offsets;
-
-    Meta(long numValues, int blockShift) {
-      this.blockShift = blockShift;
-      long numBlocks = numValues >>> blockShift;
-      if ((numBlocks << blockShift) < numValues) {
-        numBlocks += 1;
-      }
-      this.numBlocks = (int) numBlocks;
-      this.mins = new long[this.numBlocks];
-      this.avgs = new float[this.numBlocks];
-      this.bpvs = new byte[this.numBlocks];
-      this.offsets = new long[this.numBlocks];
-    }
-  }
-
-  /**
-   * Load metadata from the given {@link IndexInput}.
-   *
-   * @see DirectMonotonicReader#getInstance(Meta, RandomAccessInput)
-   */
-  public static Meta loadMeta(IndexInput metaIn, long numValues, int blockShift)
-      throws IOException {
-    boolean allValuesZero = true;
-    Meta meta = new Meta(numValues, blockShift);
-    for (int i = 0; i < meta.numBlocks; ++i) {
-      long min = metaIn.readLong();
-      meta.mins[i] = min;
-      int avgInt = metaIn.readInt();
-      meta.avgs[i] = Float.intBitsToFloat(avgInt);
-      meta.offsets[i] = metaIn.readLong();
-      byte bpvs = metaIn.readByte();
-      meta.bpvs[i] = bpvs;
-      allValuesZero = allValuesZero && min == 0L && avgInt == 0 && bpvs == 0;
-    }
-    // save heap in case all values are zero
-    return allValuesZero ? Meta.SINGLE_ZERO_BLOCK : meta;
-  }
-
-  /** Retrieves a non-merging instance from the specified slice. */
-  public static DirectMonotonicReader getInstance(Meta meta, RandomAccessInput data)
-      throws IOException {
-    return getInstance(meta, data, false);
-  }
-
-  /** Retrieves an instance from the specified slice. */
-  public static DirectMonotonicReader getInstance(
-      Meta meta, RandomAccessInput data, boolean merging) throws IOException {
-    final LongValues[] readers = new LongValues[meta.numBlocks];
-    for (int i = 0; i < meta.numBlocks; ++i) {
-      if (meta.bpvs[i] == 0) {
-        readers[i] = LongValues.ZEROES;
-      } else if (merging
-          && i < meta.numBlocks - 1 // we only know the number of values for the last block
-          && meta.blockShift >= DirectReader.MERGE_BUFFER_SHIFT) {
-        readers[i] =
-            DirectReader.getMergeInstance(
-                data, meta.bpvs[i], meta.offsets[i], 1L << meta.blockShift);
-      } else {
-        readers[i] = DirectReader.getInstance(data, meta.bpvs[i], meta.offsets[i]);
-      }
-    }
-
-    return new DirectMonotonicReader(meta.blockShift, readers, meta.mins, meta.avgs, meta.bpvs);
-  }
-
-  private final int blockShift;
-  private final long blockMask;
-  private final LongValues[] readers;
-  private final long[] mins;
-  private final float[] avgs;
-  private final byte[] bpvs;
-
-  private DirectMonotonicReader(
-      int blockShift, LongValues[] readers, long[] mins, float[] avgs, byte[] bpvs) {
-    this.blockShift = blockShift;
-    this.blockMask = (1L << blockShift) - 1;
-    this.readers = readers;
-    this.mins = mins;
-    this.avgs = avgs;
-    this.bpvs = bpvs;
-    if (readers.length != mins.length
-        || readers.length != avgs.length
-        || readers.length != bpvs.length) {
-      throw new IllegalArgumentException();
-    }
-  }
-
-  @Override
-  public long get(long index) {
-    final int block = (int) (index >>> blockShift);
-    final long blockIndex = index & blockMask;
-    final long delta = readers[block].get(blockIndex);
-    return mins[block] + (long) (avgs[block] * blockIndex) + delta;
+    protected abstract DirectMonotonicReader getInstance(RandomAccessInput data, boolean merging);
   }
 
   /** Get lower/upper bounds for the value at a given index without hitting the direct reader. */
-  private long[] getBounds(long index) {
-    final int block = Math.toIntExact(index >>> blockShift);
-    final long blockIndex = index & blockMask;
-    final long lowerBound = mins[block] + (long) (avgs[block] * blockIndex);
-    final long upperBound = lowerBound + (1L << bpvs[block]) - 1;
-    if (bpvs[block] == 64 || upperBound < lowerBound) { // overflow
-      return new long[] {Long.MIN_VALUE, Long.MAX_VALUE};
-    } else {
-      return new long[] {lowerBound, upperBound};
-    }
-  }
+  protected abstract long[] getBounds(long mid);
 
   /**
    * Return the index of a key if it exists, or its insertion point otherwise like {@link
@@ -158,7 +51,7 @@ public final class DirectMonotonicReader extends LongValues {
    *
    * @see Arrays#binarySearch(long[], int, int, long)
    */
-  public long binarySearch(long fromIndex, long toIndex, long key) {
+  public final long binarySearch(long fromIndex, long toIndex, long key) {
     if (fromIndex < 0 || fromIndex > toIndex) {
       throw new IllegalArgumentException("fromIndex=" + fromIndex + ",toIndex=" + toIndex);
     }
@@ -185,7 +78,205 @@ public final class DirectMonotonicReader extends LongValues {
         }
       }
     }
-
     return -1 - lo;
+  }
+
+  /**
+   * Load metadata from the given {@link IndexInput}.
+   *
+   * @see DirectMonotonicReader#getInstance(Meta, RandomAccessInput)
+   */
+  public static Meta loadMeta(IndexInput metaIn, long numValues, int blockShift)
+      throws IOException {
+    final int numBlocks = numBlocks(numValues, blockShift);
+    if (numBlocks == 1) {
+      return loadSingleBlockMeta(metaIn);
+    } else {
+      return loadMultiBlockMeta(metaIn, numBlocks, blockShift);
+    }
+  }
+
+  private static int numBlocks(long numValues, int blockShift) {
+    long numBlocks = numValues >>> blockShift;
+    if ((numBlocks << blockShift) < numValues) {
+      numBlocks += 1;
+    }
+    return (int) numBlocks;
+  }
+
+  private static Meta loadSingleBlockMeta(IndexInput metaIn) throws IOException {
+    final long min = metaIn.readLong();
+    final float avgInt = Float.intBitsToFloat(metaIn.readInt());
+    final long offsets = metaIn.readLong();
+    final byte bpvs = metaIn.readByte();
+    final boolean allValuesZero = min == 0L && avgInt == 0 && bpvs == 0;
+    // save heap in case all values are zero
+    return allValuesZero ? SINGLE_ZERO_BLOCK : new SingleBlockMeta(min, avgInt, bpvs, offsets);
+  }
+
+  private static Meta loadMultiBlockMeta(IndexInput metaIn, int numBlocks, int blockShift)
+      throws IOException {
+    final MultiBlockMeta meta = new MultiBlockMeta(numBlocks, blockShift);
+    boolean allValuesZero = true;
+    for (int i = 0; i < numBlocks; ++i) {
+      final long min = metaIn.readLong();
+      meta.mins[i] = min;
+      final int avgInt = metaIn.readInt();
+      meta.avgs[i] = Float.intBitsToFloat(avgInt);
+      meta.offsets[i] = metaIn.readLong();
+      final byte bpvs = metaIn.readByte();
+      meta.bpvs[i] = bpvs;
+      allValuesZero = allValuesZero && min == 0L && avgInt == 0 && bpvs == 0;
+    }
+    // save heap in case all values are zero
+    return allValuesZero ? SINGLE_ZERO_BLOCK : meta;
+  }
+
+  /** Retrieves a non-merging instance from the specified slice. */
+  public static DirectMonotonicReader getInstance(Meta meta, RandomAccessInput data)
+      throws IOException {
+    return getInstance(meta, data, false);
+  }
+
+  /** Retrieves an instance from the specified slice. */
+  public static DirectMonotonicReader getInstance(
+      Meta meta, RandomAccessInput data, boolean merging) {
+    return meta.getInstance(data, merging);
+  }
+
+  private static final class SingleBlockMeta extends Meta {
+    private final long min;
+    private final float avg;
+    private final byte bpv;
+    private final long offset;
+
+    private SingleBlockMeta(long min, float avg, byte bpv, long offset) {
+      this.min = min;
+      this.avg = avg;
+      this.bpv = bpv;
+      // TODO is this always zero?
+      this.offset = offset;
+    }
+
+    @Override
+    protected DirectMonotonicReader getInstance(RandomAccessInput data, boolean merging) {
+      LongValues reader;
+      if (bpv == 0) {
+        reader = LongValues.ZEROES;
+      } else {
+        reader = DirectReader.getInstance(data, bpv, offset);
+      }
+      return new SingleBlockDirectMonotonicReader(reader, min, avg, bpv);
+    }
+  }
+
+  protected static final class SingleBlockDirectMonotonicReader extends DirectMonotonicReader {
+    private final LongValues reader;
+    private final long min;
+    private final float avg;
+    private final byte bpv;
+
+    private SingleBlockDirectMonotonicReader(LongValues reader, long min, float avg, byte bpv) {
+      this.reader = reader;
+      this.min = min;
+      this.avg = avg;
+      this.bpv = bpv;
+    }
+
+    @Override
+    public long get(long index) {
+      final long delta = reader.get(index);
+      return min + (long) (avg * index) + delta;
+    }
+
+    @Override
+    protected long[] getBounds(long index) {
+      final long lowerBound = min + (long) (avg * index);
+      final long upperBound = lowerBound + (1L << bpv) - 1;
+      if (bpv == 64 || upperBound < lowerBound) { // overflow
+        return new long[] {Long.MIN_VALUE, Long.MAX_VALUE};
+      } else {
+        return new long[] {lowerBound, upperBound};
+      }
+    }
+  }
+
+  private static final class MultiBlockMeta extends Meta {
+    private final int blockShift;
+    private final long[] mins;
+    private final float[] avgs;
+    private final byte[] bpvs;
+    private final long[] offsets;
+
+    private MultiBlockMeta(int numBlocks, int blockShift) {
+      this.blockShift = blockShift;
+      this.mins = new long[numBlocks];
+      this.avgs = new float[numBlocks];
+      this.bpvs = new byte[numBlocks];
+      this.offsets = new long[numBlocks];
+    }
+
+    @Override
+    protected DirectMonotonicReader getInstance(RandomAccessInput data, boolean merging) {
+      final int numBlocks = mins.length;
+      final LongValues[] readers = new LongValues[numBlocks];
+      for (int i = 0; i < numBlocks; ++i) {
+        if (bpvs[i] == 0) {
+          readers[i] = LongValues.ZEROES;
+        } else if (merging
+            && i < numBlocks - 1 // we only know the number of values for the last block
+            && blockShift >= DirectReader.MERGE_BUFFER_SHIFT) {
+          readers[i] = DirectReader.getMergeInstance(data, bpvs[i], offsets[i], 1L << blockShift);
+        } else {
+          readers[i] = DirectReader.getInstance(data, bpvs[i], offsets[i]);
+        }
+      }
+      return new MultiBlockDirectMonotonicReader(blockShift, readers, mins, avgs, bpvs);
+    }
+  }
+
+  protected static final class MultiBlockDirectMonotonicReader extends DirectMonotonicReader {
+    private final int blockShift;
+    private final long blockMask;
+    private final LongValues[] readers;
+    private final long[] mins;
+    private final float[] avgs;
+    private final byte[] bpvs;
+
+    private MultiBlockDirectMonotonicReader(
+        int blockShift, LongValues[] readers, long[] mins, float[] avgs, byte[] bpvs) {
+      this.blockMask = (1L << blockShift) - 1;
+      this.blockShift = blockShift;
+      this.readers = readers;
+      this.mins = mins;
+      this.avgs = avgs;
+      this.bpvs = bpvs;
+      if (readers.length != mins.length
+          || readers.length != avgs.length
+          || readers.length != bpvs.length) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    @Override
+    public long get(long index) {
+      final int block = (int) (index >>> blockShift);
+      final long blockIndex = index & blockMask;
+      final long delta = readers[block].get(blockIndex);
+      return mins[block] + (long) (avgs[block] * blockIndex) + delta;
+    }
+
+    @Override
+    protected long[] getBounds(long index) {
+      final int block = Math.toIntExact(index >>> blockShift);
+      final long blockIndex = index & blockMask;
+      final long lowerBound = mins[block] + (long) (avgs[block] * blockIndex);
+      final long upperBound = lowerBound + (1L << bpvs[block]) - 1;
+      if (bpvs[block] == 64 || upperBound < lowerBound) { // overflow
+        return new long[] {Long.MIN_VALUE, Long.MAX_VALUE};
+      } else {
+        return new long[] {lowerBound, upperBound};
+      }
+    }
   }
 }
