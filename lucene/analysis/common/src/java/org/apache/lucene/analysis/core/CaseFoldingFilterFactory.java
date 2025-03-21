1aa26c188e30def38c86eb158aec98ae2478b6fa
Robert Muir <rmuir@apache.org>
2025-03-21 15:55:51 -0400

    Add CaseFolding.fold(), inverse of expand(), move to UnicodeUtil, add filter
    Regexp has the ability to erase case differences at query time (the slow
    way), but there's no corresponding ability to do it the fast-way: at
    index time.
    
    There's LowerCaseFilter, but LowerCaseFilter normalizes text for display
    purposes, which is different than case folding which eliminates case
    differences and is appropriate for search.
    
    Generate fold() data in a similar way as expand() data. Expose via
    UnicodeUtil and tableize basic latin for performance. Add
    CaseFoldingFilter.
    
    No Analyzer chains have been modified yet, but we should be able to
    improve Unicode support by swapping out LowerCaseFilter as a followup.
    Some filters such as GreekLowerCaseFilter can probably be eliminated.

---
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/core/CaseFoldingFilterFactory.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.lucene.analysis.core;
+
+import java.util.Map;
+import org.apache.lucene.analysis.CaseFoldingFilter;
+import org.apache.lucene.analysis.TokenFilterFactory;
+import org.apache.lucene.analysis.TokenStream;
+
+/**
+ * Factory for {@link CaseFoldingFilter}.
+ *
+ * <pre class="prettyprint">
+ * &lt;fieldType name="text_fold" class="solr.TextField" positionIncrementGap="100"&gt;
+ *   &lt;analyzer&gt;
+ *     &lt;tokenizer class="solr.WhitespaceTokenizerFactory"/&gt;
+ *     &lt;filter class="solr.CaseFoldingFilterFactory"/&gt;
+ *   &lt;/analyzer&gt;
+ * &lt;/fieldType&gt;</pre>
+ *
+ * @since 10.2.0
+ * @lucene.spi {@value #NAME}
+ */
+public class CaseFoldingFilterFactory extends TokenFilterFactory {
+
+  /** SPI name */
+  public static final String NAME = "casefold";
+
+  /** Creates a new CaseFoldingFilterFactory */
+  public CaseFoldingFilterFactory(Map<String, String> args) {
+    super(args);
+    if (!args.isEmpty()) {
+      throw new IllegalArgumentException("Unknown parameters: " + args);
+    }
+  }
+
+  /** Default ctor for compatibility with SPI */
+  public CaseFoldingFilterFactory() {
+    throw defaultCtorException();
+  }
+
+  @Override
+  public TokenStream create(TokenStream input) {
+    return new CaseFoldingFilter(input);
+  }
+
+  @Override
+  public TokenStream normalize(TokenStream input) {
+    return create(input);
+  }
+}
