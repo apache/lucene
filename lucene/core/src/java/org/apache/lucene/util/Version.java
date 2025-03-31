5b6bb4c6f9573cbb2b842ae0994945bd0a91544d
Ignacio Vera <ivera@apache.org>
2025-03-31 08:10:19 +0200

    Add next minor version 10.3.0

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -185,11 +185,16 @@ public final class Version {
   @Deprecated public static final Version LUCENE_10_1_0 = new Version(10, 1, 0);
 
   /**
-   * Match settings and bugs in Lucene's 10.2.0 release.
+   * @deprecated (10.3.0) Use latest
+   */
+  @Deprecated public static final Version LUCENE_10_2_0 = new Version(10, 2, 0);
+
+  /**
+   * Match settings and bugs in Lucene's 10.3.0 release.
    *
    * <p>Use this to get the latest &amp; greatest settings, bug fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_10_2_0 = new Version(10, 2, 0);
+  public static final Version LUCENE_10_3_0 = new Version(10, 3, 0);
 
   // To add a new version:
   //  * Only add above this comment
@@ -205,7 +210,7 @@ public final class Version {
    * <b>re-test your entire application</b> to ensure it behaves as expected, as some defaults may
    * have changed and may break functionality in your application.
    */
-  public static final Version LATEST = LUCENE_10_2_0;
+  public static final Version LATEST = LUCENE_10_3_0;
 
   /**
    * Constant for backwards compatibility.
