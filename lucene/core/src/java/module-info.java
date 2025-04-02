a2d49e92598032821ac7474402ab64ab8ffdd2d2
guofeng.my <guofeng.my@bytedance.com>
2025-04-02 15:57:35 +0800

    export blocktree

--- lucene/core/src/java/module-info.java
+++ lucene/core/src/java/module-info.java
@@ -33,6 +33,7 @@ module org.apache.lucene.core {
   exports org.apache.lucene.codecs.lucene99;
   exports org.apache.lucene.codecs.lucene101;
   exports org.apache.lucene.codecs.lucene102;
+  exports org.apache.lucene.codecs.lucene103.blocktree;
   exports org.apache.lucene.codecs.lucene103;
   exports org.apache.lucene.codecs.perfield;
   exports org.apache.lucene.codecs;
