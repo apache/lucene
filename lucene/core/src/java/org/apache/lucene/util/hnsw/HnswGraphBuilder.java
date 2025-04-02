ba32e512c89abd8ecbc18014307896c637da3fbe
Tommaso Teofili <tommaso.teofili@elastic.co>
2025-04-02 16:20:13 +0200

    Add a HNSW collector that exits early when nearest neighbor queue saturates (#14094)
    * Add a HNSW early termination based on nearest neighbor queue saturation
    
    Co-authored-by: Benjamin Trent <ben.w.trent@gmail.com>
    (cherry picked from commit 525bf34bfdfc16cc220d326d2cf30541f1afef29)

--- lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraphBuilder.java
+++ lucene/core/src/java/org/apache/lucene/util/hnsw/HnswGraphBuilder.java
@@ -642,7 +642,7 @@ public class HnswGraphBuilder implements HnswBuilder {
 
     @Override
     public KnnSearchStrategy getSearchStrategy() {
-      throw new IllegalArgumentException("Should not use unique strategy during graph building");
+      return null;
     }
   }
 }
