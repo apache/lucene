773be41dd727056b1cd7ddfbe254b81ababee906
guofeng.my <guofeng.my@bytedance.com>
2025-04-04 21:02:58 +0800

    change 101

--- lucene/core/src/java/org/apache/lucene/codecs/lucene101/Lucene101PostingsFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene101/Lucene101PostingsFormat.java
@@ -24,8 +24,8 @@ import org.apache.lucene.codecs.FieldsProducer;
 import org.apache.lucene.codecs.PostingsFormat;
 import org.apache.lucene.codecs.PostingsReaderBase;
 import org.apache.lucene.codecs.PostingsWriterBase;
-import org.apache.lucene.codecs.lucene90.blocktree.Lucene90BlockTreeTermsReader;
-import org.apache.lucene.codecs.lucene90.blocktree.Lucene90BlockTreeTermsWriter;
+import org.apache.lucene.codecs.lucene103.blocktree.Lucene103BlockTreeTermsReader;
+import org.apache.lucene.codecs.lucene103.blocktree.Lucene103BlockTreeTermsWriter;
 import org.apache.lucene.index.ImpactsEnum;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.SegmentReadState;
@@ -94,7 +94,7 @@ import org.apache.lucene.util.packed.PackedInts;
  *   <dd><b>Term Dictionary</b>
  *       <p>The .tim file contains the list of terms in each field along with per-term statistics
  *       (such as docfreq) and pointers to the frequencies, positions, payload and skip data in the
- *       .doc, .pos, and .pay files. See {@link Lucene90BlockTreeTermsWriter} for more details on
+ *       .doc, .pos, and .pay files. See {@link Lucene103BlockTreeTermsWriter} for more details on
  *       the format.
  *       <p>NOTE: The term dictionary can plug into different postings implementations: the postings
  *       writer/reader are actually responsible for encoding and decoding the PostingsHeader and
@@ -146,7 +146,7 @@ import org.apache.lucene.util.packed.PackedInts;
  * <dl>
  *   <dd><b>Term Index</b>
  *       <p>The .tip file contains an index into the term dictionary, so that it can be accessed
- *       randomly. See {@link Lucene90BlockTreeTermsWriter} for more details on the format.
+ *       randomly. See {@link Lucene103BlockTreeTermsWriter} for more details on the format.
  * </dl>
  *
  * <a id="Frequencies"></a>
@@ -386,8 +386,8 @@ public final class Lucene101PostingsFormat extends PostingsFormat {
   /** Creates {@code Lucene101PostingsFormat} with default settings. */
   public Lucene101PostingsFormat() {
     this(
-        Lucene90BlockTreeTermsWriter.DEFAULT_MIN_BLOCK_SIZE,
-        Lucene90BlockTreeTermsWriter.DEFAULT_MAX_BLOCK_SIZE);
+        Lucene103BlockTreeTermsWriter.DEFAULT_MIN_BLOCK_SIZE,
+        Lucene103BlockTreeTermsWriter.DEFAULT_MAX_BLOCK_SIZE);
   }
 
   /**
@@ -395,7 +395,7 @@ public final class Lucene101PostingsFormat extends PostingsFormat {
    * maxBlockSize} passed to block terms dictionary.
    *
    * @see
-   *     Lucene90BlockTreeTermsWriter#Lucene90BlockTreeTermsWriter(SegmentWriteState,PostingsWriterBase,int,int)
+   *     Lucene103BlockTreeTermsWriter#Lucene103BlockTreeTermsWriter(SegmentWriteState,PostingsWriterBase,int,int)
    */
   public Lucene101PostingsFormat(int minTermBlockSize, int maxTermBlockSize) {
     this(minTermBlockSize, maxTermBlockSize, VERSION_CURRENT);
@@ -408,7 +408,7 @@ public final class Lucene101PostingsFormat extends PostingsFormat {
       throw new IllegalArgumentException("Version out of range: " + version);
     }
     this.version = version;
-    Lucene90BlockTreeTermsWriter.validateSettings(minTermBlockSize, maxTermBlockSize);
+    Lucene103BlockTreeTermsWriter.validateSettings(minTermBlockSize, maxTermBlockSize);
     this.minTermBlockSize = minTermBlockSize;
     this.maxTermBlockSize = maxTermBlockSize;
   }
@@ -419,7 +419,7 @@ public final class Lucene101PostingsFormat extends PostingsFormat {
     boolean success = false;
     try {
       FieldsConsumer ret =
-          new Lucene90BlockTreeTermsWriter(
+          new Lucene103BlockTreeTermsWriter(
               state, postingsWriter, minTermBlockSize, maxTermBlockSize);
       success = true;
       return ret;
@@ -435,7 +435,7 @@ public final class Lucene101PostingsFormat extends PostingsFormat {
     PostingsReaderBase postingsReader = new Lucene101PostingsReader(state);
     boolean success = false;
     try {
-      FieldsProducer ret = new Lucene90BlockTreeTermsReader(postingsReader, state);
+      FieldsProducer ret = new Lucene103BlockTreeTermsReader(postingsReader, state);
       success = true;
       return ret;
     } finally {
