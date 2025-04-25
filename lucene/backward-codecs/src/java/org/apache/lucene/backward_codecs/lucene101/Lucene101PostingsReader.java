8af1bad08d8520153008ef8d5e3a534c2ee68612
expani <anijainc@amazon.com>
2025-04-25 07:46:23 -0700

    Same bug fix for 101PostingsReader
    Signed-off-by: expani <anijainc@amazon.com>

--- lucene/backward-codecs/src/java/org/apache/lucene/backward_codecs/lucene101/Lucene101PostingsReader.java
+++ lucene/backward-codecs/src/java/org/apache/lucene/backward_codecs/lucene101/Lucene101PostingsReader.java
@@ -70,6 +70,10 @@ public final class Lucene101PostingsReader extends PostingsReaderBase {
   private static final List<Impact> DUMMY_IMPACTS =
       Collections.singletonList(new Impact(Integer.MAX_VALUE, 1L));
 
+  // We stopped storing a placeholder impact with freq=1 for fields with DOCS after 9.12.0
+  private static final List<Impact> DUMMY_IMPACTS_NO_FREQS =
+      Collections.singletonList(new Impact(1, 1L));
+
   private final IndexInput docIn;
   private final IndexInput posIn;
   private final IndexInput payIn;
@@ -1325,6 +1329,9 @@ public final class Lucene101PostingsReader extends PostingsReaderBase {
 
           @Override
           public List<Impact> getImpacts(int level) {
+            if (indexHasFreq == false) {
+              return DUMMY_IMPACTS_NO_FREQS;
+            }
             if (indexHasFreq) {
               if (level == 0 && level0LastDocID != NO_MORE_DOCS) {
                 return readImpacts(level0SerializedImpacts, level0Impacts);
