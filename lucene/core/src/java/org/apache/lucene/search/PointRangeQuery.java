20c868bbfa397e686afe76e3a2e0faf08a110ee7
Ankit Jain <jain.ankitk@gmail.com>
2025-05-08 00:34:07 -0700

    Revert "Improving the readability"
    This reverts commit f1bbe7ff3794799ca44417a2c96460edaec8a1ce.

--- lucene/core/src/java/org/apache/lucene/search/PointRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/PointRangeQuery.java
@@ -154,10 +154,10 @@ public abstract class PointRangeQuery extends Query {
             return Relation.CELL_OUTSIDE_QUERY;
           }
 
-          if (crosses == false
-              && (comparator.compare(minPackedValue, offset, lowerPoint, offset) < 0
-                  || comparator.compare(maxPackedValue, offset, upperPoint, offset) > 0)) {
-            crosses = true;
+          if (crosses == false) {
+            crosses =
+                comparator.compare(minPackedValue, offset, lowerPoint, offset) < 0
+                    || comparator.compare(maxPackedValue, offset, upperPoint, offset) > 0;
           }
         }
 
