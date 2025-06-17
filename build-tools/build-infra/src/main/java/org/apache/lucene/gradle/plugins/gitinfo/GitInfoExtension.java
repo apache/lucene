0524c7af687c3a89fd2d7f9646647cd30700d9da
Dawid Weiss <dawid.weiss@carrotsearch.com>
2025-06-17 08:50:55 +0200

    Fix regression in assembleSourceTgz #14796

--- build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/gitinfo/GitInfoExtension.java
+++ build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/gitinfo/GitInfoExtension.java
@@ -1,9 +1,19 @@
 package org.apache.lucene.gradle.plugins.gitinfo;
 
+import org.gradle.api.file.FileSystemLocation;
 import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
 
 public abstract class GitInfoExtension {
   public static final String NAME = "gitinfo";
 
+  /**
+   * @return Return a map of key-value pairs extracted from the current git status.
+   */
   public abstract MapProperty<String, String> getGitInfo();
+
+  /**
+   * @return Return the location of {@code .git} directory (or file, if worktrees are used).
+   */
+  public abstract Property<FileSystemLocation> getDotGitDir();
 }
