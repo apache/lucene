ecfa0917fbf7d51361494bc1805423a0cf461385
zhouhui <vsop_479@163.com>
2025-06-20 09:49:36 +0800

    Tidy.

--- lucene/core/src/java/org/apache/lucene/util/PriorityQueue.java
+++ lucene/core/src/java/org/apache/lucene/util/PriorityQueue.java
@@ -343,9 +343,7 @@ public abstract class PriorityQueue<T> implements Iterable<T> {
     };
   }
 
-  /**
-   * Returns true if this list contains no elements
-   */
+  /** Returns true if this list contains no elements */
   public boolean isEmpty() {
     return size == 0;
   }
