493570e48d25af4d3707eb979ae10c7e91731258
zhouhui <vsop_479@163.com>
2025-06-20 18:23:27 +0800

    Fix import wildcard.

--- lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/FirstPassGroupingBenchmark.java
+++ lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/FirstPassGroupingBenchmark.java
@@ -25,7 +25,17 @@ import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import org.apache.lucene.util.PriorityQueue;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
