9936b20153bc1edf3eba90d8d82a1aa6bf4a433c
Simon Cooper <simon.cooper@elastic.co>
2025-06-24 16:35:43 +0100

    Update the IOContext rather than the ReadAdvice on IndexInput (#14702)
    This ensures the IOContext is set on IndexInput for merges, rather than the raw ReadAdvice. This is because ReadAdvice should be an internal implementation detail of MMapDirectory, not part of IndexInput API.

--- lucene/core/src/java/org/apache/lucene/codecs/lucene99/Lucene99HnswVectorsReader.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene99/Lucene99HnswVectorsReader.java
@@ -131,7 +131,7 @@ public final class Lucene99HnswVectorsReader extends KnnVectorsReader
   }
 
   @Override
-  public KnnVectorsReader getMergeInstance() {
+  public KnnVectorsReader getMergeInstance() throws IOException {
     return new Lucene99HnswVectorsReader(this, this.flatVectorsReader.getMergeInstance());
   }
 
