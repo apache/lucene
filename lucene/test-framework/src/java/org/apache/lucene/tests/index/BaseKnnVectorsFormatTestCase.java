f89ed78a07819bb8c90612ac539586561c13fa7f
Shubham Chaudhary <shubhmasand@gmail.com>
2025-08-12 23:55:58 +0530

    Fix assertion in testSearchWithVisitedLimit

--- lucene/test-framework/src/java/org/apache/lucene/tests/index/BaseKnnVectorsFormatTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/tests/index/BaseKnnVectorsFormatTestCase.java
@@ -1501,7 +1501,11 @@ public abstract class BaseKnnVectorsFormatTestCase extends BaseIndexFileFormatTe
                       AcceptDocs.fromLiveDocs(liveDocs, ctx.reader().maxDoc()),
                       visitedLimit);
           assertEquals(TotalHits.Relation.GREATER_THAN_OR_EQUAL_TO, results.totalHits.relation());
-          assertEquals(visitedLimit, results.totalHits.value());
+          // Lucene99HnswVectorsReader uses bulk scoring for scoring EXHAUSTIVE_BULK_SCORE_ORDS i.e.
+          // 64 docs at once
+          assertTrue(
+              visitedLimit == results.totalHits.value()
+                  || (((visitedLimit + 63) / 64) * 64) == results.totalHits.value());
 
           // check the limit is not hit when it clearly exceeds the number of vectors
           k = vectorValues.size();
