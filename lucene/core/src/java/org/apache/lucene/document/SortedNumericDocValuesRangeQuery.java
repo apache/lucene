c0775067c3ff0fd5949e8ba1bf4cc03d21711eab
Alan Woodward <romseygeek@apache.org>
2025-08-15 15:44:17 +0100

    Use DocValuesSkipper for count() in SortedNumericDocValuesRangeQuery
    If a field has a skipper enabled then we can shortcut range queries where
    the requested range is entirely outside of the skipper, or if the skipper
    is fully included within the requested range and there are no deleted
    documents or documents with no value in the field.

--- lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
@@ -184,6 +184,25 @@ final class SortedNumericDocValuesRangeQuery extends Query {
         return ConstantScoreScorerSupplier.fromIterator(
             TwoPhaseIterator.asDocIdSetIterator(iterator), score(), scoreMode, maxDoc);
       }
+
+      @Override
+      public int count(LeafReaderContext context) throws IOException {
+        DocValuesSkipper skipper = context.reader().getDocValuesSkipper(field);
+        if (skipper == null) {
+          return -1;
+        }
+        if (skipper.minValue() > upperValue || skipper.maxValue() < lowerValue) {
+          return 0;
+        }
+        if (skipper.docCount() == context.reader().maxDoc()
+            && context.reader().hasDeletions() == false
+            && skipper.minValue() >= lowerValue
+            && skipper.maxValue() <= upperValue) {
+
+          return context.reader().maxDoc();
+        }
+        return -1;
+      }
     };
   }
 
