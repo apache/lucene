c60d477acfd499d77080eb44fcfac8f8819e2079
Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>
2025-08-26 13:58:19 -0700

    Use try-with-resources for directory and indexWriter

--- lucene/core/src/test/org/apache/lucene/search/TestLRUQueryCache.java
+++ lucene/core/src/test/org/apache/lucene/search/TestLRUQueryCache.java
@@ -1064,35 +1064,33 @@ public class TestLRUQueryCache extends LuceneTestCase {
   }
 
   public void testCacheRamBytesWithALargeTermQuery() throws IOException {
-    Directory dir = newDirectory();
-    final RandomIndexWriter w = new RandomIndexWriter(random(), dir);
-    Document doc = new Document();
-    doc.add(new StringField("foo", "bar", Store.YES));
-    doc.add(new StringField("foo", "quux", Store.YES));
-    w.addDocument(doc);
-    w.commit();
-    final IndexReader reader = w.getReader();
-    final IndexSearcher searcher = newSearcher(reader);
-    w.close();
-    final LRUQueryCache queryCache =
-        new LRUQueryCache(1000000, 10000000, _ -> true, Float.POSITIVE_INFINITY);
-    searcher.setQueryCache(queryCache);
-    searcher.setQueryCachingPolicy(ALWAYS_CACHE);
-    StringBuilder sb = new StringBuilder();
-    // Create a large string for the field value so it certainly exceeds the default query size we
-    // use ie 1024 bytes.
-    sb.append("a".repeat(1200));
-    String longTerm = sb.toString();
-    TermQuery must = new TermQuery(new Term("foo", longTerm));
-    long queryInBytes = RamUsageEstimator.sizeOf(must, 0);
-    assertTrue(queryInBytes > QUERY_DEFAULT_RAM_BYTES_USED);
-    searcher.search(new ConstantScoreQuery(must), 1);
+    try (Directory dir = newDirectory();
+        RandomIndexWriter w = new RandomIndexWriter(random(), dir)) {
+      Document doc = new Document();
+      doc.add(new StringField("foo", "bar", Store.YES));
+      doc.add(new StringField("foo", "quux", Store.YES));
+      w.addDocument(doc);
+      w.commit();
+      final IndexReader reader = w.getReader();
+      final IndexSearcher searcher = newSearcher(reader);
+      final LRUQueryCache queryCache =
+          new LRUQueryCache(1000000, 10000000, _ -> true, Float.POSITIVE_INFINITY);
+      searcher.setQueryCache(queryCache);
+      searcher.setQueryCachingPolicy(ALWAYS_CACHE);
+      StringBuilder sb = new StringBuilder();
+      // Create a large string for the field value so it certainly exceeds the default query size we
+      // use ie 1024 bytes.
+      sb.append("a".repeat(1200));
+      String longTerm = sb.toString();
+      TermQuery must = new TermQuery(new Term("foo", longTerm));
+      long queryInBytes = RamUsageEstimator.sizeOf(must, 0);
+      assertTrue(queryInBytes > QUERY_DEFAULT_RAM_BYTES_USED);
+      searcher.search(new ConstantScoreQuery(must), 1);
 
-    assertEquals(1, queryCache.cachedQueries().size());
-    assertTrue(queryCache.ramBytesUsed() >= queryInBytes);
-
-    reader.close();
-    dir.close();
+      assertEquals(1, queryCache.cachedQueries().size());
+      assertTrue(queryCache.ramBytesUsed() >= queryInBytes);
+      reader.close();
+    }
   }
 
   private static Term randomTerm() {
