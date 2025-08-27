d3a154c26c925b9dc5b54100d8fad079156afcb0
Ankit Jain <jain.ankitk@gmail.com>
2025-08-27 16:34:06 -0700

    Revert "Enabling point range collector if doc values not indexed"
    This reverts commit 3a292740d5cfc083243be0b6aa6f55d2745aa307.

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/plain/histograms/HistogramCollector.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/facet/plain/histograms/HistogramCollector.java
@@ -64,17 +64,12 @@ final class HistogramCollector implements Collector {
       throw new CollectionTerminatedException();
     }
 
-    boolean docValuesIndexed =
-        fi.getDocValuesType() == DocValuesType.NUMERIC
-            || fi.getDocValuesType() == DocValuesType.SORTED_NUMERIC;
-
     // We can use multi range traversal logic to collect the histogram on numeric
     // field indexed as point for MATCH_ALL cases. In future, this can be extended
     // for Point Range Query cases as well
     if (weight != null && weight.count(context) == context.reader().maxDoc()) {
       final PointValues pointValues = context.reader().getPointValues(field);
-      if (docValuesIndexed == false
-          || PointTreeBulkCollector.canCollectEfficiently(pointValues, bucketWidth)) {
+      if (PointTreeBulkCollector.canCollectEfficiently(pointValues, bucketWidth)) {
         // In case of intra segment concurrency, only one collector should collect
         // documents for all the partitions to avoid duplications across collectors
         if (leafBulkCollected.putIfAbsent(context, true) == null) {
@@ -86,7 +81,8 @@ final class HistogramCollector implements Collector {
       }
     }
 
-    if (docValuesIndexed == false) {
+    if (fi.getDocValuesType() != DocValuesType.NUMERIC
+        && fi.getDocValuesType() != DocValuesType.SORTED_NUMERIC) {
       throw new IllegalStateException(
           "Expected numeric field, but got doc-value type: " + fi.getDocValuesType());
     }
