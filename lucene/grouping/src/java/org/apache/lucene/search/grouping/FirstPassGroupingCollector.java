a1c583d6a73b81afbda5e61ca48b1ec4856cd578
Binlong Gao <gbinlong@amazon.com>
2025-09-05 17:35:56 +0800

    FirstPassGroupingCollector supports ignoring docs without group field
    Signed-off-by: Binlong Gao <gbinlong@amazon.com>

--- lucene/grouping/src/java/org/apache/lucene/search/grouping/FirstPassGroupingCollector.java
+++ lucene/grouping/src/java/org/apache/lucene/search/grouping/FirstPassGroupingCollector.java
@@ -32,6 +32,7 @@ import org.apache.lucene.search.SimpleCollector;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.SortField;
 import org.apache.lucene.util.CollectionUtil;
+import org.apache.lucene.util.mutable.MutableValue;
 
 /**
  * FirstPassGroupingCollector is the first of two passes necessary to collect grouped hits. This
@@ -44,6 +45,7 @@ import org.apache.lucene.util.CollectionUtil;
 public class FirstPassGroupingCollector<T> extends SimpleCollector {
 
   private final GroupSelector<T> groupSelector;
+  private final boolean ignoreDocsWithoutGroupField;
 
   private final FieldComparator<?>[] comparators;
   private final LeafFieldComparator[] leafComparators;
@@ -74,7 +76,28 @@ public class FirstPassGroupingCollector<T> extends SimpleCollector {
   @SuppressWarnings({"unchecked", "rawtypes"})
   public FirstPassGroupingCollector(
       GroupSelector<T> groupSelector, Sort groupSort, int topNGroups) {
+    this(groupSelector, groupSort, topNGroups, false);
+  }
+
+  /**
+   * Create the first pass collector with ignoreDocsWithoutGroupField
+   *
+   * @param groupSelector a GroupSelector used to defined groups
+   * @param groupSort The {@link Sort} used to sort the groups. The top sorted document within each
+   *     group according to groupSort, determines how that group sorts against other groups. This
+   *     must be non-null, ie, if you want to groupSort by relevance use Sort.RELEVANCE.
+   * @param topNGroups How many top groups to keep.
+   * @param ignoreDocsWithoutGroupField if true, ignore documents that don't have the group field
+   *     instead of putting them in a null group
+   */
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  public FirstPassGroupingCollector(
+      GroupSelector<T> groupSelector,
+      Sort groupSort,
+      int topNGroups,
+      boolean ignoreDocsWithoutGroupField) {
     this.groupSelector = groupSelector;
+    this.ignoreDocsWithoutGroupField = ignoreDocsWithoutGroupField;
     if (topNGroups < 1) {
       throw new IllegalArgumentException("topNGroups must be >= 1 (got " + topNGroups + ")");
     }
@@ -198,11 +221,13 @@ public class FirstPassGroupingCollector<T> extends SimpleCollector {
       return;
     }
 
-    // TODO: should we add option to mean "ignore docs that
-    // don't have the group field" (instead of stuffing them
-    // under null group)?
     groupSelector.advanceTo(doc);
     T groupValue = groupSelector.currentValue();
+
+    // Skip documents without group field if option is enabled
+    if (ignoreDocsWithoutGroupField && isNullGroupValue(groupValue)) {
+      return;
+    }
 
     final CollectedSearchGroup<T> group = groupMap.get(groupValue);
 
@@ -362,5 +387,16 @@ public class FirstPassGroupingCollector<T> extends SimpleCollector {
    */
   public GroupSelector<T> getGroupSelector() {
     return groupSelector;
+  }
+
+  private boolean isNullGroupValue(T groupValue) {
+    if (groupValue == null) {
+      return true;
+    }
+    // For ValueSourceGroupSelector, check if MutableValue exists
+    if (groupValue instanceof MutableValue) {
+      return !((MutableValue) groupValue).exists();
+    }
+    return false;
   }
 }
