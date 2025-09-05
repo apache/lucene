af1883a4110d472eae4f5e402c7eae090a731dd8
Adrien Grand <jpountz@gmail.com>
2025-09-05 15:53:05 +0200

    Add bulk-retrieval API to NumericDocValues. (#15149)
    Lucene recently got very good performance improvements by introducing APIs that
    apply to batches of doc IDs at once: `DocIdSetIterator#intoBitSet`,
    `PostingsEnum#nextPostings`, `Scorer#nextDocsAndScores` and `SimScorer#score`.
    This helps better amortize the cost of virtual function calls across many doc
    IDs, and also apply additional optimizations, e.g. it's more efficient to
    bulk-iterate set bits in a `FixedBitSet` than to iterate them one-by-one via
    `FixedBitSet#nextSetBit`.
    
    This PR introduces bulk retrieval for numeric doc values. It is currently only
    implemented on norms and used to retrieve norms for doc IDs to score, but I
    tried to design the API in a way that also works for numeric doc values and is
    sustainable. Specifically, I'm thinking that optimizing the single-valued and
    dense case should go a very long way, so I did not try to help users retrieve
    information about which docs have a value or not. In some cases, this is not
    even needed. E.g. if you want to compute the sum of the values of a field,
    returning 0 for docs that don't have a value is good. In the event when knowing
    which docs have a value is important (such as Lucene's `HistogramCollector`),
    it is still possible to optimize the case when there are long runs of docs with
    a value with something like below:
    
    ```java
    void doSomethingWith(int size, int[] docs, NumericDocValues values) {
      if (size > 0 && values.advanceExact(docs[0]) && values.docIDRunEnd() &gt; docs[size - 1]) {
        long[] longValues = new long[size];
        values.longValues(size, docs, longValues);
        // do something with the `longValues` array
      } else {
        // use #advanceExact / #longValue directly
        for (int i = 0; i < size; i++) {
          if (values.advanceExact(docs[i])) {
            // do something with values#longValue
          } else {
            // handle the case when docs don't have a value
          }
        }
      }
    }
    ```

--- lucene/core/src/java/org/apache/lucene/index/CheckIndex.java
+++ lucene/core/src/java/org/apache/lucene/index/CheckIndex.java
@@ -1381,6 +1381,8 @@ public final class CheckIndex implements Closeable {
       for (FieldInfo info : reader.getFieldInfos()) {
         if (info.hasNorms()) {
           checkNumericDocValues(info.name, normsReader.getNorms(info), normsReader.getNorms(info));
+          checkBulkFetchNumericDocValues(
+              info.name, normsReader.getNorms(info), normsReader.getNorms(info), reader.maxDoc());
           ++status.totFields;
         }
       }
@@ -3330,7 +3332,7 @@ public final class CheckIndex implements Closeable {
       for (FieldInfo fieldInfo : reader.getFieldInfos()) {
         if (fieldInfo.getDocValuesType() != DocValuesType.NONE) {
           status.totalValueFields++;
-          checkDocValues(fieldInfo, dvReader, status);
+          checkDocValues(fieldInfo, reader.maxDoc(), dvReader, status);
         }
       }
 
@@ -3824,8 +3826,44 @@ public final class CheckIndex implements Closeable {
     }
   }
 
+  private static void checkBulkFetchNumericDocValues(
+      String fieldName, NumericDocValues ndv, NumericDocValues ndv2, int maxDoc)
+      throws IOException {
+
+    int[] docs = new int[16];
+    long[] values = new long[16];
+
+    for (int doc = -1; doc < maxDoc; ) {
+      int size = 0;
+      for (int j = 0; j < docs.length; ++j) {
+        doc += 1 + (j & 0x03);
+        if (doc >= maxDoc) {
+          break;
+        }
+        docs[size++] = doc;
+      }
+
+      long defaultValue = 42L;
+      ndv.longValues(size, docs, values, defaultValue);
+
+      for (int j = 0; j < size; ++j) {
+        long expected;
+        if (ndv2.advanceExact(docs[j])) {
+          expected = ndv2.longValue();
+        } else {
+          expected = defaultValue;
+        }
+        if (values[j] != expected) {
+          throw new CheckIndexException(
+              "#longValues reports different value: " + values[j] + " != " + expected);
+        }
+      }
+    }
+  }
+
   private static void checkDocValues(
-      FieldInfo fi, DocValuesProducer dvReader, DocValuesStatus status) throws Exception {
+      FieldInfo fi, int maxDoc, DocValuesProducer dvReader, DocValuesStatus status)
+      throws Exception {
     if (fi.docValuesSkipIndexType() != DocValuesSkipIndexType.NONE) {
       status.totalSkippingIndex++;
       checkDocValueSkipper(fi, dvReader.getSkipper(fi));
@@ -3856,6 +3894,8 @@ public final class CheckIndex implements Closeable {
         status.totalNumericFields++;
         checkDVIterator(fi, dvReader::getNumeric);
         checkNumericDocValues(fi.name, dvReader.getNumeric(fi), dvReader.getNumeric(fi));
+        checkBulkFetchNumericDocValues(
+            fi.name, dvReader.getNumeric(fi), dvReader.getNumeric(fi), maxDoc);
         break;
       case NONE:
       default:
