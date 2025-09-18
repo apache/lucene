af9d4e5fba36a36bdc90384fc69d43c7e3cac45c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2025-09-18 21:58:13 +0200

    Use git-ls-files as the source of file paths for license checks (reapplied) (#15203)

--- build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/gitinfo/GitFileListValueSource.java
+++ build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/gitinfo/GitFileListValueSource.java
@@ -20,23 +20,22 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 import javax.inject.Inject;
 import org.gradle.api.GradleException;
 import org.gradle.api.provider.ValueSource;
 import org.gradle.process.ExecOperations;
+import org.slf4j.LoggerFactory;
 
-public abstract class GitInfoValueSource
-    implements ValueSource<Map<String, String>, GitValueSourceParameters> {
+/** Returns a list of versioned and non-versioned (but not ignored) git files. */
+public abstract class GitFileListValueSource
+    implements ValueSource<List<String>, GitValueSourceParameters> {
 
   @Inject
   public abstract ExecOperations getExecOps();
 
   @Override
-  public Map<String, String> obtain() {
+  public List<String> obtain() {
     try (var baos = new ByteArrayOutputStream();
         var out = new BufferedOutputStream(baos)) {
       var result =
@@ -50,55 +49,34 @@ public abstract class GitInfoValueSource
                     getParameters().configure(spec);
 
                     spec.setExecutable(getParameters().getGitExec().get());
-                    spec.args("status", "--porcelain=v2", "--branch");
+                    spec.args(
+                        "ls-files",
+                        // show cached
+                        "-c",
+                        // show others
+                        "-o",
+                        // apply .gitignore exclusions
+                        "--exclude-standard",
+                        // don't quote paths, 0-terminate strings.
+                        "-z");
                   });
       out.flush();
 
-      // Just assume it's UTF-8. I don't know if this can break anyhow.
+      // Assume the output is UTF-8, even if it has 0 eols.
       String gitOutput = baos.toString(StandardCharsets.UTF_8);
 
       if (result.getExitValue() != 0) {
         // Something went wrong. Assume this isn't a git checkout and
-        // add placeholder values.
-        return Map.ofEntries(
-            Map.entry("git.commit", "unknown"),
-            Map.entry("git.commit-short", "unknown"),
-            Map.entry("git.clean", "false"),
-            Map.entry("git.changed-files", "not a checkout?"),
-            Map.entry("git.error-log", gitOutput));
+        // return an empty list of files.
+        LoggerFactory.getLogger(getClass())
+            .warn(
+                "Failed executing git (exit status: {}). An empty list of versioned files will be used (run 'git init'?).",
+                result.getExitValue());
+
+        return List.of();
       }
 
-      // Parse git-porcelain output.
-      Map<String, String> properties = new TreeMap<>();
-
-      Pattern headerLine = Pattern.compile("# (?<key>[^ ]+) (?<value>.+)");
-      Pattern changeLine = Pattern.compile("(?<type>[12u?!]) (?<value>.+)");
-      StringBuilder changedLines = new StringBuilder();
-      for (var line : gitOutput.split("\\n")) {
-        if (line.startsWith("#")) {
-          Matcher matcher = headerLine.matcher(line);
-          if (matcher.matches()) {
-            String key = matcher.group("key");
-            String value = matcher.group("value");
-            properties.put(key, value);
-
-            if (key.equals("branch.oid")) {
-              properties.put("git.commit", value);
-              properties.put("git.commit-short", value.substring(0, 8));
-            }
-          }
-        } else if (changeLine.matcher(line).matches()) {
-          changedLines.append(line);
-          changedLines.append("\n");
-        } else {
-          // Omit other lines.
-        }
-      }
-
-      properties.put("git.changed-files", changedLines.toString());
-      properties.put("git.clean", changedLines.isEmpty() ? "true" : "false");
-
-      return properties;
+      return List.of(gitOutput.split("\u0000"));
     } catch (IOException e) {
       throw new GradleException("Errors calling git to fetch local repository status.", e);
     }
