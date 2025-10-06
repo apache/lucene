171148bd7102e612527dad454560b6a65bdfea69
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2025-10-06 11:57:47 -0400

    Add next bugfix version 10.3.2

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -219,11 +219,16 @@ public final class Version {
   @Deprecated public static final Version LUCENE_10_3_0 = new Version(10, 3, 0);
 
   /**
-   * Match settings and bugs in Lucene's 10.3.1 release.
+   * @deprecated (10.3.2) Use latest
+   */
+  @Deprecated public static final Version LUCENE_10_3_1 = new Version(10, 3, 1);
+
+  /**
+   * Match settings and bugs in Lucene's 10.3.2 release.
    *
    * <p>Use this to get the latest &amp; greatest settings, bug fixes, etc, for Lucene.
    */
-  public static final Version LUCENE_10_3_1 = new Version(10, 3, 1);
+  public static final Version LUCENE_10_3_2 = new Version(10, 3, 2);
 
   // To add a new version:
   //  * Only add above this comment
@@ -239,7 +244,7 @@ public final class Version {
    * <b>re-test your entire application</b> to ensure it behaves as expected, as some defaults may
    * have changed and may break functionality in your application.
    */
-  public static final Version LATEST = LUCENE_10_3_1;
+  public static final Version LATEST = LUCENE_10_3_2;
 
   /**
    * Constant for backwards compatibility.
