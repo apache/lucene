3504221de5eec8649c5bb8ad69a4896dc2d32199
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2025-10-06 11:55:04 -0400

    Add bugfix version 10.3.1

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -219,6 +219,11 @@ public final class Version {
   @Deprecated public static final Version LUCENE_10_3_0 = new Version(10, 3, 0);
 
   /**
+   * @deprecated Use latest
+   */
+  @Deprecated public static final Version LUCENE_10_3_1 = new Version(10, 3, 1);
+
+  /**
    * Match settings and bugs in Lucene's 10.4.0 release.
    *
    * <p>Use this to get the latest &amp; greatest settings, bug fixes, etc, for Lucene.
