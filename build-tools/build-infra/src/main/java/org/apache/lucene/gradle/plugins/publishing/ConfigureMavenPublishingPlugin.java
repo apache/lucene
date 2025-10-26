apache/lucene

commit 1ebee12e4a671c6368a8cff94c3729b5247a14c2

Fix manifest content on source/javadoc artifacts. Wrong import caused this. (#15362)

build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/publishing/ConfigureMavenPublishingPlugin.java
@@ -30,7 +30,7 @@ import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 import org.gradle.api.publish.tasks.GenerateModuleMetadata;
 import org.gradle.api.tasks.Delete;
-import org.gradle.jvm.tasks.Jar;
+import org.gradle.api.tasks.bundling.Jar;
 import org.gradle.plugins.signing.SigningExtension;
 import org.gradle.plugins.signing.SigningPlugin;
 

build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/java/ModularPathsPlugin.java
@@ -33,9 +33,9 @@ import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.bundling.Jar;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.api.tasks.testing.Test;
-import org.gradle.jvm.tasks.Jar;
 
 /** Configures miscellaneous aspects required to support the java module system layer. */
 public class ModularPathsPlugin extends LuceneGradlePlugin {

build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/misc/PyLuceneHelperPlugin.java
@@ -20,7 +20,7 @@ import org.apache.lucene.gradle.plugins.LuceneGradlePlugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.tasks.Sync;
-import org.gradle.jvm.tasks.Jar;
+import org.gradle.api.tasks.bundling.Jar;
 
 public class PyLuceneHelperPlugin extends LuceneGradlePlugin {
   @Override

commit 3389f2e3749ea2f880e598b2b9e3ba5c81c0d210

Fix snapshot publishing regression introduced in #15362

build-tools/build-infra/src/main/java/org/apache/lucene/gradle/plugins/publishing/ConfigureMavenPublishingPlugin.java
@@ -74,6 +74,11 @@ public class ConfigureMavenPublishingPlugin extends LuceneGradlePlugin {
 
     var type = useReleaseRepository ? "Releases" : "Snapshots";
 
+    var publicationTask =
+        useReleaseRepository
+            ? "publishSignedJarsPublicationToApacheReleasesRepository"
+            : "publishJarsPublicationToApacheSnapshotsRepository";
+
     var tasks = rootProject.getTasks();
     tasks.register(
         "mavenToApache" + type,
@@ -86,14 +91,7 @@ public class ConfigureMavenPublishingPlugin extends LuceneGradlePlugin {
                   + apacheNexusRepositoryUrl);
 
           for (var p : getLuceneBuildGlobals(rootProject).getPublishedProjects()) {
-            var matchingTasks =
-                p.getTasks()
-                    .matching(
-                        t ->
-                            t.getName()
-                                .equals(
-                                    "publishSignedJarsPublicationToApache" + type + "Repository"));
-            task.dependsOn(matchingTasks);
+            task.dependsOn(p.getTasks().matching(t -> t.getName().equals(publicationTask)));
           }
         });
 