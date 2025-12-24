9929ee31a01ff14defb423278fcbe8b04f4cb294
zhouhui <vsop_479@163.com>
2025-12-24 15:09:50 +0800

    Fix import.

--- lucene/core/src/java/org/apache/lucene/codecs/lucene103/blocktree/art/ARTBuilderPre.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene103/blocktree/art/ARTBuilderPre.java
@@ -17,7 +17,9 @@
 package org.apache.lucene.codecs.lucene103.blocktree.art;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.BiConsumer;
 import org.apache.lucene.store.DataOutput;
 import org.apache.lucene.store.IndexOutput;
