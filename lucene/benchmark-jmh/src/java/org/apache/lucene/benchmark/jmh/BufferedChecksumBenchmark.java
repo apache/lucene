cfe199bd8a425bd20317d2e09808b96fb483796b
Sagar Upadhyaya <sagar.upadhyaya.121@gmail.com>
2026-01-21 10:40:40 -0800

    Remove wildcard imports

--- lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/BufferedChecksumBenchmark.java
+++ lucene/benchmark-jmh/src/java/org/apache/lucene/benchmark/jmh/BufferedChecksumBenchmark.java
@@ -20,7 +20,17 @@ import java.util.concurrent.TimeUnit;
 import java.util.zip.CRC32;
 import java.util.zip.CRC32C;
 import org.apache.lucene.store.BufferedChecksum;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
