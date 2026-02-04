8b78a1040ac5a19f3ee81059a5425249da59bdd0
Shubham Chaudhary <shubhmasand@gmail.com>
2026-02-04 18:15:49 -0500

    Log info in NativeVectorizationProvider

--- lucene/core/src/java25/org/apache/lucene/internal/vectorization/panama/NativeVectorizationProvider.java
+++ lucene/core/src/java25/org/apache/lucene/internal/vectorization/panama/NativeVectorizationProvider.java
@@ -18,11 +18,15 @@
 package org.apache.lucene.internal.vectorization.panama;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.logging.Logger;
 import org.apache.lucene.codecs.hnsw.FlatVectorsScorer;
 import org.apache.lucene.internal.vectorization.PostingDecodingUtil;
 import org.apache.lucene.internal.vectorization.VectorUtilSupport;
 import org.apache.lucene.internal.vectorization.VectorizationProvider;
 import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.util.Constants;
+import org.apache.lucene.util.SuppressForbidden;
 
 /** Native provider returning native implementations which delegates to Panama implementation. */
 public final class NativeVectorizationProvider extends VectorizationProvider {
@@ -39,6 +43,25 @@ public final class NativeVectorizationProvider extends VectorizationProvider {
     }
     vectorUtilSupport =
         new NativeVectorUtilSupport(delegateVectorUtilProvider.getVectorUtilSupport());
+    logIncubatorSetup();
+  }
+
+  @SuppressForbidden(reason = "We log at info level here, it's fine.")
+  private void logIncubatorSetup() {
+    var log = Logger.getLogger(getClass().getName());
+    log.info(
+        String.format(
+            Locale.ENGLISH,
+            "Native vectorization enabled; uses preferredBitSize=%d%s%s; strict mode=%s; native library loaded=%s;",
+            PanamaVectorConstants.PREFERRED_VECTOR_BITSIZE,
+            Constants.HAS_FAST_VECTOR_FMA ? "; FMA enabled" : "",
+            VectorizationProvider.TESTS_VECTOR_SIZE.isPresent() ? "; testMode enabled" : "",
+            Constants.NATIVE_STRICT_MODE ? "true" : "false",
+            NativeVectorUtilSupport.isLibraryLoaded()
+                ? "true, using " + getClass().getSimpleName()
+                : "false, switching to "
+                    + delegateVectorUtilProvider.getClass().getSimpleName()
+                    + "as fallback"));
   }
 
   @Override
