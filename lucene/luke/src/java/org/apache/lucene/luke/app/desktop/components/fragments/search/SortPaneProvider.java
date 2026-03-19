4b4a98cf96bb4db3ae42c12c11908c94ec7d5c62
Binlong Gao <gbinlong@amazon.com>
2026-03-19 16:08:01 +0800

    Replace explicit type conversion with instanceof pattern variable in luke module
    Signed-off-by: Binlong Gao <gbinlong@amazon.com>

--- lucene/luke/src/java/org/apache/lucene/luke/app/desktop/components/fragments/search/SortPaneProvider.java
+++ lucene/luke/src/java/org/apache/lucene/luke/app/desktop/components/fragments/search/SortPaneProvider.java
@@ -214,8 +214,8 @@ public final class SortPaneProvider implements SortTabOperator {
         sortFields.stream()
             .map(
                 sf -> {
-                  if (sf instanceof SortedNumericSortField) {
-                    return ((SortedNumericSortField) sf).getNumericType().name();
+                  if (sf instanceof SortedNumericSortField snsf) {
+                    return snsf.getNumericType().name();
                   } else {
                     return sf.getType().name();
                   }
