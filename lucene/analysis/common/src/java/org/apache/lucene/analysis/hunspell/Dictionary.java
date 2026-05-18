9bdee8a583f3c7984d6fb0298e8c506e455a3a12
Zhou Hui <vsop_479@163.com>
2026-05-18 17:18:08 +0800

    Test toArray(new String[n]).

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Dictionary.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Dictionary.java
@@ -1622,7 +1622,7 @@ public class Dictionary {
     final String[] starting, ending, middle;
 
     Breaks(Collection<String> starting, Collection<String> ending, Collection<String> middle) {
-      this.starting = starting.toArray(String[]::new);
+      this.starting = starting.toArray(new String[starting.size()]);
       this.ending = ending.toArray(String[]::new);
       this.middle = middle.toArray(String[]::new);
     }
