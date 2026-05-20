b4e02e0ffeaf6248c9028b5deb84651df16c522b
Binlong Gao <gbinlong@amazon.com>
2026-05-20 13:20:49 +0800

    Make totalHits private
    Signed-off-by: Binlong Gao <gbinlong@amazon.com>

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/search/LargeNumHitsTopDocsCollector.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/search/LargeNumHitsTopDocsCollector.java
@@ -45,7 +45,7 @@ public final class LargeNumHitsTopDocsCollector implements Collector {
   // package private for testing
   HitQueue pq;
   ScoreDoc pqTop;
-  int totalHits;
+  private int totalHits;
 
   public LargeNumHitsTopDocsCollector(int requestedHitCount) {
     if (requestedHitCount <= 0) {
