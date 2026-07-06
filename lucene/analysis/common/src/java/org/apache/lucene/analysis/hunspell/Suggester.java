4a71bfaba9db8d4b9a57c1da509ba8689c0e05b8
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2026-07-06 16:21:06 +0300

    Format code

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Suggester.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/hunspell/Suggester.java
@@ -31,7 +31,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.lucene.analysis.hunspell.GeneratingSuggester.Provider;
 import org.apache.lucene.util.CharsRef;
 
