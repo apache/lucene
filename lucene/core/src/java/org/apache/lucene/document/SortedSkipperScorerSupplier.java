0013fe98f896ffe3eb9244c4ed14377ac9ee0132
ivera <ignacio.vera@elastic.co>
2026-08-18 10:17:06 +0200

    iter

--- lucene/core/src/java/org/apache/lucene/document/SortedSkipperScorerSupplier.java
+++ lucene/core/src/java/org/apache/lucene/document/SortedSkipperScorerSupplier.java
@@ -102,8 +102,6 @@ abstract class SortedSkipperScorerSupplier extends ScorerSupplier {
   @Override
   public long cost() throws IOException {
     if (skipperMinDocId == -1) {
-      // Similar to PointValues, IOExceptions needs to be caught and rethrown as
-      // UncheckedIOException
       computeSkipperDocIds();
     }
     if (skipperMaxDocIdExact) {
