56086f5267be4518c69061043e2ae5606ab21451
ivera <ignacio.vera@elastic.co>
2026-08-18 09:36:31 +0200

    ScoreSupplier#cost throws an IOException

--- lucene/core/src/java/org/apache/lucene/search/DisjunctionMaxQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/DisjunctionMaxQuery.java
@@ -171,7 +171,7 @@ public final class DisjunctionMaxQuery extends Query implements Iterable<Query> 
           }
 
           @Override
-          public long cost() {
+          public long cost() throws IOException {
             if (cost == -1) {
               long cost = 0;
               for (ScorerSupplier ss : scorerSuppliers) {
