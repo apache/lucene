4c5b5e3693835ac027bf3c7f719047c1645da30d
ChrisHegarty <chegar999@gmail.com>
2026-09-07 12:32:00 +0100

    unwrapSegmentReader

--- lucene/core/src/java/org/apache/lucene/index/SlowCodecReaderWrapper.java
+++ lucene/core/src/java/org/apache/lucene/index/SlowCodecReaderWrapper.java
@@ -215,21 +215,32 @@ public final class SlowCodecReaderWrapper {
 
       @Override
       public int getVectorCount(FieldInfo fieldInfo) throws IOException {
-        SegmentReader segmentReader = segmentReader(reader);
-        var vectorsReader = segmentReader.getVectorReader();
-        vectorsReader = vectorsReader.unwrapReaderForField(fieldInfo.name);
-        return vectorsReader.getVectorCount(fieldInfo);
+        SegmentReader segmentReader = unwrapSegmentReader(reader);
+        if (segmentReader != null) {
+          var vectorsReader = segmentReader.getVectorReader();
+          vectorsReader = vectorsReader.unwrapReaderForField(fieldInfo.name);
+          return vectorsReader.getVectorCount(fieldInfo);
+        }
+        return super.getVectorCount(fieldInfo);
       }
 
       static SegmentReader segmentReader(LeafReader reader) {
+        SegmentReader segmentReader = unwrapSegmentReader(reader);
+        if (segmentReader == null) {
+          throw new AssertionError("unexpected reader [" + reader + "]");
+        }
+        return segmentReader;
+      }
+
+      static SegmentReader unwrapSegmentReader(LeafReader reader) {
         if (reader instanceof SegmentReader sr) {
           return sr;
         } else if (reader instanceof final FilterLeafReader fReader) {
-          return segmentReader(FilterLeafReader.unwrap(fReader));
+          return unwrapSegmentReader(FilterLeafReader.unwrap(fReader));
         } else if (reader instanceof final FilterCodecReader fReader) {
-          return segmentReader(FilterCodecReader.unwrap(fReader));
+          return unwrapSegmentReader(FilterCodecReader.unwrap(fReader));
         }
-        throw new AssertionError("unexpected reader [" + reader + "]");
+        return null;
       }
 
       @Override
