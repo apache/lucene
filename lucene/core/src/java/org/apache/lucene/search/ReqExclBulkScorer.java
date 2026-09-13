b3094fab91497decba50cc248cf2efec77ea7e14
kewe.11 <kewei.11@bytedance.com>
2026-09-13 14:50:59 +0800

    Use intoBitSet to speed up ReqExclBulkScorer with dense exclusions

--- lucene/core/src/java/org/apache/lucene/search/ReqExclBulkScorer.java
+++ lucene/core/src/java/org/apache/lucene/search/ReqExclBulkScorer.java
@@ -18,12 +18,16 @@ package org.apache.lucene.search;
 
 import java.io.IOException;
 import org.apache.lucene.util.Bits;
+import org.apache.lucene.util.FixedBitSet;
 
 final class ReqExclBulkScorer extends BulkScorer {
 
+  private static final int WINDOW_SIZE = 4096;
   private final BulkScorer req;
   private final DocIdSetIterator exclApproximation;
   private final TwoPhaseIterator exclTwoPhase;
+  private final boolean useBitSetWindows;
+  private WindowBits windowBits;
 
   ReqExclBulkScorer(BulkScorer req, Scorer excl) {
     this.req = req;
@@ -33,22 +37,79 @@ final class ReqExclBulkScorer extends BulkScorer {
     } else {
       this.exclApproximation = excl.iterator();
     }
+    this.useBitSetWindows = shouldUseBitSet(req, exclApproximation);
   }
 
   ReqExclBulkScorer(BulkScorer req, DocIdSetIterator excl) {
     this.req = req;
     this.exclTwoPhase = null;
     this.exclApproximation = excl;
+    this.useBitSetWindows = shouldUseBitSet(req, exclApproximation);
   }
 
   ReqExclBulkScorer(BulkScorer req, TwoPhaseIterator excl) {
     this.req = req;
     this.exclTwoPhase = excl;
     this.exclApproximation = excl.approximation();
+    this.useBitSetWindows = shouldUseBitSet(req, exclApproximation);
   }
 
   @Override
   public int score(LeafCollector collector, Bits acceptDocs, int min, int max) throws IOException {
+    if (useBitSetWindows == false) {
+      return sparseScore(collector, acceptDocs, min, max);
+    } else {
+      return denseScore(collector, acceptDocs, min, max);
+    }
+  }
+
+  private int denseScore(LeafCollector collector, Bits acceptDocs, int min, int max)
+      throws IOException {
+    if (windowBits == null) {
+      windowBits = new WindowBits();
+    }
+    FixedBitSet windowMask = windowBits.windowMask;
+
+    int upTo = min;
+
+    while (upTo < max) {
+      int windowMin = upTo;
+      int windowMax = (int) Math.min((long) max, (long) windowMin + WINDOW_SIZE);
+
+      if (exclApproximation.docID() < windowMin) {
+        exclApproximation.advance(windowMin);
+      }
+
+      windowMask.clear();
+      if (exclTwoPhase == null) {
+        exclApproximation.intoBitSet(windowMax, windowMask, windowMin);
+      } else {
+        exclTwoPhase.intoBitSet(windowMax, windowMask, windowMin);
+      }
+      int windowLength = windowMax - windowMin;
+      int validWindowLength =
+          acceptDocs == null
+              ? windowLength
+              : Math.max(0, Math.min(windowLength, acceptDocs.length() - windowMin));
+      windowMask.flip(0, validWindowLength);
+      if (validWindowLength < windowLength) {
+        windowMask.clear(validWindowLength, windowLength);
+      }
+      if (acceptDocs != null) {
+        acceptDocs.applyMask(windowMask, windowMin);
+      }
+      windowBits.reset(windowMin, windowMax, acceptDocs == null ? max : acceptDocs.length());
+      upTo = req.score(collector, windowBits, windowMin, windowMax);
+    }
+
+    if (upTo == max) {
+      upTo = req.score(collector, acceptDocs, max, max);
+    }
+    return upTo;
+  }
+
+  private int sparseScore(LeafCollector collector, Bits acceptDocs, int min, int max)
+      throws IOException {
     int upTo = min;
     int exclDoc = exclApproximation.docID();
 
@@ -76,12 +137,63 @@ final class ReqExclBulkScorer extends BulkScorer {
     if (upTo == max) {
       upTo = req.score(collector, acceptDocs, upTo, upTo);
     }
-
     return upTo;
   }
 
   @Override
   public long cost() {
     return req.cost();
+  }
+
+  private static boolean shouldUseBitSet(BulkScorer req, DocIdSetIterator excl) {
+    return excl.cost() >= (req.cost() >>> 7);
+  }
+
+  static final class WindowBits implements Bits {
+    final FixedBitSet windowMask = new FixedBitSet(WINDOW_SIZE);
+    private int windowBase;
+    private int windowEnd;
+    private int length;
+
+    void reset(int windowBase, int windowEnd, int length) {
+      this.windowBase = windowBase;
+      this.windowEnd = windowEnd;
+      this.length = length;
+    }
+
+    @Override
+    public boolean get(int index) {
+      assert index >= windowBase && index < windowEnd;
+      return windowMask.get(index - windowBase);
+    }
+
+    @Override
+    public int length() {
+      return length;
+    }
+
+    @Override
+    public void applyMask(FixedBitSet bitSet, int offset) {
+      int bitSetEnd = Math.min(DocIdSetIterator.NO_MORE_DOCS, offset + bitSet.length());
+      // The absolute doc ID ranges for bitSet and the current window.
+      int from = Math.max(windowBase, offset);
+      int to = Math.min(windowEnd, bitSetEnd);
+      if (from >= to) {
+        bitSet.clear();
+        return;
+      }
+
+      int targetFrom = from - offset;
+      if (targetFrom > 0) {
+        bitSet.clear(0, targetFrom);
+      }
+
+      int targetTo = to - offset;
+      if (targetTo < bitSet.length()) {
+        bitSet.clear(targetTo, bitSet.length());
+      }
+
+      FixedBitSet.andRange(windowMask, from - windowBase, bitSet, targetFrom, to - from);
+    }
   }
 }
