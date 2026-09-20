0a813cb56c05603b34ba519228ebbc85d6502fc7
Uwe Schindler <uschindler@apache.org>
2026-09-21 00:19:06 +0200

    Fix forbidden catch block (new feat in forbiddenapis 3.11)

--- lucene/core/src/java/org/apache/lucene/util/RamUsageEstimator.java
+++ lucene/core/src/java/org/apache/lucene/util/RamUsageEstimator.java
@@ -19,7 +19,6 @@ package org.apache.lucene.util;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.security.AccessControlException;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.text.DecimalFormat;
@@ -545,15 +544,7 @@ public final class RamUsageEstimator {
     // Walk type hierarchy
     for (; clazz != null; clazz = clazz.getSuperclass()) {
       final Class<?> target = clazz;
-      final Field[] fields;
-      try {
-        fields = doPrivileged((PrivilegedAction<Field[]>) target::getDeclaredFields);
-      } catch (
-          @SuppressWarnings("removal")
-          AccessControlException e) {
-        throw new RuntimeException("Can't access fields of class: " + target, e);
-      }
-
+      final Field[] fields = doPrivileged((PrivilegedAction<Field[]>) target::getDeclaredFields);
       for (Field f : fields) {
         if (!Modifier.isStatic(f.getModifiers())) {
           size = adjustForField(size, f);
