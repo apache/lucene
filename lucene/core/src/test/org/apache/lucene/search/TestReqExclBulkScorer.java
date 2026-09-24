c5fa32ce49f6d28e82ac1b6f111264d955d54a7f
kkewwei <kewei.11@bytedance.com>
2026-09-24 16:30:33 +0800

    Use intoBitSet to speed up ReqExclBulkScorer (#16669)

--- lucene/core/src/test/org/apache/lucene/search/TestReqExclBulkScorer.java
+++ lucene/core/src/test/org/apache/lucene/search/TestReqExclBulkScorer.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import org.apache.lucene.tests.search.RandomApproximationQuery.RandomTwoPhaseView;
 import org.apache.lucene.tests.util.LuceneTestCase;
 import org.apache.lucene.tests.util.TestUtil;
+import org.apache.lucene.util.BitSetIterator;
 import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.DocIdSetBuilder;
 import org.apache.lucene.util.FixedBitSet;
@@ -92,6 +93,13 @@ public class TestReqExclBulkScorer extends LuceneTestCase {
       reqExcl = new ReqExclBulkScorer(reqBulkScorer, excl.iterator());
     }
     final FixedBitSet actualMatches = new FixedBitSet(maxDoc);
+    final FixedBitSet acceptedDocs = new FixedBitSet(maxDoc);
+    for (int doc = 0; doc < maxDoc; doc++) {
+      if (random().nextBoolean()) {
+        acceptedDocs.set(doc);
+      }
+    }
+    Bits acceptDocs = random().nextBoolean() ? null : acceptedDocs.asReadOnlyBits();
     if (random().nextBoolean()) {
       reqExcl.score(
           new LeafCollector() {
@@ -103,7 +111,7 @@ public class TestReqExclBulkScorer extends LuceneTestCase {
               actualMatches.set(doc);
             }
           },
-          null,
+          acceptDocs,
           0,
           DocIdSetIterator.NO_MORE_DOCS);
     } else {
@@ -122,7 +130,7 @@ public class TestReqExclBulkScorer extends LuceneTestCase {
                     actualMatches.set(doc);
                   }
                 },
-                null,
+                acceptDocs,
                 min,
                 max);
         assertTrue(next >= max);
@@ -134,7 +142,153 @@ public class TestReqExclBulkScorer extends LuceneTestCase {
     FixedBitSet excludedSet = new FixedBitSet(maxDoc);
     excludedSet.or(excl.iterator());
     expectedMatches.andNot(excludedSet);
+    if (acceptDocs != null) {
+      expectedMatches.and(acceptedDocs);
+    }
 
     assertArrayEquals(expectedMatches.getBits(), actualMatches.getBits());
+  }
+
+  public void testDenseScore() throws IOException {
+    int maxDoc = 10_000;
+    FixedBitSet required = new FixedBitSet(maxDoc);
+    FixedBitSet excluded = new FixedBitSet(maxDoc);
+    FixedBitSet accepted = null;
+    FixedBitSet expected = new FixedBitSet(maxDoc);
+
+    boolean hasDelete = random().nextBoolean();
+    if (hasDelete) {
+      accepted = new FixedBitSet(maxDoc);
+    }
+
+    for (int doc = 0; doc < maxDoc; doc++) {
+      if (doc % 3 == 1) {
+        required.set(doc);
+      }
+      if ((doc & 1) == 0) {
+        excluded.set(doc);
+      }
+
+      if (required.get(doc) && excluded.get(doc) == false) {
+        expected.set(doc);
+      }
+
+      if (hasDelete) {
+        if (doc % 5 != 0) {
+          accepted.set(doc);
+        } else {
+          expected.clear(doc);
+        }
+      }
+    }
+    Bits acceptDocs = hasDelete ? accepted.asReadOnlyBits() : null;
+
+    BitSetIterator exclusion = new BitSetIterator(excluded, excluded.cardinality());
+    ReqExclBulkScorer scorer =
+        new ReqExclBulkScorer(
+            new ConstantScoreBulkScorer(
+                0f,
+                ScoreMode.COMPLETE_NO_SCORES,
+                new BitSetIterator(required, required.cardinality())),
+            exclusion);
+    FixedBitSet actual = new FixedBitSet(maxDoc);
+    assertEquals(
+        DocIdSetIterator.NO_MORE_DOCS,
+        scorer.score(
+            new LeafCollector() {
+              @Override
+              public void setScorer(Scorable scorer) {}
+
+              @Override
+              public void collect(int doc) {
+                actual.set(doc);
+              }
+            },
+            acceptDocs,
+            0,
+            maxDoc));
+
+    assertArrayEquals(expected.getBits(), actual.getBits());
+  }
+
+  public void testSparseScore() throws IOException {
+    int maxDoc = 10_000;
+    FixedBitSet excluded = new FixedBitSet(maxDoc);
+    FixedBitSet accepted = null;
+    FixedBitSet expected = new FixedBitSet(maxDoc);
+    expected.set(0, maxDoc);
+
+    excluded.set(42);
+    excluded.set(600);
+
+    expected.clear(42);
+    expected.clear(600);
+
+    boolean hasDelete = random().nextBoolean();
+    if (hasDelete) {
+      accepted = new FixedBitSet(maxDoc);
+      accepted.set(0, maxDoc);
+      accepted.clear(42);
+      accepted.clear(601);
+      expected.clear(42);
+      expected.clear(601);
+    }
+
+    BitSetIterator exclusion = new BitSetIterator(excluded, excluded.cardinality());
+    ReqExclBulkScorer scorer =
+        new ReqExclBulkScorer(
+            new ConstantScoreBulkScorer(
+                0f, ScoreMode.COMPLETE_NO_SCORES, DocIdSetIterator.all(maxDoc)),
+            exclusion);
+    FixedBitSet collected = new FixedBitSet(maxDoc);
+    assertEquals(
+        DocIdSetIterator.NO_MORE_DOCS,
+        scorer.score(
+            new LeafCollector() {
+              @Override
+              public void setScorer(Scorable scorer) {}
+
+              @Override
+              public void collect(int doc) {
+                collected.set(doc);
+              }
+            },
+            accepted != null ? accepted.asReadOnlyBits() : null,
+            0,
+            maxDoc));
+
+    assertArrayEquals(expected.getBits(), collected.getBits());
+  }
+
+  public void testWindowBitsApplyMask() {
+    ReqExclBulkScorer.WindowBits windowBits = new ReqExclBulkScorer.WindowBits();
+    windowBits.reset(100, 108, 1_000);
+    windowBits.windowMask.set(1);
+    windowBits.windowMask.set(3);
+    windowBits.windowMask.set(6);
+
+    // intersection
+    FixedBitSet target = new FixedBitSet(12);
+    target.set(0, 12);
+    target.clear(5);
+    windowBits.applyMask(target, 98);
+    FixedBitSet expected = new FixedBitSet(12);
+    // 100+1-98=3
+    // 100+6-98=8
+    expected.set(3);
+    expected.set(8);
+    assertArrayEquals(expected.getBits(), target.getBits());
+
+    // no intersection
+    target = new FixedBitSet(4);
+    target.set(0, target.length());
+    windowBits.applyMask(target, 96);
+    assertEquals(0, target.cardinality());
+
+    // no intersection
+    target = new FixedBitSet(4);
+    target.set(0, target.length());
+    windowBits.applyMask(target, 108);
+    assertEquals(0, target.cardinality());
   }
 }
